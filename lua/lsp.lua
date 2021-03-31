c65e55797b51187f9b6b82035b6dd9a509b81850
n3wborn <n3wborn@protonmail.com>
2021-03-31 20:53:27 +0200

    Autocommands : Add LSP

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -35,6 +35,20 @@ local on_attach = function(client, bufnr)
 	elseif client.resolved_capabilities.document_range_formatting then
 		buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
 	end
+
+	-- Set autocommands conditional on server_capabilities
+	if client.resolved_capabilities.document_highlight then
+		vim.api.nvim_exec([[
+		hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
+		hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
+		hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
+		augroup lsp_document_highlight
+		autocmd! * <buffer>
+		autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+		autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+		augroup END
+		]], false)
+	end
 end
 
 -- Use a loop to conveniently both setup defined servers
