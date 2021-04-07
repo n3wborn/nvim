06e0db8c8cdec4eda6456ed8f42ed72a51d726e8
n3wborn <n3wborn@protonmail.com>
2021-04-07 20:15:53 +0200

    Mappings : Update Telescope Mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,8 +4,6 @@ local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
 local g = vim.g      -- a table to access global variables
 local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
 
-
-
 local map = vim.api.nvim_set_keymap
 
 -- map key
@@ -13,6 +11,7 @@ g.mapleader = ','
 
 -- set options here to not repeat them each time
 options = { noremap = true }
+opt_nmap_silent = { noremap = true, silent = true }
 
 --
 -- Normal Mode
@@ -59,7 +58,7 @@ map('n', '<leader>+', ':vertical resize +10<cr>', options)
 map('n', '<leader>-', ':vertical resize -10<cr>', options)
 
 --- COC Goto's
-map('n', 'gd', '<Plug>(coc-definition)', { silent=true })
+map('n', 'gd', '<Plug>(coc-definition)', { silent = true })
 map('n', 'gy', '<Plug>(coc-type-definition)', { silent = true })
 map('n', 'gi', '<Plug>(coc-implementation)', { silent = true })
 map('n', 'gr', '<Plug>(coc-references)', { silent = true })
@@ -71,8 +70,6 @@ map('n', 'k', ':call show_documentation()<cr>', { silent = true })
 map('n', '<leader>e', ':NvimTreeToggle<cr>', options)
 map('n', '<leader>tr', ':NvimTreeRefreshe<cr>', options)
 
---- Telescope file_browser
-map('n', '<space>e', ':Telescope file_browser<cr>', options)
 
 
 --- Phpcbf
@@ -135,4 +132,20 @@ map('x', 'ga', '<Plug>(EasyAlign)', options)
 
 map('n', 'iga', '<Plug>(LiveEasyAlign)', options)
 map('x', 'iga', '<Plug>(LiveEasyAlign)', options)
+
+--- Telescope
+--map('n', '<space>e', ':Telescope file_browser<cr>', options)
+map('n', '<leader>f', [[<cmd>lua require('telescope.builtin').find_files()<cr>]],  opt_nmap_silent)
+map('n', '<leader>b', [[<cmd>lua require('telescope.builtin').buffers()<cr>]],  opt_nmap_silent)
+map('n', '<leader>l', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>]],  opt_nmap_silent)
+map('n', '<leader>t', [[<cmd>lua require('telescope.builtin').tags()<cr>]],  opt_nmap_silent)
+map('n', '<leader>?', [[<cmd>lua require('telescope.builtin').oldfiles()<cr>]],  opt_nmap_silent)
+map('n', '<leader>sd', [[<cmd>lua require('telescope.builtin').grep_string()<cr>]],  opt_nmap_silent)
+map('n', '<leader>sp', [[<cmd>lua require('telescope.builtin').live_grep()<cr>]],  opt_nmap_silent)
+map('n', '<leader>o', [[<cmd>lua require('telescope.builtin').tags only_current_buffer = true <cr>]], opt_nmap_silent)
+map('n', '<leader>gc', [[<cmd>lua require('telescope.builtin').git_commits()<cr>]],  opt_nmap_silent)
+map('n', '<leader>gb', [[<cmd>lua require('telescope.builtin').git_branches()<cr>]],  opt_nmap_silent)
+map('n', '<leader>gs', [[<cmd>lua require('telescope.builtin').git_status()<cr>]],  opt_nmap_silent)
+map('n', '<leader>gp', [[<cmd>lua require('telescope.builtin').git_bcommits()<cr>]],  opt_nmap_silent)
+map('n', '<leader>wb', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<cr>]],  opt_nmap_silent)
 
