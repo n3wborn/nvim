6852e0ba9e95195ff67e30adb6ca3389cc92b519
n3wborn <n3wborn@protonmail.com>
2021-05-12 16:13:19 +0200

    Plugins : Update first-indent,space-char blankline settings

--- lua/plugins/blankline.lua
+++ lua/plugins/blankline.lua
@@ -8,7 +8,6 @@ local g = vim.g
 g.indentLine_enabled = true
 
 g.indent_blankline_char = "▏"
-g.indent_blankline_space_char_blankline = ' '
 
 g.indent_blankline_use_treesitter = true
 g.indent_blankline_show_current_context = true
@@ -20,7 +19,7 @@ g.indent_blankline_filetype_exclude = {"help", "terminal"}
 g.indent_blankline_buftype_exclude = {"terminal"}
 
 g.indent_blankline_show_trailing_blankline_indent = false
-g.indent_blankline_show_first_indent_level = true
+g.indent_blankline_show_first_indent_level = false
 
 g.indent_blankline_filetype_exclude = { 'help', 'packer' }
 g.indent_blankline_buftype_exclude = { 'terminal', 'nofile'}
