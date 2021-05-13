4c37d3050b7303aeaf955b2c316e3a2a91f2fe49
n3wborn <n3wborn@protonmail.com>
2021-05-13 21:51:23 +0200

    Refactor : Cleanup init.lua

--- init.lua
+++ init.lua
@@ -1,11 +1,10 @@
 local execute = vim.api.nvim_command
 local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
-
+-- install packer if needed
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
     execute('!git clone https://github.com/wbthomason/packer.nvim '.. install_path)
 end
-
 
 vim.api.nvim_exec([[
     augroup Packer
@@ -18,8 +17,8 @@ vim.api.nvim_exec([[
 local use = require('packer').use
 require('packer').startup(function()
 
-    -- Packer can manage itself as an optional plugin
-    use { 'wbthomason/packer.nvim' }
+    -- Plugin manager
+    use 'wbthomason/packer.nvim'
 
     --- Lsp
     use 'neovim/nvim-lspconfig'
@@ -29,73 +28,82 @@ require('packer').startup(function()
     use 'onsails/lspkind-nvim'
     use 'glepnir/lspsaga.nvim'
 
+    -- Completion
+    use 'hrsh7th/nvim-compe'
+
+    -- Snippets
+    use 'hrsh7th/vim-vsnip'
+    use 'hrsh7th/vim-vsnip-integ'
+
     --- Telescope
-    use { 'nvim-telescope/telescope.nvim',
+    use {
+        'nvim-telescope/telescope.nvim',
         requires = {
-            { 'nvim-lua/popup.nvim' } ,
-            { 'nvim-lua/plenary.nvim'}
-    	}
+            'nvim-lua/popup.nvim',
+            'nvim-lua/plenary.nvim'
+        }
     }
 
-    use { 'nvim-telescope/telescope-media-files.nvim',
+    use {
+        'nvim-telescope/telescope-media-files.nvim',
         requires = {
-            { 'nvim-telescope/telescope.nvim' }
+            'nvim-telescope/telescope.nvim'
         }
     }
 
     --- Treesitter
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
     use 'nvim-treesitter/playground'
     use 'nvim-treesitter/nvim-treesitter-textobjects'
 
     -- Fzf
-    use { 'junegunn/fzf', dir = '~/.fzf', run = './install --all' }
+    use {
+        'junegunn/fzf',
+        dir = '~/.fzf',
+        run = './install --all'
+    }
     use 'junegunn/fzf.vim'
 
-    --- Code helpers
+    -- Dev div tools
+    use 'editorconfig/editorconfig-vim'
     use 'tpope/vim-commentary'
     use 'junegunn/vim-easy-align'
     use 'tpope/vim-surround'
     use 'mattn/emmet-vim'
-    use { 'lukas-reineke/indent-blankline.nvim', branch = 'lua' }
-
+    use 'ap/vim-css-color'
+    use 'lumiliet/vim-twig'
 
     -- Prettier/Linter
     use 'prettier/vim-prettier'
 
-    --- File explorer
-    use 'kyazdani42/nvim-tree.lua'
-
-    --- Lang spec
-    use 'ap/vim-css-color'
-    use 'lumiliet/vim-twig'
-
-    --- Completion
-    use 'hrsh7th/nvim-compe'
-
-    --- Snippets
-    use 'hrsh7th/vim-vsnip'
-    use 'hrsh7th/vim-vsnip-integ'
-
-    --- Git
+    -- Git
     use 'tpope/vim-fugitive'
-    use { 'lewis6991/gitsigns.nvim',
-        requires = { 'nvim-lua/plenary.nvim' }
+    use {
+        'lewis6991/gitsigns.nvim',
+        requires = {
+            'nvim-lua/plenary.nvim'
+        }
     }
 
-    --- Colors and nice stuff
-    use 'RRethy/nvim-base16'
+    -- File explorer
+    use 'kyazdani42/nvim-tree.lua'
 
-    -- replace
+    -- Colors and nice stuff
+    use 'RRethy/nvim-base16'
     use 'flazz/vim-colorschemes'
     use 'kyazdani42/nvim-web-devicons'
 
-    -- Galaxyline
-    --use 'glepnir/galaxyline.nvim'
+    -- Statusline
     use 'famiu/feline.nvim'
 
     -- Div
-    use 'editorconfig/editorconfig-vim'
+    use {
+        'lukas-reineke/indent-blankline.nvim',
+        branch = 'lua'
+    }
     use 'farmergreg/vim-lastplace'
 
 end)
