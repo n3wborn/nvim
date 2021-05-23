6e6e2a853ba9089995ccee21aa3b662477618d73
n3wborn <n3wborn@protonmail.com>
2021-05-23 14:39:46 +0200

    Mappings : Update lsp/lspsaga mappings

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,29 +1,51 @@
---"
-local nvim_lsp = require('lspconfig')
+-- lsp
 
+-- handlers config
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        underline = true,
+        virtual_text = false,
+        signs = false,
+        update_in_insert = true,
+  }
+)
 
+vim.lsp.handlers["textDocument/hover"] =
+  vim.lsp.with(
+  vim.lsp.handlers.hover,
+  {
+    border = "shadow"
+  }
+)
+
+vim.lsp.handlers["textDocument/signatureHelp"] =
+  vim.lsp.with(
+  vim.lsp.handlers.signature_help,
+  {
+    border = "shadow"
+  }
+)
+
+-- attach
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+  -- buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
+  -- Mappings (most are used by lspsaga)
   local opts = { noremap=true, silent=true }
   buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', '<leader>R', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  -- buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  -- buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  -- buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  -- buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  -- buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  -- buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  -- buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  -- buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
 
   -- Set some keybinds conditional on server capabilities
   if client.resolved_capabilities.document_formatting then
--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -35,14 +35,14 @@ map('n', '<C-b>', [[<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1
 map('n', '<leader>S', [[<cmd>lua require('lspsaga.signaturehelp').signature_help()<cr>]],  opts)
 
 -- rename
-map('n', '<leader>R', [[<cmd>lua require('lspsaga.rename').rename()<cr>]],  opts)
+-- map('n', '<leader>R', [[<cmd>lua require('lspsaga.rename').rename()<cr>]],  opts)
 
 -- preview definition
 map('n', '<leader>P', [[<cmd>lua require('lspsaga.provider').preview_definition()<cr>]],  opts)
 
 -- show diagnostic
 map('n', '<leader>D', [[<cmd>lua require('lspsaga.diagnostic').show_line_diagnostics()<cr>]],  opts)
-map('n', '<leader>d', [[<cmd>lua require('lspsaga.diagnostic').show_cursor_diagnostics()<cr>]],  opts)
+-- map('n', '<leader>d', [[<cmd>lua require('lspsaga.diagnostic').show_cursor_diagnostics()<cr>]],  opts)
 
 -- jump diagnostic
 map('n', ']d', [[<cmd>lua require('lspsaga.diagnostic').lsp_jump_diagnostic_prev()<cr>]],  opts)
