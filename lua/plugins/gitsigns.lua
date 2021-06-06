6b754c0d2d74990dfd5f32b013a9881aa2b41d43
n3wborn <n3wborn@protonmail.com>
2021-06-06 15:37:45 +0200

    Settings : Add gitsigns current_line_blame_position "right" value

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -28,6 +28,7 @@ require('gitsigns').setup {
         interval = 1000
     },
     current_line_blame = false,
+    current_line_blame_position = "right_align",
     sign_priority = 6,
     update_debounce = 100,
     status_formatter = nil, -- Use default
