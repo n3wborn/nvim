30bb09236355148d540de0f831841da1e9e9fae7
n3wborn <n3wborn@protonmail.com>
2021-06-09 21:48:35 +0200

    Settings : Set background transparent

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -18,7 +18,7 @@ local cmd = vim.cmd
 -- https://github.com/shaunsingh/nord.nvim
 g.nord_contrast               = false
 g.nord_borders                = true
-g.nord_disable_background     = false
+g.nord_disable_background     = true
 g.nord_cursorline_transparent = false
 require('nord').set()
 
