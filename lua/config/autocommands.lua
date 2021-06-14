76233bd0d60f562fe6412ab6446f467e6c3e1a30
n3wborn <n3wborn@protonmail.com>
2021-06-14 23:13:53 +0200

    Fix : Fix autocmd missing ")"

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -1,7 +1,9 @@
 -- autocommands
-local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
+local cmd = vim.api.nvim_command
 
 -- delete trailing spaces
-cmd 'autocmd BufWritePre * :call TrimWhitespace()'
+cmd('autocmd BufWritePre * :call TrimWhitespace()')
 -- check all git commit messages
-cmd 'au BufNewFile,BufRead COMMIT_EDITMSG set spell nonumber nolist wrap linebreak'
+cmd('au BufNewFile,BufRead COMMIT_EDITMSG set spell nonumber nolist wrap linebreak')
+-- show doc at CursorHold
+--cmd('autocmd CursorHold  <buffer> lua vim.lsp.buf.hover()')
