49b6067ab8574ab01490e7b26dc422192b578779
n3wborn <n3wborn@protonmail.com>
2021-06-20 11:34:45 +0200

    Settings  : Remove nvim-tree default icons settings

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -36,32 +36,3 @@ g.nvim_tree_show_icons = {
     files = 1,
 }
 
-g.nvim_tree_icons = {
-    default = '',
-    symlink = '',
-    git = {
-        unstaged = "✗",
-        staged = "✓",
-        unmerged = "",
-        renamed = "➜",
-        untracked = "★",
-        deleted = "",
-        ignored = "◌"
-    },
-    folder = {
-        arrow_open = "",
-        arrow_closed = "",
-        default = "",
-        open = "",
-        empty = "",
-        empty_open = "",
-        symlink = "",
-        symlink_open = "",
-    },
-    lsp = {
-        hint = "",
-        info = "",
-        warning = "",
-        error = "",
-    }
-}
