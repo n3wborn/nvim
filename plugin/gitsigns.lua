6af9cc783d5e41c63ce780dc889393038f53989a
n3wborn <n3wborn@protonmail.com>
2021-07-06 00:08:02 +0200

    Mappings : Update gitsigns
        - Group similar together
        - Add a bit of doc
        - Add new ones (actions, diffthis, toggle_current_line_blame)

--- plugin/gitsigns.lua
+++ plugin/gitsigns.lua
@@ -2,7 +2,6 @@
 
 require('gitsigns').setup {
     signs = {
-        -- TODO add hl to colorscheme
         add          = {hl = 'GitSignsAdd'   , text = '▎', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
         change       = {hl = 'GitSignsChange', text = '▎', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
         delete       = {hl = 'GitSignsDelete', text = '▎', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
@@ -12,20 +11,40 @@ require('gitsigns').setup {
     numhl = false,
     linehl = false,
     keymaps = {
-        -- Default keymap options
         noremap = true,
         buffer = true,
+
+        -- netx/prev_hunk
         ['n <leader>hn'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"},
         ['n <leader>hN'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"},
 
+        -- stage/unstage hunk/buffer
         ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
         ['v <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
         ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+        ['n <leader>hS'] = '<cmd>lua require"gitsigns".stage_buffer()<CR>',
+
+        -- reset hunk/buffer
         ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
         ['v <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
         ['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
+
+        -- diff
+        ['n <leader>hd'] = [[<cmd>lua require"gitsigns".diffthis()<CR>]],
+
+        -- preview
         ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-        ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
+
+        -- blame
+        ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
+        ['n <leader>hB'] = '<cmd>lua require"gitsigns".toggle_current_line_blame()<CR>',
+
+        -- actions
+        ['n <leader>ha'] = '<cmd>lua require"gitsigns".get_actions()<CR>',
+
+        -- refresh/toggle(?)
+        ['n <leader>ht'] = '<cmd>lua require"gitsigns".refresh()<CR>',
+
     },
     watch_index = {
         interval = 1000
@@ -34,6 +53,8 @@ require('gitsigns').setup {
     current_line_blame_position = "right_align",
     sign_priority = 6,
     update_debounce = 100,
-    status_formatter = nil, -- Use default
-    use_decoration_api = true
+    status_formatter = nil,
+    use_decoration_api = true,
+    use_internal_diff = true,
 }
+
