9b34b221d0383ed1e9deae7f8be38801a9b5620e
Stéphane P <n3wborn@protonmail.com>
2021-07-15 20:19:12 +0200

    Refactor(Telescope) : Replace mappings with setup-mappings() (#7)

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -1,5 +1,4 @@
 -- https://github.com/nvim-telescope/telescope.nvim
-
 require('telescope').setup {
     defaults = {
         sorting_strategy = "ascending",
@@ -7,36 +6,48 @@ require('telescope').setup {
     },
 }
 
+
 -- Mappings
+local function setup_mappings()
 
-local map  = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
+    local map  = vim.api.nvim_set_keymap
+    local o = { noremap = true, silent = true }
+    local b_require    = '<cmd>lua require(\'telescope.builtin\').'
 
--- Call Telescope
-map('n', '<leader>T', [[:Telescope<cr>]],  opts)
+    local kmaps = {
+        -- General builtins
+        sp = 'live_grep()',
+        d  = 'grep_string()',
+        f  = 'find_files()',
+        b  = 'buffers()',
+        o  = 'oldfiles()',
+        E  = 'file_browser()',
+        t  = 'treesitter()',
+        -- Git builtins
+        gc = 'git_commits()',
+        gb = 'git_branches()',
+        gs = 'git_status()',
+        gp = 'git_bcommits()',
+        gS = 'git_stash()',
+        -- lsp builtins
+        ls = 'lsp_document_symbols()',
+        lD = 'lsp_document_diagnostics()',
+        lr = 'lsp_references()',
+        li = 'lsp_implementations()',
+        ld = 'lsp_definitions()',
+    }
 
--- General builtins
-map('n', '<leader>sp', [[<cmd>lua require('telescope.builtin').live_grep()<cr>]],  opts)
-map('n', '<leader>f' , [[<cmd>lua require('telescope.builtin').find_files()<cr>]],  opts)
-map('n', '<leader>b' , [[<cmd>lua require('telescope.builtin').buffers()<cr>]],  opts)
-map('n', '<leader>t' , [[<cmd>lua require('telescope.builtin').tags()<cr>]],  opts)
-map('n', '<leader>?' , [[<cmd>lua require('telescope.builtin').oldfiles()<cr>]],  opts)
-map('n', '<leader>sd', [[<cmd>lua require('telescope.builtin').grep_string()<cr>]],  opts)
-map('n', '<leader>E' , [[<cmd>lua require('telescope.builtin').file_browser()<cr>]],  opts)
-map('n', '<leader>t' , [[<cmd>lua require('telescope.builtin').treesitter()<cr>]],  opts)
--- Git builtins
-map('n', '<leader>gc', [[<cmd>lua require('telescope.builtin').git_commits()<cr>]],  opts)
-map('n', '<leader>gb', [[<cmd>lua require('telescope.builtin').git_branches()<cr>]],  opts)
-map('n', '<leader>gs', [[<cmd>lua require('telescope.builtin').git_status()<cr>]],  opts)
-map('n', '<leader>gp', [[<cmd>lua require('telescope.builtin').git_bcommits()<cr>]],  opts)
-map('n', '<leader>gS', [[<cmd>lua require('telescope.builtin').git_stash()<cr>]],  opts)
--- lsp builtins
-map('n', '<leader>ls', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<cr>]],  opts)
-map('n', '<leader>lD', [[<cmd>lua require('telescope.builtin').lsp_document_diagnostics()<cr>]],  opts)
-map('n', '<leader>lr', [[<cmd>lua require('telescope.builtin').lsp_references()<cr>]],  opts)
-map('n', '<leader>li', [[<cmd>lua require('telescope.builtin').lsp_implementations()<cr>]],  opts)
-map('n', '<leader>ld', [[<cmd>lua require('telescope.builtin').lsp_definitions()<cr>]],  opts)
--- Telescope project
-map('n', '<C-p>', [[:lua require'telescope'.extensions.project.project{}<CR>]], opts)
--- Extensions example
---map('n', 'leader>gw', [[<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<cr>]], opts)
+    for key,builtin in pairs(kmaps) do
+        map('n', '<leader>' .. key, b_require .. builtin .. '<cr>', o)
+    end
+
+    -- Call Telescope with sugar
+    map('n', '<leader>T', [[:Telescope<cr>]],  o)
+
+    -- Telescope project extension
+    map('n', '<C-p>', [[:lua require'telescope'.extensions.project.project{}<CR>]], o)
+end
+
+
+setup_mappings()
+
