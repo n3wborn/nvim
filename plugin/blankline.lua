1ca9a78190cb27598a9e2760ec5e9d9474e5bfc3
n3wborn <n3wborn@protonmail.com>
2021-07-17 19:28:29 +0200

    Fix(indent-blankline) : Remove duplicated and default config

--- plugin/blankline.lua
+++ plugin/blankline.lua
@@ -5,24 +5,15 @@
 local cmd = vim.cmd
 local g   = vim.g
 
-g.indentLine_enabled = true
-
-g.indent_blankline_char = "▏"
-
 g.indent_blankline_use_treesitter = true
 g.indent_blankline_show_current_context = true
 g.indent_blankline_context_patterns = {
     'class', 'function', 'method', '^if', '^while',
     '^for', '^object', '^table', 'block', 'arguments'
 }
-g.indent_blankline_filetype_exclude = {"help", "terminal"}
-g.indent_blankline_buftype_exclude = {"terminal"}
 
-g.indent_blankline_show_trailing_blankline_indent = false
-g.indent_blankline_show_first_indent_level = false
-
-g.indent_blankline_filetype_exclude = { 'help', 'packer' }
-g.indent_blankline_buftype_exclude = { 'terminal', 'nofile'}
+g.indent_blankline_filetype_exclude = { 'help', 'packer', 'terminal' }
+g.indent_blankline_buftype_exclude = { 'help', 'terminal', 'nofile'}
 g.indent_blankline_char_highlight = 'LineNr'
 
 cmd [[ hi IndentBlanklineChar guifg=#aaaaaa ]]
