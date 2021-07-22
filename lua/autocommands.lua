6cb7f96e883d758d2003d4258e4bba0262afbd00
n3wborn <n3wborn@protonmail.com>
2021-07-22 21:30:29 +0200

    Settings(autocmds): Prefer format mappings vs autocmd

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -60,9 +60,9 @@ local autocmds = {
     -- hover = {
     --     {"CursorHold", "*", "lua vim.lsp.buf.hover()"};
     -- };
-    formatter = {
-        { 'BufWritePost', '*.js,*.rs,*.lua', 'FormatWrite' },
-    },
+    -- formatter = {
+    --     { 'BufWritePost', '*.js,*.rs,*.lua', 'FormatWrite' },
+    -- },
 }
 
 nvim_create_augroups(autocmds)
