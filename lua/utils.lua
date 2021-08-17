384058476ab10b6564063d32b377e4b8ae7dee7e
n3wborn <n3wborn@protonmail.com>
2021-08-17 13:44:16 +0200

    Chore : Cleanup utils

--- lua/utils.lua
+++ lua/utils.lua
@@ -3,6 +3,12 @@
 local M = {}
 local map_opts = { noremap = true, silent = true }
 
+--[[
+--
+-- local functions
+--
+---]]
+
 -- nvim_replace_termcodes
 local t = function(str)
     return vim.api.nvim_replace_termcodes(str, true, true, true)
@@ -13,6 +19,20 @@ local check_back_space = function()
     local col = vim.fn.col('.') - 1
     return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
 end
+
+-- https://github.com/neovim/nvim-lspconfig/wiki/User-contributed-tips#peek-definition
+local function preview_location_callback(_, _, result)
+    if result == nil or vim.tbl_isempty(result) then
+        return nil
+    end
+    vim.lsp.util.preview_location(result[1])
+end
+
+--[[
+--
+-- utils methods
+--
+----]]
 
 -- autocommands
 M.nvim_create_augroups = function(definitions)
@@ -35,37 +55,6 @@ end
 -- mappings
 M.map = function(mode, lhs, rhs, ...)
     vim.api.nvim_set_keymap(mode, lhs, rhs, ... or map_opts)
-end
-
---- https://github.com/hrsh7th/nvim-compe#how-to-use-tab-to-navigate-completion-menu
-_G.tab_complete = function()
-    if vim.fn.pumvisible() == 1 then
-        return t('<C-n>')
-    elseif require('luasnip').expand_or_jumpable() then
-        return t("<cmd>lua require'luasnip'.jump(1)<Cr>")
-    elseif check_back_space() then
-        return t('<Tab>')
-    else
-        return vim.fn['compe#complete']()
-    end
-end
-
-_G.s_tab_complete = function()
-    if vim.fn.pumvisible() == 1 then
-        return t('<C-p>')
-    elseif require('luasnip').jumpable(-1) then
-        return t("<cmd>lua require'luasnip'.jump(-1)<CR>")
-    else
-        return t('<S-Tab>')
-    end
-end
-
--- https://github.com/neovim/nvim-lspconfig/wiki/User-contributed-tips#peek-definition
-local function preview_location_callback(_, _, result)
-    if result == nil or vim.tbl_isempty(result) then
-        return nil
-    end
-    vim.lsp.util.preview_location(result[1])
 end
 
 M.PeekDefinition = function()
@@ -109,5 +98,39 @@ M.kind_cfg = {
         TypeParameter = ' ',
     },
 }
+
+--[[
+--
+-- global functions
+--
+---]]
+function _G.dump(...)
+    local objects = vim.tbl_map(vim.inspect, { ... })
+    print(unpack(objects))
+    return ...
+end
+
+--- https://github.com/hrsh7th/nvim-compe#how-to-use-tab-to-navigate-completion-menu
+_G.tab_complete = function()
+    if vim.fn.pumvisible() == 1 then
+        return t('<C-n>')
+    elseif require('luasnip').expand_or_jumpable() then
+        return t("<cmd>lua require'luasnip'.jump(1)<Cr>")
+    elseif check_back_space() then
+        return t('<Tab>')
+    else
+        return vim.fn['compe#complete']()
+    end
+end
+
+_G.s_tab_complete = function()
+    if vim.fn.pumvisible() == 1 then
+        return t('<C-p>')
+    elseif require('luasnip').jumpable(-1) then
+        return t("<cmd>lua require'luasnip'.jump(-1)<CR>")
+    else
+        return t('<S-Tab>')
+    end
+end
 
 return M
