8aef5496169cc49741ab7ed77b35611b137a7e18
n3wborn <n3wborn@protonmail.com>
2021-08-22 21:39:35 +0200

    Settings(formatter) : Add shfmt and vue format options

--- lua/modules/plugins/formatter.lua
+++ lua/modules/plugins/formatter.lua
@@ -2,7 +2,10 @@
 local function luaformat()
     return {
         exe = 'lua-format',
-        args = { '-c', 'luaformat.rc' },
+        args = {
+            '-c',
+            'luaformat.rc',
+        },
         stdin = true,
     }
 end
@@ -10,7 +13,10 @@ end
 local function luastyle()
     return {
         exe = 'stylua',
-        args = { '--search-parent-directories', '-' },
+        args = {
+            '--search-parent-directories',
+            '-',
+        },
         stdin = true,
     }
 end
@@ -18,7 +24,10 @@ end
 local function prettier()
     return {
         exe = 'prettier',
-        args = { '--stdin-filepath', vim.api.nvim_buf_get_name(0) },
+        args = {
+            '--stdin-filepath',
+            vim.fn.shellescape(vim.api.nvim_buf_get_name(0)),
+        },
         stdin = true,
     }
 end
@@ -26,7 +35,12 @@ end
 local function eslint_d()
     return {
         exe = 'eslint_d',
-        args = { '--fix-to-stdout', '--stdin', '--stdin-filename', vim.api.nvim_buf_get_name(0) },
+        args = {
+            '--fix-to-stdout',
+            '--stdin',
+            '--stdin-filename',
+            vim.fn.shellescape(vim.api.nvim_buf_get_name(0)),
+        },
         stdin = true,
     }
 end
@@ -39,11 +53,35 @@ local function rustfmt()
     }
 end
 
+local function shfmt()
+    return {
+        exe = 'shfmt',
+        stdin = true,
+    }
+end
+
+local function vue()
+    return {
+        exe = 'prettier',
+        args = {
+            '--stdin-filepath',
+            vim.fn.shellescape(vim.api.nvim_buf_get_name(0)),
+            '--single-quote',
+            '--parser',
+            'vue',
+        },
+        stdin = true,
+    }
+end
+
 require('formatter').setup({
     logging = false,
     filetype = {
-        javascript = { eslint_d },
+        javascript = { prettier },
+        scss = { prettier },
         rust = { rustfmt },
         lua = { luastyle },
+        vue = { vue },
+        sh = { shfmt },
     },
 })
