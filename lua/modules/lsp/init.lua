282c83b66d9f44590130ed08fb2f9c9a8ccbc6ec
n3wborn <n3wborn@protonmail.com>
2021-08-30 16:43:36 +0200

    Settings(lsp) : Add doc,detail,additionalTextEdits capabilities

--- lua/modules/lsp/init.lua
+++ lua/modules/lsp/init.lua
@@ -105,6 +105,13 @@ end
 -- Capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+  properties = {
+    'documentation',
+    'detail',
+    'additionalTextEdits',
+  }
+}
 
 -- Servers
 local servers = {
