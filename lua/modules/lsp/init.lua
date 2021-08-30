3bc109b56615164743e429cb4390ac3d80c195d7
n3wborn <n3wborn@users.noreply.github.com>
2021-08-30 16:22:47 +0000

    Format with stylua

--- lua/modules/lsp/init.lua
+++ lua/modules/lsp/init.lua
@@ -106,11 +106,11 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 capabilities.textDocument.completion.completionItem.resolveSupport = {
-  properties = {
-    'documentation',
-    'detail',
-    'additionalTextEdits',
-  }
+    properties = {
+        'documentation',
+        'detail',
+        'additionalTextEdits',
+    },
 }
 
 -- Servers
