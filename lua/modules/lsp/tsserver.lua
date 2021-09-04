1079d615e1d0bb495704041f65e3293461ed7e2e
n3wborn <n3wborn@users.noreply.github.com>
2021-09-04 11:20:01 +0000

    Format with stylua

--- lua/modules/lsp/tsserver.lua
+++ lua/modules/lsp/tsserver.lua
@@ -1,19 +1,19 @@
-local lspconfig = require("lspconfig")
-local u = require("utils")
+local lspconfig = require('lspconfig')
+local u = require('utils')
 
 local ts_utils_settings = {
     enable_import_on_completion = true,
     import_all_scan_buffers = 100,
-    eslint_bin = "eslint_d",
+    eslint_bin = 'eslint_d',
     eslint_enable_diagnostics = true,
     eslint_opts = {
         condition = function(utils)
-            return utils.root_has_file(".eslintrc.js")
+            return utils.root_has_file('.eslintrc.js')
         end,
-        diagnostics_format = "#{m} [#{c}]",
+        diagnostics_format = '#{m} [#{c}]',
     },
     enable_formatting = true,
-    formatter = "eslint_d",
+    formatter = 'eslint_d',
     update_imports_on_move = true,
     -- filter out dumb module warning
     filter_out_diagnostics_by_code = { 80001 },
@@ -28,17 +28,17 @@ M.setup = function(on_attach)
 
             on_attach(client, bufnr)
 
-            local ts_utils = require("nvim-lsp-ts-utils")
+            local ts_utils = require('nvim-lsp-ts-utils')
             ts_utils.setup(ts_utils_settings)
             ts_utils.setup_client(client)
 
-            u.buf_map("n", "gs", ":TSLspOrganize<CR>", nil, bufnr)
-            u.buf_map("n", "gI", ":TSLspRenameFile<CR>", nil, bufnr)
-            u.buf_map("n", "go", ":TSLspImportAll<CR>", nil, bufnr)
-            u.buf_map("n", "qq", ":TSLspFixCurrent<CR>", nil, bufnr)
-            u.buf_map("i", ".", ".<C-x><C-o>", nil, bufnr)
+            u.buf_map('n', 'gs', ':TSLspOrganize<CR>', nil, bufnr)
+            u.buf_map('n', 'gI', ':TSLspRenameFile<CR>', nil, bufnr)
+            u.buf_map('n', 'go', ':TSLspImportAll<CR>', nil, bufnr)
+            u.buf_map('n', 'qq', ':TSLspFixCurrent<CR>', nil, bufnr)
+            u.buf_map('i', '.', '.<C-x><C-o>', nil, bufnr)
 
-            vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
+            vim.opt_local.omnifunc = 'v:lua.vim.lsp.omnifunc'
         end,
         flags = {
             debounce_text_changes = 150,
@@ -47,4 +47,3 @@ M.setup = function(on_attach)
 end
 
 return M
-
