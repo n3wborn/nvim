6c1de8cd9dbcf0ee55077b1a4467b73ef5532ff8
n3wborn <n3wborn@protonmail.com>
2021-09-24 01:35:25 +0200

    Settings : Refactor luasnip config

--- lua/modules/plugins/snippets.lua
+++ lua/modules/plugins/snippets.lua
@@ -1,41 +1,14 @@
-local present, ls = pcall(require, 'luasnip')
-if not present then
-    return
+-- https://github.com/L3MON4D3/LuaSnip
+local function prequire(...)
+    local status, lib = pcall(require, ...)
+    if status then
+        return lib
+    end
+    return nil
 end
 
-local s = ls.snippet
-local sn = ls.snippet_node
-local t = ls.text_node
-local i = ls.insert_node
-local f = ls.function_node
-local c = ls.choice_node
-local d = ls.dynamic_node
-local l = require('luasnip.extras').lambda
-local r = require('luasnip.extras').rep
-local p = require('luasnip.extras').partial
-local m = require('luasnip.extras').match
-local n = require('luasnip.extras').nonempty
-local dl = require('luasnip.extras').dynamic_lambda
-local types = require('luasnip.util.types')
+local luasnip = prequire('luasnip')
 
 vim.api.nvim_command('hi LuasnipChoiceNodePassive cterm=italic')
-
-ls.config.set_config({
-    history = true,
-    -- Update more often, :h events for more info.
-    updateevents = 'TextChanged,TextChangedI',
-    ext_opts = {
-        [types.choiceNode] = {
-            active = {
-                virt_text = { { 'choiceNode', 'Comment' } },
-            },
-        },
-    },
-    -- treesitter-hl has 100, use something higher (default is 200).
-    ext_base_prio = 300,
-    -- minimal increase in priority.
-    ext_prio_increase = 1,
-    enable_autosnippets = true,
-})
 
 require('luasnip/loaders/from_vscode').lazy_load()
