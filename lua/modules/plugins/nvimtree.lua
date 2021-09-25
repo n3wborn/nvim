0654f436d599b881bb1464ce110c1780dc472928
n3wborn <n3wborn@protonmail.com>
2021-09-25 21:41:18 +0200

    Fix(nvimtree) : Update using new setup() function
    See : https://github.com/kyazdani42/nvim-tree.lua/commit/a864b80bafeba6ea17d1a55e4c6f5e73f189f6bb

--- lua/modules/plugins/nvimtree.lua
+++ lua/modules/plugins/nvimtree.lua
@@ -1,9 +1,15 @@
 -- https://github.com/kyazdani42/nvim-tree.lua
+vim.g.nvim_tree_ignore = { '.git', 'node_modules', '.cache' }
 
-local g = vim.g
-
-g.nvim_tree_auto_close = 1
-g.nvim_tree_group_empty = 1
-g.nvim_tree_hijack_netrw = 1
-g.nvim_tree_ignore = { '.git', 'node_modules', '.cache' }
-g.nvim_tree_tab_open = 1
+require('nvim-tree').setup({
+    disable_netrw = true,
+    ignore_ft_on_setup = { 'startify', 'dashboard' },
+    auto_close = true,
+    hijack_cursor = false,
+    update_cwd = true,
+    update_focused_file = {
+        enable = true,
+        update_cwd = false,
+        ignore_list = {},
+    },
+})
