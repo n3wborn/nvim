53a36e247f1cb4c04aa4a57be82c52a2f64b8758
Stéphane P <n3wborn@protonmail.com>
2021-10-11 14:05:01 +0200

    chore(cmp) : Update next/prev mappings (#67)

--- lua/modules/plugins/cmp.lua
+++ lua/modules/plugins/cmp.lua
@@ -57,8 +57,10 @@ cmp.setup({
         end,
     },
     mapping = {
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<Down>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        ['<Up>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
