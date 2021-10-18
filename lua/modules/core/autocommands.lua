2ae1705adf7346235900437bbb76f3cfc9400cdc
n3wborn <n3wborn@protonmail.com>
2021-10-18 16:36:53 +0200

    Settings(autocommands) : Replace buf_augroup with nvim_exec

--- lua/modules/core/autocommands.lua
+++ lua/modules/core/autocommands.lua
@@ -1,12 +1,12 @@
 -- autocommands
-local u = require('utils')
+local exec = vim.api.nvim_exec
 
-local TrimWhitespace = vim.api.nvim_exec(
+local TrimWhitespace = exec(
     [[
     function! TrimWhitespace()
-    let l:save = winsaveview()
-    keeppatterns %s/\s\+$//e
-    call winrestview(l:save)
+        let l:save = winsaveview()
+        keeppatterns %s/\s\+$//e
+        call winrestview(l:save)
     endfunction
 
     call TrimWhitespace()
@@ -14,29 +14,22 @@ local TrimWhitespace = vim.api.nvim_exec(
     true
 )
 
-local autocmds = {
-    set_formatoptions = {
-        { 'BufEnter', '*', 'setlocal formatoptions-=o' },
-    },
-    restore_cursor = {
-        { 'BufRead', '*', [[call setpos(".", getpos("'\""))]] },
-    },
-    resize_windows_proportionally = {
-        { 'VimResized', '*', [[tabdo wincmd =]] },
-    },
-    solidity = {
-        { 'BufRead,BufNewFile', '*.sol', 'set filetype=solidity' },
-    },
-    twig = {
-        { 'BufRead,BufNewFile', '*.twig', 'set filetype=html.twig' },
-    },
-    trim_white_space = {
-        { 'BufWritePre', '*', 'call TrimWhitespace()' },
-    },
-    git_commit = {
-        { 'BufNewFile, BufRead', 'COMMIT_EDITMSG', 'set spell nonumber wrap linebreak' },
-        { 'FileType', 'gitcommit', 'let b:EditorConfig_disable = 1' },
-    },
-}
+-- don't auto commenting new lines
+exec([[au BufEnter * set fo-=c fo-=r fo-=o]], false)
 
-u.nvim_create_augroups(autocmds)
+-- disable IndentLine for markdown files (avoid concealing)
+exec([[au FileType markdown let g:indentLine_enabled=0]], false)
+
+-- keep windows equally resized
+exec([[au VimResized * tabdo wincmd =]], false)
+
+-- filetype
+exec([[au BufRead,BufNewFile *.sol set filetype=solidity]], false)
+exec([[au BufRead,BufNewFile *.twig set filetype=html.twig]], false)
+
+-- git commit
+exec([[au BufNewFile,BufRead COMMIT_EDITMSG set spell nonumber wrap linebreak]], false)
+exec([[au filetype gitcommit let b:EditorConfig_disable=1]], false)
+
+-- trim_white_space
+exec([[au BufWritePre * call TrimWhitespace()]], false)
