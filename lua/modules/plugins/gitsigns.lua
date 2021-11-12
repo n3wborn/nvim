235d144e8e8020d6797b6cddf507337a14ac70bc
n3wborn <n3wborn@protonmail.com>
2021-11-12 16:55:50 +0100

    Settings(gitsigns) : Remove default settings and cleanup

--- lua/modules/plugins/gitsigns.lua
+++ lua/modules/plugins/gitsigns.lua
@@ -3,64 +3,29 @@ local gitsigns = require('gitsigns')
 
 gitsigns.setup({
     signs = {
-        add = { hl = 'GitSignsAdd', text = '▎', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-        change = { hl = 'GitSignsChange', text = '▎', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-        delete = { hl = 'GitSignsDelete', text = '▎', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-        topdelete = { hl = 'GitSignsDelete', text = '▎', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-        changedelete = {
-            hl = 'GitSignsChange',
-            text = '▎',
-            numhl = 'GitSignsChangeNr',
-            linehl = 'GitSignsChangeLn',
-        },
+        add = { hl = 'GitSignsAdd', text = '▎' },
+        change = { hl = 'GitSignsChange', text = '▎' },
+        delete = { hl = 'GitSignsDelete', text = '▎' },
+        topdelete = { hl = 'GitSignsDelete', text = '▎' },
+        changedelete = { hl = 'GitSignsChange', text = '▎' },
     },
-    numhl = false,
-    linehl = false,
+    word_diff = true,
     keymaps = {
         noremap = true,
-
-        -- netx/prev_hunk
         ['n <leader>hn'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'" },
         ['n <leader>hN'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'" },
-
-        -- stage/unstage hunk/buffer
-        ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+        ['n <leader>hs'] = ':Gitsigns stage_hunk<CR>',
         ['v <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-        ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-        ['n <leader>hS'] = '<cmd>lua require"gitsigns".stage_buffer()<CR>',
-
-        -- reset hunk/buffer
-        ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+        ['n <leader>hu'] = ':Gitsigns undo_stage_hunk<CR>',
+        ['n <leader>hS'] = ':Gitsigns stage_buffer<CR>',
+        ['n <leader>hr'] = ':Gitsigns reset_hunk<CR>',
         ['v <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-        ['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
-
-        -- diff
-        ['n <leader>hd'] = [[<cmd>lua require"gitsigns".diffthis()<CR>]],
-
-        -- preview
-        ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-
-        -- blame
-        ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
-        ['n <leader>hB'] = '<cmd>lua require"gitsigns".toggle_current_line_blame()<CR>',
-
-        -- actions
-        ['n <leader>ha'] = '<cmd>lua require"gitsigns".get_actions()<CR>',
-
-        -- refresh/toggle(?)
-        ['n <leader>ht'] = '<cmd>lua require"gitsigns".refresh()<CR>',
+        ['n <leader>hR'] = ':Gitsigns reset_buffer<CR>',
+        ['n <leader>hd'] = ':Gitsigns diffthis<CR>',
+        ['n <leader>hp'] = ':Gitsigns preview_hunk<CR>',
+        ['n <leader>hb'] = ':Gitsigns toggle_current_line_blame<CR>',
+        ['n <leader>ha'] = ':Gitsigns get_actions<CR>',
+        ['n <leader>ht'] = ':Gitsigns refresh<CR>',
     },
-    watch_gitdir = {
-        interval = 1000,
-    },
-    current_line_blame = false,
-    current_line_blame_opts = {
-        virt_text = true,
-        virt_text_pos = 'right_align',
-        delay = 1000,
-    },
-    sign_priority = 6,
-    update_debounce = 100,
-    status_formatter = nil,
-    diff_opts = { internal = true },
+    preview_config = { border = 'rounded' },
 })
