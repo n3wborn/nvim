619e6250bc56a10e652f6cb0d7e55735c415f7ee
n3wborn <n3wborn@protonmail.com>
2021-12-10 10:48:12 +0100

    Settings(trouble) : Update diagnostics settings

--- lua/modules/plugins/trouble.lua
+++ lua/modules/plugins/trouble.lua
@@ -1,20 +1,20 @@
 -- https://github.com/folke/trouble.nvim
 
 require('trouble').setup({
-    -- "lsp_workspace_diagnostics",
-    -- "lsp_document_diagnostics",
+    -- "workspace_diagnostics",
+    -- "document_diagnostics",
     -- "quickfix",
     -- "lsp_references",
     -- "loclist"
-    mode = 'lsp_document_diagnostics',
+    mode = 'document_diagnostics',
 })
 
 -- mappings
 local u = require('utils')
 
 u.map('n', '<leader>xx', '<cmd>Trouble<cr>')
-u.map('n', '<leader>xD', '<cmd>Trouble lsp_workspace_diagnostics<cr>')
-u.map('n', '<leader>xd', '<cmd>Trouble lsp_document_diagnostics<cr>')
+u.map('n', '<leader>xD', '<cmd>Trouble workspace_diagnostics<cr>')
+u.map('n', '<leader>xd', '<cmd>Trouble document_diagnostics<cr>')
 u.map('n', '<leader>xl', '<cmd>Trouble loclist<cr>')
 u.map('n', '<leader>xq', '<cmd>Trouble quickfix<cr>')
 u.map('n', '<leader>xR', '<cmd>Trouble lsp_references<cr>')
