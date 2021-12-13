52322a2129afd31dca627513ed67d01d674c6af0
n3wborn <n3wborn@protonmail.com>
2021-12-13 11:24:49 +0100

    Settings(lsp) : explode config
    - move each LS in it's own file
    - update null-ls config
    - add missing LS
    - update utils
    - add missing plugins

--- lua/modules/lsp/tsserver.lua
+++ lua/modules/lsp/tsserver.lua
@@ -1,48 +1,34 @@
-local lspconfig = require('lspconfig')
 local u = require('utils')
 
 local ts_utils_settings = {
-    enable_import_on_completion = true,
     import_all_scan_buffers = 100,
-    eslint_bin = 'eslint_d',
-    eslint_enable_diagnostics = true,
-    eslint_opts = {
-        condition = function(utils)
-            return utils.root_has_file('.eslintrc.js')
-        end,
-        diagnostics_format = '#{m} [#{c}]',
-    },
-    enable_formatting = true,
-    formatter = 'eslint_d',
     update_imports_on_move = true,
-    -- filter out dumb module warning
     filter_out_diagnostics_by_code = { 80001 },
 }
 
 local M = {}
-M.setup = function(on_attach)
-    lspconfig.tsserver.setup({
-        on_attach = function(client, bufnr)
-            client.resolved_capabilities.document_formatting = false
-            client.resolved_capabilities.document_range_formatting = false
 
+M.setup = function(on_attach, capabilities)
+    local lspconfig = require('lspconfig')
+    local ts_utils = require('nvim-lsp-ts-utils')
+
+    lspconfig['tsserver'].setup({
+        root_dir = lspconfig.util.root_pattern('package.json'),
+        init_options = ts_utils.init_options,
+        on_attach = function(client, bufnr)
             on_attach(client, bufnr)
 
-            local ts_utils = require('nvim-lsp-ts-utils')
             ts_utils.setup(ts_utils_settings)
             ts_utils.setup_client(client)
 
-            u.buf_map('n', 'gs', ':TSLspOrganize<CR>', nil, bufnr)
-            u.buf_map('n', 'gI', ':TSLspRenameFile<CR>', nil, bufnr)
-            u.buf_map('n', 'go', ':TSLspImportAll<CR>', nil, bufnr)
-            u.buf_map('n', 'qq', ':TSLspFixCurrent<CR>', nil, bufnr)
-            u.buf_map('i', '.', '.<C-x><C-o>', nil, bufnr)
-
-            vim.opt_local.omnifunc = 'v:lua.vim.lsp.omnifunc'
+            u.buf_map(bufnr, 'n', 'gO', ':TSLspOrganize<CR>')
+            u.buf_map(bufnr, 'n', 'gR', ':TSLspRenameFile<CR>')
+            u.buf_map(bufnr, 'n', 'gI', ':TSLspImportAll<CR>')
         end,
         flags = {
             debounce_text_changes = 150,
         },
+        capabilities = capabilities,
     })
 end
 
--- lua/utils.lua
+++ lua/utils.lua
@@ -27,17 +27,14 @@ end
 M.map = function(mode, target, source, opts)
     api.nvim_set_keymap(mode, target, source, get_map_options(opts))
 end
-M.nmap = function(...)
-    M.map('n', ...)
-end
-M.omap = function(...)
-    M.map('o', ...)
-end
-M.imap = function(...)
-    M.map('i', ...)
+
+for _, mode in ipairs({ 'n', 'o', 'i', 'x', 't' }) do
+    M[mode .. 'map'] = function(...)
+        M.map(mode, ...)
+    end
 end
 
-M.buf_map = function(mode, target, source, opts, bufnr)
+M.buf_map = function(bufnr, mode, target, source, opts)
     api.nvim_buf_set_keymap(bufnr or 0, mode, target, source, get_map_options(opts))
 end
 
@@ -135,6 +132,45 @@ M.buf_augroup = function(name, event, fn)
         ),
         false
     )
+end
+
+M.warn = function(msg)
+    api.nvim_echo({ { msg, 'WarningMsg' } }, true, {})
+end
+
+M.is_file = function(path)
+    if path == '' then
+        return false
+    end
+
+    local stat = vim.loop.fs_stat(path)
+    return stat and stat.type == 'file'
+end
+
+M.make_floating_window = function(custom_window_config, height_ratio, width_ratio)
+    height_ratio = height_ratio or 0.8
+    width_ratio = width_ratio or 0.8
+
+    local height = math.ceil(vim.opt.lines:get() * height_ratio)
+    local width = math.ceil(vim.opt.columns:get() * width_ratio)
+    local window_config = {
+        relative = 'editor',
+        style = 'minimal',
+        border = 'rounded',
+        width = width,
+        height = height,
+        row = width / 2,
+        col = height / 2,
+    }
+    window_config = vim.tbl_extend('force', window_config, custom_window_config or {})
+
+    local bufnr = api.nvim_create_buf(false, true)
+    local winnr = api.nvim_open_win(bufnr, true, window_config)
+    return winnr, bufnr
+end
+
+M.get_system_output = function(cmd)
+    return vim.split(vim.fn.system(cmd), '\n')
 end
 
 return M
