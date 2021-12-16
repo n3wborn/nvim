93cdc8ad7ccf35bbf89d3e8d4d2fa166a22a888e
n3wborn <n3wborn@protonmail.com>
2021-12-16 11:43:53 +0100

    Settings(cmp) : Update config to latest changes

--- lua/modules/plugins/cmp.lua
+++ lua/modules/plugins/cmp.lua
@@ -46,32 +46,28 @@ cmp.setup({
         format = function(entry, vim_item)
             vim_item.kind = string.format('%s %s', cmp_kinds[vim_item.kind], vim_item.kind)
             vim_item.menu = ({
-                nvim_lsp = '[LSP]',
+                nvim_lsp = '[Lsp]',
                 luasnip = '[Snp]',
                 buffer = '[Buf]',
                 nvim_lua = '[Lua]',
                 path = '[Pth]',
                 calc = '[Clc]',
                 emoji = '[Emj]',
-                rg = '[RipGrep]',
+                rg = '[Rg]',
             })[entry.source.name]
 
             return vim_item
         end,
     },
     mapping = {
-        ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-        ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-        ['<Down>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-        ['<Up>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
+        ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+        ['<C-e>'] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
         }),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
         ['<Tab>'] = function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
@@ -91,14 +87,30 @@ cmp.setup({
             end
         end,
     },
-    sources = {
+    sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
+    }, {
+        { name = 'nvim_lua' },
+    }, {
         { name = 'buffer' },
         { name = 'path' },
-        { name = 'nvim_lua' },
         { name = 'rg' },
+    }),
+})
+
+cmp.setup.cmdline('/', {
+    sources = {
+        { name = 'buffer' },
     },
+})
+
+cmp.setup.cmdline(':', {
+    sources = cmp.config.sources({
+        { name = 'path' },
+    }, {
+        { name = 'cmdline' },
+    }),
 })
 
 -- https://github.com/windwp/nvim-autopairs
