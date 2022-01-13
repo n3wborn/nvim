836ada252f9a14486e5fd9214fed09853096d623
Stéphane P <n3wborn@protonmail.com>
2022-01-13 02:00:47 +0100

    Settings(lualine) : Refac defaults and lsp_progress settings (#105)

--- lua/modules/core/statusline.lua
+++ lua/modules/core/statusline.lua
@@ -2,17 +2,58 @@
 
 -- https://github.com/nvim-lualine/lualine.nvim
 -- https://github.com/arkav/lualine-lsp-progress
-require('lualine').setup({
+local config = {
     options = {
+        icons_enabled = true,
         theme = 'material-nvim',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        disabled_filetypes = {},
+        always_divide_middle = true,
     },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {},
+    },
+    tabline = {},
     extensions = {
         'quickfix',
         'nvim-tree',
     },
-    sections = {
-        lualine_c = {
-            'lsp_progress',
-        },
+}
+
+local function ins_left(component)
+    table.insert(config.sections.lualine_c, component)
+end
+
+ins_left({
+    'lsp_progress',
+    display_components = { 'lsp_client_name', 'spinner', { 'title', 'percentage', 'message' } },
+    separators = {
+        component = ' ',
+        progress = ' | ',
+        message = { pre = '(', post = ')' },
+        percentage = { pre = '', post = '%% ' },
+        title = { pre = '', post = ': ' },
+        lsp_client_name = { pre = '[', post = ']' },
+        spinner = { pre = '', post = '' },
+        message = { commenced = 'In Progress', completed = 'Completed' },
     },
+    display_components = { 'lsp_client_name', 'spinner', { 'title', 'percentage', 'message' } },
+    timer = { progress_enddelay = 500, spinner = 1000, lsp_client_name_enddelay = 1000 },
+    spinner_symbols = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' },
 })
+
+require('lualine').setup(config)
