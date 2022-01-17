d8cb876ab88bb022849d0fbdf3fced97c8ccd790
Stéphane P <n3wborn@pm.me>
2022-01-17 10:37:22 +0100

    Settings(lsp) : Update eslint/tsserver config (#108)
    * update eslint config
    * disable tsserver document_formatting capability

--- lua/modules/lsp/eslint.lua
+++ lua/modules/lsp/eslint.lua
@@ -7,8 +7,30 @@ local M = {
             end,
             capabilities = capabilities,
             settings = {
-                format = {
-                    enable = true,
+                codeAction = {
+                    disableRuleComment = {
+                        enable = true,
+                        location = 'separateLine',
+                    },
+                    showDocumentation = {
+                        enable = true,
+                    },
+                },
+                codeActionOnSave = {
+                    enable = false,
+                    mode = 'all',
+                },
+                format = true,
+                nodePath = '',
+                onIgnoredFiles = 'off',
+                packageManager = 'yarn',
+                quiet = false,
+                rulesCustomizations = {},
+                run = 'onType',
+                useESLintClass = false,
+                validate = 'on',
+                workingDirectory = {
+                    mode = 'location',
                 },
             },
         })
