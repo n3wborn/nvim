11cb3a95fde1472c6f418f9343e811bfc893ae1e
Stéphane P <n3wborn@pm.me>
2022-01-23 14:52:58 +0100

    Plugins(gitsigns) : Implement new on_attach settings (#109)
    gitsigns introduced a breaking change in the way it sets mappings.
    Now, mappings hare given through a function given to on_attach()
    setting

--- lua/modules/plugins/gitsigns.lua
+++ lua/modules/plugins/gitsigns.lua
@@ -1,5 +1,34 @@
 -- https://github.com/lewis6991/gitsigns.nvim
 local gitsigns = require('gitsigns')
+local u = require('utils')
+local bufnr = bufnr or vim.api.nvim_get_current_buf()
+
+local mapping = function(bufnr)
+    local gs = package.loaded.gitsigns
+    local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map('n', '<leader>hn', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
+    map('n', '<leader>hN', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
+
+    -- Actions
+    map({ 'n', 'v' }, '<leader>hs', gs.stage_hunk)
+    map({ 'n', 'v' }, '<leader>hr', gs.reset_hunk)
+    map('n', '<leader>hS', gs.stage_buffer)
+    map('n', '<leader>hu', gs.undo_stage_hunk)
+    map('n', '<leader>hR', gs.reset_buffer)
+    map('n', '<leader>hp', gs.preview_hunk)
+    -- map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+    map('n', '<leader>tb', gs.toggle_current_line_blame)
+    map('n', '<leader>hd', gs.diffthis)
+    map('n', '<leader>hD', function()
+        gs.diffthis('~')
+    end)
+end
 
 gitsigns.setup({
     signs = {
@@ -10,21 +39,6 @@ gitsigns.setup({
         changedelete = { hl = 'GitSignsChange', text = '▎' },
     },
     word_diff = true,
-    keymaps = {
-        noremap = true,
-        ['n <leader>hn'] = { expr = true, "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'" },
-        ['n <leader>hN'] = { expr = true, "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'" },
-        ['n <leader>hs'] = '<cmd>Gitsigns stage_hunk<CR>',
-        ['v <leader>hs'] = '<cmd>Gitsigns stage_hunk<CR>',
-        ['n <leader>hu'] = '<cmd>Gitsigns undo_stage_hunk<CR>',
-        ['n <leader>hS'] = '<cmd>Gitsigns stage_buffer<CR>',
-        ['n <leader>hr'] = '<cmd>Gitsigns reset_hunk<CR>',
-        ['v <leader>hr'] = '<cmd>Gitsigns reset_hunk<CR>',
-        ['n <leader>hR'] = '<cmd>Gitsigns reset_buffer<CR>',
-        ['n <leader>hd'] = '<cmd>Gitsigns diffthis<CR>',
-        ['n <leader>hp'] = '<cmd>Gitsigns preview_hunk<CR>',
-        ['n <leader>hb'] = '<cmd>Gitsigns toggle_current_line_blame<CR>',
-        ['n <leader>ht'] = '<cmd>Gitsigns refresh<CR>',
-    },
+    on_attach = mapping(),
     preview_config = { border = 'rounded' },
 })
