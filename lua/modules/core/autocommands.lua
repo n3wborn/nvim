7d76e741c2e82a828f503bd5ac5d5a8a1aa02fab
Stéphane P <n3wborn@protonmail.com>
2022-03-12 14:42:20 +0100

    Refactor(autocommands) : Use var instead of vim.api.nvim_create_autocmd (#127)

--- lua/modules/core/autocommands.lua
+++ lua/modules/core/autocommands.lua
@@ -1,45 +1,45 @@
--- autocommands
-vim.api.nvim_create_autocmd('BufEnter', {
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd('BufEnter', {
     desc = 'Do not auto comment on new line',
     command = 'set fo-=c fo-=r fo-=o',
 })
 
-vim.api.nvim_create_autocmd({ 'FileType markdown' }, {
+autocmd({ 'FileType markdown' }, {
     desc = 'Disable IndentLine for markdown files',
     command = 'let g:indentLine_enabled=0',
 })
 
--- keep windows equally resized
-vim.api.nvim_create_autocmd('VimResized', {
+autocmd('VimResized', {
     desc = 'Keep windows equally resized',
     command = 'tabdo wincmd =',
 })
 
-vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+autocmd({ 'BufNewFile', 'BufRead' }, {
     desc = 'Fix solidity filetype',
     pattern = '*.sol',
     command = 'set filetype=solidity',
 })
 
-vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+autocmd({ 'BufNewFile', 'BufRead' }, {
     desc = 'Fix twig filetype',
     pattern = '*.twig',
     command = 'set filetype=html.twig',
 })
 
-vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+autocmd({ 'BufNewFile', 'BufRead' }, {
     desc = 'Git commit messages settings',
     pattern = 'COMMIT_EDITMSG',
     command = 'set spell nonumber wrap linebreak',
 })
 
-vim.api.nvim_create_autocmd({ 'FileType' }, {
+autocmd({ 'FileType' }, {
     desc = 'Git messages settings',
     pattern = 'gitcommit',
     command = 'let b:EditorConfig_disable=1',
 })
 
-vim.api.nvim_create_autocmd('CursorHold', {
+autocmd('CursorHold', {
     desc = 'Show current line diagnostics',
     callback = function()
         vim.diagnostic.open_float(nil, { focusable = false, scope = 'cursor' })
