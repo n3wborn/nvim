82f63c1a661c968bdb8f2de8f133c3a21a7f4b96
Stéphane P <n3wborn@protonmail.com>
2022-04-10 23:29:30 +0200

    Settings(treesitter) : fix "ensure_installed" breaking change (#133)

--- lua/modules/plugins/treesitter.lua
+++ lua/modules/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 --- https://github.com/nvim-treesitter/nvim-treesitter
 
 require('nvim-treesitter.configs').setup({
-    ensure_installed = 'maintained',
+    ensure_installed = 'all',
     highlight = {
         enable = true,
     },
