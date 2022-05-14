506e6a490a70528d94594279c2af67f94d219f7f
n3wborn <n3wborn@protonmail.com>
2022-05-14 18:35:44 +0200

    Fix : Lsp augroup

--- lua/modules/lsp/init.lua
+++ lua/modules/lsp/init.lua
@@ -43,6 +43,8 @@ lsp.protocol.CompletionItemKind = {
 lsp.handlers['textDocument/signatureHelp'] = lsp.with(lsp.handlers.signature_help, border_opts)
 lsp.handlers['textDocument/hover'] = lsp.with(lsp.handlers.hover, border_opts)
 
+local lsp_augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+
 local lsp_formatting = function(bufnr)
     vim.lsp.buf.format({
         filter = function(clients)
@@ -89,9 +91,9 @@ local on_attach = function(client, bufnr)
     u.buf_map(bufnr, 'n', '<leader>lA', '<cmd>LspRangeAct<CR>')
 
     if client.supports_method('textDocument/formatting') then
-        vim.api.nvim_clear_autocmds({ group = lsp_formatting, buffer = bufnr })
+        vim.api.nvim_clear_autocmds({ group = lsp_augroup, buffer = bufnr })
         vim.api.nvim_create_autocmd('BufWritePre', {
-            group = lsp_formatting,
+            group = lsp_augroup,
             buffer = bufnr,
             callback = function()
                 lsp_formatting(bufnr)
