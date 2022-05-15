05d393efbebb83501f13dc9e54941e56c0ed0c87
Stéphane P <n3wborn@protonmail.com>
2022-05-15 17:12:14 +0200

    Settings : Remove `filetype plugin indent on` (#164)
    This was kept for no reason as this is by default in neovim

--- init.lua
+++ init.lua
@@ -42,10 +42,7 @@ o.lazyredraw = true
 o.foldlevel = 99
 o.foldmethod = 'indent'
 o.redrawtime = 3000
-vim.cmd([[
-    filetype indent plugin on
-    hi link illuminatedWord Visual
-]])
+vim.cmd('hi link illuminatedWord Visual')
 
 _G.global = {}
 
