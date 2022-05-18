510e2b8a4df95e5dc56a259eb69247330ec7f788
n3wborn <n3wborn@protonmail.com>
2022-05-18 10:21:20 +0200

    Autocommands : Add LastPosition

--- lua/modules/core/autocommands.lua
+++ lua/modules/core/autocommands.lua
@@ -61,3 +61,18 @@ autocmd('BufEnter', {
     command = 'if winnr("$") == 1 && bufname() == "NvimTree_" . tabpagenr() | quit | endif',
     group = 'UI',
 })
+
+autocmd({ 'BufReadPost' }, {
+    desc = 'return to last position known inside a buffer',
+    pattern = '*',
+    callback = function()
+        local line = vim.fn.line
+        local test_line = vim.api.nvim_buf_get_mark(0, '"')
+        local last_line = vim.api.nvim_buf_line_count(0)
+
+        if test_line[1] > 0 and test_line[1] <= last_line then
+            vim.api.nvim_win_set_cursor(0, test_line)
+        end
+    end,
+    group = vim.api.nvim_create_augroup('LastPosition', { clear = true }),
+})
