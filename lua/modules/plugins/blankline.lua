6d747cd0ca489377d933e6e6ea8b7a27fc9cc4a9
Stéphane P <n3wborn@protonmail.com>
2022-07-22 13:15:26 +0200

    Settings : Clean up indent_blankline config (#176)

--- lua/modules/plugins/blankline.lua
+++ lua/modules/plugins/blankline.lua
@@ -1,31 +1,21 @@
 -- https://github.com/lukas-reineke/indent-blankline.nvim
 
-local cmd = vim.cmd
-local g = vim.g
+vim.g.indent_blankline_char_highlight = 'LineNr'
+vim.g.indent_blankline_char = '▏'
+vim.g.indent_blankline_filetype_exclude = { 'lspinfo', 'packer', 'checkhealth', 'help', 'man', 'terminal', '' }
+vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile', 'quickfix' }
+vim.g.indent_blankline_use_treesitter = true
+vim.g.indent_blankline_max_indent_increase = 1
+vim.g.indent_blankline_show_first_indent_level = false
+vim.g.indent_blankline_show_trailing_blankline_indent = false
+vim.g.indent_blankline_show_current_context = true
 
-g.indent_blankline_char = '▏'
-
-g.indent_blankline_use_treesitter = true
-g.indent_blankline_show_current_context = true
-
-g.indent_blankline_show_first_indent_level = false
-g.indent_blankline_show_trailing_blankline_indent = false
-
-g.indent_blankline_context_patterns = {
-    'class',
-    'function',
-    'method',
-    '^if',
-    '^while',
-    '^for',
-    '^object',
-    '^table',
-    'block',
-    'arguments',
-}
-
-g.indent_blankline_filetype_exclude = { 'help', 'packer', 'terminal' }
-g.indent_blankline_buftype_exclude = { 'help', 'terminal', 'nofile' }
-g.indent_blankline_char_highlight = 'LineNr'
-g.indent_blankline_max_indent_increase = 1
-cmd([[ hi IndentBlanklineChar guifg=#aaaaaa ]])
+if vim.g.indent_blankline_show_current_context then
+    vim.cmd([[ hi IndentBlanklineChar guifg=#aaaaaa ]])
+    vim.cmd([[
+        augroup IndentBlanklineContextAutogroup
+            autocmd!
+            autocmd CursorMoved * IndentBlanklineRefresh
+        augroup END
+    ]])
+end
