a0414043caa99f103f777db91c91bbd7ca18339c
n3wborn <n3wborn@protonmail.com>
2022-07-22 13:44:09 +0200

    Fix : gitsigns mappings function

--- lua/modules/plugins/gitsigns.lua
+++ lua/modules/plugins/gitsigns.lua
@@ -1,39 +1,34 @@
 -- https://github.com/lewis6991/gitsigns.nvim
-local gitsigns = require('gitsigns')
-local buf = bufnr or vim.api.nvim_get_current_buf()
-
-local mapping = function(buf)
+local mappings = function(bufnr)
     local u = require('utils')
     local gs = package.loaded.gitsigns
 
-    -- Navigation
-    if gs.get_hunks() then
+    if vim.api.nvim_buf_get_name(bufnr) then
         u.map('n', '<leader>hn', function()
             gs.next_hunk({ preview = true, navigation_message = false })
         end)
         u.map('n', '<leader>hN', function()
             gs.prev_hunk({ preview = true, navigation_message = false })
         end)
+        -- Actions
+        u.map({ 'n', 'v' }, '<leader>hs', gs.stage_hunk)
+        u.map({ 'n', 'v' }, '<leader>hr', gs.reset_hunk)
+        u.map('n', '<leader>hS', gs.stage_buffer)
+        u.map('n', '<leader>hu', gs.undo_stage_hunk)
+        u.map('n', '<leader>hR', gs.reset_buffer)
+        u.map('n', '<leader>hp', gs.preview_hunk)
+        u.map('n', '<leader>hb', function()
+            gs.blame_line({ full = true })
+        end)
+        u.map('n', '<leader>tb', gs.toggle_current_line_blame)
+        u.map('n', '<leader>hd', gs.diffthis)
+        u.map('n', '<leader>hD', function()
+            gs.diffthis('~')
+        end)
     end
-
-    -- Actions
-    u.map({ 'n', 'v' }, '<leader>hs', gs.stage_hunk)
-    u.map({ 'n', 'v' }, '<leader>hr', gs.reset_hunk)
-    u.map('n', '<leader>hS', gs.stage_buffer)
-    u.map('n', '<leader>hu', gs.undo_stage_hunk)
-    u.map('n', '<leader>hR', gs.reset_buffer)
-    u.map('n', '<leader>hp', gs.preview_hunk)
-    u.map('n', '<leader>hb', function()
-        gs.blame_line({ full = true })
-    end)
-    u.map('n', '<leader>tb', gs.toggle_current_line_blame)
-    u.map('n', '<leader>hd', gs.diffthis)
-    u.map('n', '<leader>hD', function()
-        gs.diffthis('~')
-    end)
 end
 
-gitsigns.setup({
+require('gitsigns').setup({
     signs = {
         add = { hl = 'GitSignsAdd', text = '│ ' },
         change = { hl = 'GitSignsChange', text = '│ ' },
@@ -42,6 +37,6 @@ gitsigns.setup({
         changedelete = { hl = 'GitSignsChange', text = '│ ' },
     },
     word_diff = true,
-    on_attach = mapping(buf),
+    on_attach = mappings,
     preview_config = { border = 'rounded' },
 })
