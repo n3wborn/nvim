9a01369c71e315d9efc1b70434a91d402b91baf8
Stéphane P <n3wborn@protonmail.com>
2022-09-11 11:52:15 +0200

    Settings(blankline) : Remove useless autocmd (#183)

--- lua/modules/plugins/blankline.lua
+++ lua/modules/plugins/blankline.lua
@@ -9,13 +9,3 @@ vim.g.indent_blankline_max_indent_increase = 1
 vim.g.indent_blankline_show_first_indent_level = false
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 vim.g.indent_blankline_show_current_context = true
-
-if vim.g.indent_blankline_show_current_context then
-    vim.cmd([[ hi IndentBlanklineChar guifg=#aaaaaa ]])
-    vim.cmd([[
-        augroup IndentBlanklineContextAutogroup
-            autocmd!
-            autocmd CursorMoved * IndentBlanklineRefresh
-        augroup END
-    ]])
-end
