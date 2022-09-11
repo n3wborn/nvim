2659c54afbc95629cbcf477e80856866626b421b
n3wborn <n3wborn@protonmail.com>
2022-09-11 13:13:31 +0200

    Plugins : Add nvim-notify

--- lua/modules/plugins/nvim_notify.lua
+++ lua/modules/plugins/nvim_notify.lua
@@ -1,5 +1,15 @@
-vim.notify = require('notify')
+-- https://github.com/rcarriga/nvim-notify
+local notify_ok, vim_notify = pcall(require, 'notify')
 
-vim.notify.setup({
-    background_colour = 'CursorColumn',
-})
+if not notify_ok then
+    print('Something went wrong with nvim-notify')
+    return
+else
+    vim.notify = vim_notify
+
+    vim_notify.setup({
+        background_colour = 'CursorColumn',
+        render = 'default',
+        stages = 'fade_in_slide_out',
+    })
+end
