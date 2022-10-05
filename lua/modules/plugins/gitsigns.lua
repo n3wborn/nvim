79b8974d13370bf62729ccc491add90270913a32
n3wborn <n3wborn@protonmail.com>
2022-10-05 23:56:08 +0200

    Settings(gitsigns) : Minor config update

--- lua/modules/plugins/gitsigns.lua
+++ lua/modules/plugins/gitsigns.lua
@@ -1,6 +1,6 @@
 -- https://github.com/lewis6991/gitsigns.nvim
 local u = require('utils')
-local gitsigns_ok, gitsigns = pcall(require, 'gitsigns')
+local gitsigns_ok, gs = pcall(require, 'gitsigns')
 
 if not gitsigns_ok then
     u.notif('Plugins :', 'Something went wrong with gitsigns', vim.log.levels.WARN)
@@ -9,38 +9,57 @@ else
     local jump_hunk_opts = { preview = true, navigation_message = 'f' }
 
     local mappings = function(bufnr)
-        local u = require('utils')
-        local gs = package.loaded.gitsigns
-        bufnr = bufnr or vim.api.nvim_get_current_buf()
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
 
-        if vim.api.nvim_buf_get_name(bufnr) then
-            -- jumps
-            u.map('n', '<leader>hn', function()
+        -- Navigation
+        map('n', '<leader>hn', function()
+            if vim.wo.diff then
+                return ']c'
+            end
+            vim.schedule(function()
                 gs.next_hunk(jump_hunk_opts)
             end)
-            u.map('n', '<leader>hN', function()
+            return '<Ignore>'
+        end, {
+            expr = true,
+            desc = 'next hunk',
+        })
+
+        map('n', '<leader>hN', function()
+            if vim.wo.diff then
+                return '[c'
+            end
+            vim.schedule(function()
                 gs.prev_hunk(jump_hunk_opts)
             end)
+            return '<Ignore>'
+        end, {
+            expr = true,
+            desc = 'previous hunk',
+        })
 
-            -- actions
-            u.map({ 'n', 'v' }, '<leader>hs', gs.stage_hunk)
-            u.map({ 'n', 'v' }, '<leader>hr', gs.reset_hunk)
-            u.map('n', '<leader>hS', gs.stage_buffer)
-            u.map('n', '<leader>hu', gs.undo_stage_hunk)
-            u.map('n', '<leader>hR', gs.reset_buffer)
-            u.map('n', '<leader>hp', gs.preview_hunk)
-            u.map('n', '<leader>hb', function()
-                gs.blame_line({ full = true })
-            end)
-            u.map('n', '<leader>tb', gs.toggle_current_line_blame)
-            u.map('n', '<leader>hd', gs.diffthis)
-            u.map('n', '<leader>hD', function()
-                gs.diffthis('~')
-            end)
-        end
+        -- Actions
+        map({ 'n', 'v' }, '<leader>hs', gs.stage_hunk)
+        map({ 'n', 'v' }, '<leader>hr', gs.reset_hunk)
+        map('n', '<leader>hS', gs.stage_buffer)
+        map('n', '<leader>hu', gs.undo_stage_hunk)
+        map('n', '<leader>hR', gs.reset_buffer)
+        map('n', '<leader>hp', gs.preview_hunk)
+        map('n', '<leader>hb', function()
+            gs.blame_line({ full = true })
+        end)
+        map('n', '<leader>tb', gs.toggle_current_line_blame)
+        map('n', '<leader>hd', gs.diffthis)
+        map('n', '<leader>hD', function()
+            gs.diffthis('~')
+        end)
     end
 
-    gitsigns.setup({
+    gs.setup({
         signs = {
             add = { hl = 'GitSignsAdd', text = '│ ' },
             change = { hl = 'GitSignsChange', text = '│ ' },
