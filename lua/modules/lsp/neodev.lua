98d462b1a9d4df54bf58b26c8ce4f73c2e413dc8
Stéphane P <n3wborn@protonmail.com>
2022-10-29 15:36:43 +0200

    Settings(lsp) : add neodev (#193)

---
+++ lua/modules/lsp/neodev.lua
@@ -0,0 +1,51 @@
+local settings = {
+    Lua = {
+        diagnostics = {
+            globals = {
+                'vim',
+                'use',
+                'describe',
+                'it',
+                'assert',
+                'before_each',
+                'after_each',
+            },
+        },
+        completion = {
+            showWord = 'Disable',
+            callSnippet = 'Disable',
+            keywordSnippet = 'Disable',
+        },
+        workspace = {
+            checkThirdParty = false,
+            library = {
+                [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+            },
+        },
+    },
+}
+
+local M = {}
+
+M.setup = function(on_attach, capabilities)
+    require('neodev').setup({
+        override = function(root_dir, library)
+            if root_dir:match('dotfiles') then
+                library.enabled = true
+                library.plugins = true
+            end
+        end,
+    })
+
+    require('lspconfig').sumneko_lua.setup({
+        on_attach = on_attach,
+        settings = settings,
+        cmd = { vim.fn.expand('lua-language-server') },
+        flags = {
+            debounce_text_changes = 150,
+        },
+        capabilities = capabilities,
+    })
+end
+
+return M
