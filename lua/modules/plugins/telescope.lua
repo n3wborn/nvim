6e015cd67c1461e0abdb1416b0e21bce57e1a27b
n3wborn <n3wborn@protonmail.com>
2022-10-31 13:09:35 +0100

    Plugins : Remove telescope-github.nvim

--- lua/modules/plugins/telescope.lua
+++ lua/modules/plugins/telescope.lua
@@ -98,7 +98,6 @@ else
 
     require('telescope').load_extension('project')
     require('telescope').load_extension('lazygit')
-    require('telescope').load_extension('gh')
     require('telescope').load_extension('ui-select')
 
     u.command('Project', 'Telescope project')
@@ -106,16 +105,4 @@ else
 
     u.command('Lazygit', 'Telescope lazygit')
     u.map('n', '<space>G', '<cmd>Lazygit<CR>')
-
-    u.command('GithubIssues', 'Telescope gh issues')
-    u.map('n', '<space>gi', '<cmd>GithubIssues<CR>')
-
-    u.command('GithubPr', 'Telescope gh pull_request')
-    u.map('n', '<space>gp', '<cmd>GithubPr<CR>')
-
-    u.command('GithubGist', 'Telescope gh gist')
-    u.map('n', '<space>gg', '<cmd>GithubGist<CR>')
-
-    u.command('GithubRun', 'Telescope gh run')
-    u.map('n', '<space>gr', '<cmd>GithubRun<CR>')
 end
