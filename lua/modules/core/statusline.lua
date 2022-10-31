3bb31c6acc4957294cb014cb5774ad3ecbe70a55
Stéphane P <n3wborn@protonmail.com>
2022-10-31 14:52:55 +0100

    Settings(statusline): remove duplicates (#200)

--- lua/modules/core/statusline.lua
+++ lua/modules/core/statusline.lua
@@ -44,14 +44,12 @@ ins_left({
     separators = {
         component = ' ',
         progress = ' | ',
-        message = { pre = '(', post = ')' },
         percentage = { pre = '', post = '%% ' },
         title = { pre = '', post = ': ' },
         lsp_client_name = { pre = '[', post = ']' },
         spinner = { pre = '', post = '' },
         message = { commenced = 'In Progress', completed = 'Completed' },
     },
-    display_components = { 'lsp_client_name', 'spinner', { 'title', 'percentage', 'message' } },
     timer = { progress_enddelay = 500, spinner = 1000, lsp_client_name_enddelay = 1000 },
     spinner_symbols = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' },
 })
