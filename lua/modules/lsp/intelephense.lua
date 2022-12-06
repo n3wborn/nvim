2803db9c9c32c5bd022494937ab21ee513ddf623
n3wborn <n3wborn@protonmail.com>
2022-12-06 22:52:32 +0100

    Refac(lsp) : Unify M.setup()

--- lua/modules/lsp/intelephense.lua
+++ lua/modules/lsp/intelephense.lua
@@ -1,10 +1,10 @@
-local M = {}
-
-M.setup = function(on_attach, capabilities)
-    require('lspconfig').intelephense.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-    })
-end
+local M = {
+    setup = function(on_attach, capabilities)
+        require('lspconfig').intelephense.setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+        })
+    end,
+}
 
 return M
--- lua/modules/lsp/jsonls.lua
+++ lua/modules/lsp/jsonls.lua
@@ -1,15 +1,15 @@
-local M = {}
-
-M.setup = function(on_attach, capabilities)
-    require('lspconfig').jsonls.setup({
-        settings = {
-            json = {
-                schemas = require('schemastore').json.schemas(),
+local M = {
+    setup = function(on_attach, capabilities)
+        require('lspconfig').jsonls.setup({
+            settings = {
+                json = {
+                    schemas = require('schemastore').json.schemas(),
+                },
             },
-        },
-        on_attach = on_attach,
-        capabilities = capabilities,
-    })
-end
+            on_attach = on_attach,
+            capabilities = capabilities,
+        })
+    end,
+}
 
 return M
