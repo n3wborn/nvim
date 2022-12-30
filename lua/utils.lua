95f43c38613d13b55a7196e8ad6a9b038e53e837
n3wborn <n3wborn@users.noreply.github.com>
2022-12-30 19:11:45 +0000

    Format with stylua

--- lua/utils.lua
+++ lua/utils.lua
@@ -174,11 +174,8 @@ M.get_cwd = function()
     return uv.cwd
 end
 
-M.trim_trailing_whitespace = M.command(
-    'TrimTrailingWhitespace',
-    trim_space,
-    { nargs = '?', range = '%', addr = 'lines', preview = trim_space }
-)
+M.trim_trailing_whitespace =
+    M.command('TrimTrailingWhitespace', trim_space, { nargs = '?', range = '%', addr = 'lines', preview = trim_space })
 
 M.notif = function(title, msg, level)
     local async = require('plenary.async')
