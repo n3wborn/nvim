f32ab84cd6496e4a8e1a299eccabef638b3faa53
n3wborn <n3wborn@protonmail.com>
2023-01-04 16:41:35 +0100

    Fix(notif) : Move notify config in modules/plugins/nvim_notify.lua

--- lua/modules/plugins/nvim_notify.lua
+++ lua/modules/plugins/nvim_notify.lua
@@ -1,10 +1,20 @@
 -- https://github.com/rcarriga/nvim-notify
-local notify_ok, vim_notify = pcall(require, 'notify')
-
-if not notify_ok then
-    print('Something went wrong with nvim-notify')
-    return
-else
-    vim.notify = vim_notify
-    vim_notify.setup()
-end
+local config = {
+    timeout = 3000,
+    fps = 20,
+    background_colour = '#000000',
+    render = 'default',
+    stages = 'fade_in_slide_out',
+    max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+    end,
+    max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+    end,
+    on_open = function(win)
+        local buf = vim.api.nvim_win_get_buf(win)
+        vim.api.nvim_buf_set_option(buf, 'filetype', 'markdown')
+    end,
+}
+require('notify').setup(config)
+vim.notify = require('notify')
