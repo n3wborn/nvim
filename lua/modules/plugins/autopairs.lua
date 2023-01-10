b853cd5b5614941a873765237b4a33fb24a7ec16
Stéphane P <n3wborn@protonmail.com>
2023-01-10 18:04:39 +0100

    Refac : Improve cmp & autopairs (#223)

--- lua/modules/plugins/autopairs.lua
+++ lua/modules/plugins/autopairs.lua
@@ -1,7 +1,24 @@
--- https://github.com/windwp/nvim-autopairs
-require('nvim-autopairs').setup({
+local status_ok, npairs = pcall(require, 'nvim-autopairs')
+
+if not status_ok then
+    return
+end
+
+npairs.setup({
     check_ts = true,
-    map_cr = true,
+    ts_config = {
+        lua = { 'string', 'source' },
+        javascript = { 'string', 'template_string' },
+        java = false,
+    },
+    disable_filetype = { 'TelescopePrompt' },
 })
 
-require('nvim-autopairs.completion.cmp')
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp_status_ok, cmp = pcall(require, 'cmp')
+
+if not cmp_status_ok then
+    return
+end
+
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
--- lua/modules/plugins/cmp.lua
+++ lua/modules/plugins/cmp.lua
@@ -3,105 +3,126 @@ local u = require('utils')
 local cmp_ok, cmp = pcall(require, 'cmp')
 local luasnip_ok, luasnip = pcall(require, 'luasnip')
 
-require('luasnip.loaders.from_vscode').lazy_load()
 vim.api.nvim_command('hi LuasnipChoiceNodePassive cterm=italic')
-
 vim.opt.completeopt = 'menu,menuone,noselect'
+
+if not luasnip_ok then
+    u.notif('Plugins :', 'Something went wrong with luasnip', vim.log.levels.WARN)
+    return
+end
 
 if not cmp_ok then
     u.notif('Plugins :', 'Something went wrong with nvim-cmp', vim.log.levels.WARN)
     return
-else
-    cmp.setup({
-        snippet = {
-            expand = function(args)
-                if luasnip_ok then
-                    luasnip.lsp_expand(args.body)
-                else
-                    return
-                end
-            end,
-        },
-        formatting = {
-            format = function(entry, item)
-                local menu_map = {
-                    buffer = '[Buf]',
-                    nvim_lsp = '[LSP]',
-                    nvim_lua = '[API]',
-                    path = '[Path]',
-                    luasnip = '[Snip]',
-                    rg = '[RG]',
-                }
-
-                item.menu = menu_map[entry.source.name] or string.format('[%s]', entry.source.name)
-                item.kind = vim.lsp.protocol.CompletionItemKind[item.kind]
-
-                return item
-            end,
-        },
-        window = {
-            completion = {
-                border = 'rounded',
-                winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None',
-            },
-            documentation = {
-                border = 'rounded',
-                winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None',
-            },
-        },
-        mapping = cmp.mapping.preset.insert({
-            ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4)),
-            ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4)),
-            ['<C-Space>'] = cmp.mapping(cmp.mapping.complete()),
-            ['<C-e>'] = cmp.mapping.abort(),
-            ['<C-n>'] = {
-                c = function(fallback)
-                    local cmp = require('cmp')
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    else
-                        fallback()
-                    end
-                end,
-            },
-            ['<C-p>'] = {
-                c = function(fallback)
-                    local cmp = require('cmp')
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    else
-                        fallback()
-                    end
-                end,
-            },
-            ['<CR>'] = cmp.mapping.confirm({ select = true }),
-            ['<Tab>'] = function(fallback)
-                if cmp.visible() then
-                    cmp.select_next_item()
-                elseif luasnip.expand_or_jumpable() then
-                    luasnip.expand_or_jump()
-                else
-                    fallback()
-                end
-            end,
-            ['<S-Tab>'] = function(fallback)
-                if cmp.visible() then
-                    cmp.select_prev_item()
-                elseif luasnip.jumpable(-1) then
-                    luasnip.jump(-1)
-                else
-                    fallback()
-                end
-            end,
-        }),
-        sources = cmp.config.sources({
-            { name = 'nvim_lsp', priority = 5 },
-            { name = 'buffer', priority = 4 },
-            { name = 'rg', priority = 3 },
-            { name = 'luasnip', priority = 2 },
-            { name = 'path', priority = 1 },
-            { name = 'nvim_lsp_signature_help' },
-            { name = 'nvim_lua' },
-        }),
-    })
 end
+
+require('luasnip.loaders.from_vscode').lazy_load()
+
+local check_backspace = function()
+    local col = vim.fn.col('.') - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s')
+end
+
+local icons = {
+    Text = '',
+    Method = 'm',
+    Function = '',
+    Constructor = '',
+    Field = '',
+    Variable = '',
+    Class = '',
+    Interface = '',
+    Module = '',
+    Property = '',
+    Unit = '',
+    Value = '',
+    Enum = '',
+    Keyword = '',
+    Snippet = '',
+    Color = '',
+    File = '',
+    Reference = '',
+    Folder = '',
+    EnumMember = '',
+    Constant = '',
+    Struct = '',
+    Event = '',
+    Operator = '',
+    TypeParameter = '',
+}
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    formatting = {
+        fields = { 'kind', 'abbr', 'menu' },
+        format = function(entry, item)
+            item.kind = string.format('%s', icons[item.kind])
+            item.menu = ({
+                buffer = '[Buffer]',
+                luasnip = '[Snip]',
+                nvim_lsp = '[LSP]',
+                nvim_lua = '[API]',
+                path = '[Path]',
+                rg = '[RG]',
+            })[entry.source.name]
+            return item
+        end,
+    },
+    window = {
+        completion = {
+            border = 'rounded',
+            winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None',
+        },
+        documentation = {
+            border = 'rounded',
+            winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None',
+        },
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif check_backspace() then
+                fallback()
+            else
+                fallback()
+            end
+        end, {
+            'i',
+            's',
+        }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, {
+            'i',
+            's',
+        }),
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp', priority = 5 },
+        { name = 'buffer', priority = 4 },
+        { name = 'rg', priority = 3 },
+        { name = 'luasnip', priority = 2 },
+        { name = 'path', priority = 1 },
+        { name = 'nvim_lsp_signature_help' },
+        { name = 'nvim_lua' },
+    }),
+})
