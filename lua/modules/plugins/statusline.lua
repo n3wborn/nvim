8d988c354bec72346b38e9e506af81d5a692559b
n3wborn <n3wborn@protonmail.com>
2023-02-05 13:34:04 +0100

    Settings : Update winbar/statusbar

--- lua/modules/plugins/statusline.lua
+++ lua/modules/plugins/statusline.lua
@@ -1,65 +1,53 @@
 -- statusline
 -- https://github.com/nvim-lualine/lualine.nvim
--- https://github.com/arkav/lualine-lsp-progress
 local ok, lualine = pcall(require, 'lualine')
 local navic_ok, navic = pcall(require, 'nvim-navic')
 local u = require('utils')
 
-if not ok or not navic_ok then
+if not ok then
     u.notif('Plugins :', 'Something went wrong with lualine', vim.log.levels.WARN)
     return
-else
-    local config = {
-        options = {
-            icons_enabled = true,
-            theme = 'material-nvim',
-            component_separators = { left = '', right = '' },
-            section_separators = { left = '', right = '' },
-            disabled_filetypes = {},
-            always_divide_middle = true,
-        },
-        sections = {
-            lualine_a = { 'mode' },
-            lualine_b = { 'branch', 'diff', 'diagnostics' },
-            lualine_c = { 'filename', { navic.get_location, cond = navic.is_available } },
-            lualine_x = { 'encoding', 'fileformat', 'filetype' },
-            lualine_y = { 'progress' },
-            lualine_z = { 'location' },
-        },
-        inactive_sections = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = { 'filename' },
-            lualine_x = { 'location' },
-            lualine_y = {},
-            lualine_z = {},
-        },
-        tabline = {},
-        extensions = {
-            'quickfix',
-            'nvim-tree',
-        },
-    }
-
-    local function ins_left(component)
-        table.insert(config.sections.lualine_c, component)
-    end
-
-    ins_left({
-        'lsp_progress',
-        display_components = { 'lsp_client_name', 'spinner', { 'title', 'percentage', 'message' } },
-        separators = {
-            component = ' ',
-            progress = ' | ',
-            percentage = { pre = '', post = '%% ' },
-            title = { pre = '', post = ': ' },
-            lsp_client_name = { pre = '[', post = ']' },
-            spinner = { pre = '', post = '' },
-            message = { commenced = 'In Progress', completed = 'Completed' },
-        },
-        timer = { progress_enddelay = 500, spinner = 1000, lsp_client_name_enddelay = 1000 },
-        spinner_symbols = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' },
-    })
-
-    lualine.setup(config)
 end
+
+if not navic_ok then
+    u.notif('Plugins :', 'Something went wrong with navic', vim.log.levels.WARN)
+    return
+end
+
+lualine.setup({
+    options = {
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        disabled_filetypes = {},
+        always_divide_middle = true,
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'filetype' },
+        lualine_z = {},
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_y = {},
+        lualine_z = {},
+    },
+    tabline = {},
+    winbar = {
+        lualine_a = {},
+        lualine_b = { 'filename' },
+        lualine_c = { { navic.get_location, cond = navic.is_available } },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+    },
+    extensions = {
+        'quickfix',
+        'nvim-tree',
+    },
+})
