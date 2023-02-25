2d51e9453f7efd95d43ef935770686ea79a1e5f8
Stéphane P <n3wborn@protonmail.com>
2023-02-25 17:35:05 +0100

    Settings(statusline) : Use catpuccin-style separators (#245)

--- lua/modules/plugins/statusline.lua
+++ lua/modules/plugins/statusline.lua
@@ -18,8 +18,8 @@ lualine.setup({
     options = {
         icons_enabled = true,
         theme = 'auto',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
         disabled_filetypes = {},
         always_divide_middle = true,
     },
