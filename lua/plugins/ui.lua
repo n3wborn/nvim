fbedc644c3e403fbed72e1feb93e20ad638dd838
n3wborn <n3wborn@protonmail.com>
2023-04-01 13:20:23 +0200

    feat(lazy): add lualine

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -115,7 +115,68 @@ return {
             max_indent_increase = 1,
             show_first_indent_level = false,
             show_trailing_blankline_indent = false,
-            show_current_context = false,
+            show_current_context = true,
         },
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        event = 'VeryLazy',
+        opts = function()
+            local navic = require('nvim-navic')
+            local config = {
+                options = {
+                    icons_enabled = true,
+                    theme = 'material-nvim',
+                    component_separators = { left = '', right = '' },
+                    section_separators = { left = '', right = '' },
+                    disabled_filetypes = {},
+                    always_divide_middle = true,
+                },
+                sections = {
+                    lualine_a = { 'mode' },
+                    lualine_b = { 'branch', 'diff', 'diagnostics' },
+                    lualine_c = { 'filename', { navic.get_location, cond = navic.is_available } },
+                    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                    lualine_y = { 'progress' },
+                    lualine_z = { 'location' },
+                },
+                inactive_sections = {
+                    lualine_a = {},
+                    lualine_b = {},
+                    lualine_c = { 'filename' },
+                    lualine_x = { 'location' },
+                    lualine_y = {},
+                    lualine_z = {},
+                },
+                tabline = {},
+                extensions = {
+                    'quickfix',
+                    'nvim-tree',
+                },
+            }
+
+            local function ins_left(component)
+                table.insert(config.sections.lualine_c, component)
+            end
+
+            ins_left({
+                'lsp_progress',
+                display_components = { 'lsp_client_name', 'spinner', { 'title', 'percentage', 'message' } },
+                separators = {
+                    component = ' ',
+                    progress = ' | ',
+                    percentage = { pre = '', post = '%% ' },
+                    title = { pre = '', post = ': ' },
+                    lsp_client_name = { pre = '[', post = ']' },
+                    spinner = { pre = '', post = '' },
+                    message = { commenced = 'In Progress', completed = 'Completed' },
+                },
+                timer = { progress_enddelay = 500, spinner = 1000, lsp_client_name_enddelay = 1000 },
+                spinner_symbols = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' },
+            })
+        end,
+        config = function(_, opts)
+            require('lualine').setup(opts)
+        end,
     },
 }
