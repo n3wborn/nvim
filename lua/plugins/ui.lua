fa37d518cead2afea2b0116f1163b591804de3c5
n3wborn <n3wborn@protonmail.com>
2023-04-01 13:19:26 +0200

    feat(notify): use pre-lazy config

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -39,12 +39,19 @@ return {
         },
         opts = {
             timeout = 2000,
+            fps = 20,
+            background_colour = '#000000',
             render = 'compact',
+            stages = 'fade_in_slide_out',
             max_height = function()
                 return math.floor(vim.o.lines * 0.75)
             end,
             max_width = function()
                 return math.floor(vim.o.columns * 0.75)
+            end,
+            on_open = function(win)
+                local buf = vim.api.nvim_win_get_buf(win)
+                vim.api.nvim_buf_set_option(buf, 'filetype', 'markdown')
             end,
         },
     },
