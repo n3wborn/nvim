efd0dbaec52612655f17d755de08330fa71e394e
n3wborn <n3wborn@protonmail.com>
2023-04-01 18:35:32 +0200

    feat(lazy): add anuvyklack/windows

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -179,4 +179,18 @@ return {
             require('lualine').setup(opts)
         end,
     },
+    {
+        'anuvyklack/windows.nvim',
+        config = true,
+        dependencies = {
+            'anuvyklack/middleclass',
+            'anuvyklack/animation.nvim',
+        },
+        keys = {
+            { '<C-w>z', '<cmd>WindowsMaximize<cr>', desc = 'Maximize Windows' },
+            { '<C-w>_', '<cmd>WindowsMaximizeVertically<cr>', desc = 'Maximize Vertically' },
+            { '<C-w>|', '<cmd>WindowsMaximizeHorizontally<cr>', desc = 'Maximize Horizontaly' },
+            { '<C-w>=', '<cmd>WindowsEqualize<cr>', desc = 'Equalize Windows' },
+        },
+    },
 }
