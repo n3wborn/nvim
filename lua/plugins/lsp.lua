e2704fddcbe4eb833c404c9c7c4b3c763eb6936f
Stéphane P <n3wborn@protonmail.com>
2023-04-03 22:50:42 +0200

    fix(lazy): friendly-snippets/LuaSnip lazy loading (#253)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -23,11 +23,42 @@ return {
         end,
     },
     {
-        'rafamadriz/friendly-snippets',
-        dependencies = { 'L3MON4D3/LuaSnip' },
-        config = function()
-            require('luasnip.loaders.from_vscode').lazy_load()
-        end,
+        'L3MON4D3/LuaSnip',
+        dependencies = {
+            'rafamadriz/friendly-snippets',
+            config = function()
+                require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+        },
+        opts = {
+            history = true,
+            delete_check_events = 'TextChanged',
+        },
+        keys = {
+            {
+                '<tab>',
+                function()
+                    return require('luasnip').jumpable(1) and '<Plug>luasnip-jump-next' or '<tab>'
+                end,
+                expr = true,
+                silent = true,
+                mode = 'i',
+            },
+            {
+                '<tab>',
+                function()
+                    require('luasnip').jump(1)
+                end,
+                mode = 's',
+            },
+            {
+                '<s-tab>',
+                function()
+                    require('luasnip').jump(-1)
+                end,
+                mode = { 'i', 's' },
+            },
+        },
     },
     {
         'neovim/nvim-lspconfig',
