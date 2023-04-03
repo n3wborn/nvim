c2c5c53939f46bff723816ad5ddb2eb441a34d37
n3wborn <n3wborn@protonmail.com>
2023-04-03 22:49:36 +0200

    fix(lazy): friendly-snippets/LuaSnip lazy loading

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,7 +2,6 @@ return {
     'hrsh7th/nvim-cmp',
     dependencies = {
         'hrsh7th/cmp-nvim-lsp',
-        'L3MON4D3/LuaSnip',
         'saadparwaiz1/cmp_luasnip',
         'lukas-reineke/cmp-under-comparator',
         'hrsh7th/cmp-buffer',
