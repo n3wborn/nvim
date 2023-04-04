d171311398aca95ede82f989c26a4fe175e1950c
n3wborn <n3wborn@protonmail.com>
2023-04-04 16:16:11 +0200

    fix(autocommands): update gitcommit ft settings for local nonumber

--- lua/custom/autocommands.lua
+++ lua/custom/autocommands.lua
@@ -105,7 +105,7 @@ vim.api.nvim_create_autocmd('FileType', {
     callback = function()
         vim.opt_local.wrap = true
         vim.opt_local.spell = true
-        vim.opt_local.nonumber = true
+        vim.opt_local.number = false
         vim.opt_local.linebreak = true
     end,
 })
