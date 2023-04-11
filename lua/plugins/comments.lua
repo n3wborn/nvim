40badb94c39b21c08768ea1896c7ee68f7b71edc
n3wborn <n3wborn@protonmail.com>
2023-04-11 22:32:22 +0200

    feat(comment): set linewise/blockwise keymap as they were pre-lazy switch

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -7,16 +7,16 @@ return {
     {
         'numToStr/Comment.nvim',
         event = 'VeryLazy',
-        opts = {
-            toggler = {
-                ---Line-comment toggle keymap
-                line = '<C-_>',
-                ---Block-comment toggle keymap
-                -- block = '<C-_>',
-            },
-        },
-        config = function(_, opts)
-            require('Comment').setup(opts)
+        config = function()
+            require('Comment').setup({
+                mappings = {
+                    basic = false,
+                    extra = false,
+                },
+            })
+
+            vim.keymap.set('n', '<C-_>', '<Plug>(comment_toggle_linewise_current)')
+            vim.keymap.set('x', '<C-_>', '<Plug>(comment_toggle_blockwise_visual)')
         end,
     },
 }
