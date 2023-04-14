8d42a148bc26674cbbbd66d3bf7a6e119d85d49b
n3wborn <n3wborn@protonmail.com>
2023-04-14 15:25:53 +0200

    feat(plugins): replace nvim-autopairs with mini.pairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,27 +1,7 @@
 return {
-    'windwp/nvim-autopairs',
+    'echasnovski/mini.pairs',
     event = 'VeryLazy',
-    config = function()
-        local ok, npairs = pcall(require, 'nvim-autopairs')
-        local cmp_npairs = require('nvim-autopairs.completion.cmp')
-        local cmp_ok, cmp = pcall(require, 'cmp')
-
-        if not ok or not cmp_ok then
-            return
-        end
-
-        npairs.setup({
-            enable_check_bracket_line = false, -- Don't add pairs if it already has a close pair in the same line
-            ignored_next_char = '[%w%.]', -- will ignore alphanumeric and `.` symbol
-            check_ts = true, -- use treesitter to check for a pair.
-            ts_config = {
-                lua = { 'string', 'source' }, -- it will not add pair on that treesitter node
-                javascript = { 'string', 'template_string' },
-                java = false, -- don't check treesitter on java
-            },
-            disable_filetype = { 'TelescopePrompt' },
-        })
-
-        cmp.event:on('confirm_done', cmp_npairs.on_confirm_done({ map_char = { tex = '' } }))
+    config = function(_, opts)
+        require('mini.pairs').setup(opts)
     end,
 }
