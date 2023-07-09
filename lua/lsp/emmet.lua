1569cd3a9fc94c1dad1c6ac32380b16337393856
Stéphane P <n3wborn@protonmail.com>
2023-07-09 08:06:41 +0200

    refac(lsp): replace emmet-ls with @olrtg/emmet-language-server (#282)

--- lua/lsp/emmet.lua
+++ lua/lsp/emmet.lua
@@ -1,9 +1,8 @@
 local M = {
     setup = function(on_attach, capabilities)
-        require('lspconfig').emmet_ls.setup({
+        require('lspconfig').emmet_language_server.setup({
             capabilities = capabilities,
             on_attach = on_attach,
-            flags = lsp_flags,
         })
     end,
 }
