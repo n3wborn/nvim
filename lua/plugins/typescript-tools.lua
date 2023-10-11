cf5ca63a13681a9fa0b21c4b56f7f860b1d246c3
n3wborn <n3wborn@protonmail.com>
2023-10-11 18:03:26 +0200

    refac: add typescript-tools settings

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -1,5 +1,20 @@
 return {
     'pmizio/typescript-tools.nvim',
     dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+    settings = {
+        tsserver_plugins = {
+            '@styled/typescript-styled-plugin',
+        },
+        --- https://github.com/microsoft/TypeScript/blob/v5.0.4/src/server/protocol.ts#L3439
+        tsserver_file_preferences = {
+            includeInlayParameterNameHints = 'all',
+            includeCompletionsForModuleExports = true,
+        },
+        -- https://github.com/microsoft/TypeScript/blob/v5.0.4/src/server/protocol.ts#L3418
+        tsserver_format_options = {
+            allowIncompleteCompletions = false,
+            allowRenameOfImportPath = true,
+        },
+    },
     opts = {},
 }
