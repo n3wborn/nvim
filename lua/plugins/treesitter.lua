5efd7578797859c8571abeffc352687dbb81b0c9
Stéphane P <n3wborn@protonmail.com>
2023-10-19 07:56:40 +0200

    feat(treesitter-refactor): update settings (#303)
    * feat(treesitter-refactor): add smart_rename, navigation, highlight_definitions
    
    * chore(lazy): update lazy-lock.json

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -102,8 +102,24 @@ return {
                 },
                 autotag = { enable = true },
                 refactor = {
-                    highlight_definitions = { enable = true },
-                    clear_on_cursor_move = true,
+                    highlight_definitions = {
+                        enable = true,
+                        clear_on_cursor_move = true,
+                    },
+                    smart_rename = {
+                        enable = true,
+                        keymaps = {
+                            smart_rename = 'grr',
+                        },
+                    },
+                    navigation = {
+                        enable = true,
+                        keymaps = {
+                            goto_definition = 'gnd',
+                            list_definitions = 'gnD',
+                            list_definitions_toc = 'gO',
+                        },
+                    },
                 },
             })
         end,
