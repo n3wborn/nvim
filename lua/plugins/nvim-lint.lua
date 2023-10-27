ee9ff8f3bd34e75adeef9f03acb54ca390041d94
Stéphane P <n3wborn@protonmail.com>
2023-10-27 07:45:03 +0200

    refac(linters): use nvim-lint, remove none-ls (#306)

---
+++ lua/plugins/nvim-lint.lua
@@ -0,0 +1,28 @@
+return {
+    'mfussenegger/nvim-lint',
+    lazy = true,
+    event = { 'BufReadPre', 'BufNewFile' }, -- to disable, comment this out
+    config = function()
+        local lint = require('lint')
+
+        lint.linters_by_ft = {
+            gitcommit = { 'gitlint' },
+            javascript = { 'eslint_d' },
+            javascriptreact = { 'eslint_d' },
+            php = { 'php' },
+            typescript = { 'eslint_d' },
+            typescriptreact = { 'eslint_d' },
+        }
+
+        vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+            group = vim.api.nvim_create_augroup('lint', { clear = true }),
+            callback = function()
+                lint.try_lint()
+            end,
+        })
+
+        vim.keymap.set('n', '<leader>l', function()
+            lint.try_lint()
+        end, { desc = 'Trigger linting for current file' })
+    end,
+}
