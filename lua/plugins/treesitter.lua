84e50f0bb4dad468eda45c646392b615d9909985
n3wborn <n3wborn@protonmail.com>
2023-12-22 07:06:25 +0100

    refac(treesitter): add vimdoc

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -49,6 +49,7 @@ return {
                     'twig',
                     'typescript',
                     'vim',
+                    'vimdoc',
                     'vue',
                     'yaml',
                 },
