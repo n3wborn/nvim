8df688e9b1f36d4999847d2af823d62bdf7ef142
Stephane Pluchart <stephane.pluchart@quarks.pro>
2024-01-24 08:22:51 +0100

    feat(treesitter): add php_only

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -35,6 +35,7 @@ return {
                     'perl',
                     'php',
                     'phpdoc',
+                    'php_only',
                     'python',
                     'query',
                     'regex',
