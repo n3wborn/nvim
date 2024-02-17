620b334d0f1c854a1282aa20cf54cabd2a4ea4c5
Stephane Pluchart <stephane.pluchart@quarks.pro>
2024-02-17 12:11:05 +0100

    feat(gitsigns): add current_line_blame

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,6 +2,7 @@ return {
     'lewis6991/gitsigns.nvim',
     event = 'BufReadPre',
     opts = {
+        current_line_blame = true,
         signs = {
             add = { text = '│ ' },
             change = { text = '│ ' },
