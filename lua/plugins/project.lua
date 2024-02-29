a7fe33c4b27c15a69222e50d9acbe2609305dde4
Stephane Pluchart <stephane.pluchart@quarks.pro>
2024-02-29 11:49:52 +0100

    refactor(projects): telescope version, lazyness

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -16,9 +16,10 @@ return {
     end,
     dependencies = {
         { 'nvim-lua/plenary.nvim' },
-        { 'nvim-telescope/telescope.nvim', tag = '0.1.0' },
+        { 'nvim-telescope/telescope.nvim', tag = '0.1.5' },
         { 'Shatur/neovim-session-manager' },
     },
+    lazy = false,
     priority = 100,
     keys = {
         { '<C-p>', '<cmd>Telescope neovim-project history<CR>', desc = 'Projects history' },
