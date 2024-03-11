ae21a9203dcc443c4abd3829e78c16995befc85a
Stephane Pluchart <stephane.pluchart@quarks.pro>
2024-03-11 14:15:01 +0100

    fix(keymaps): update Before jump_to_* keymap

--- lua/plugins/before.lua
+++ lua/plugins/before.lua
@@ -4,7 +4,7 @@ return {
         local before = require('before')
         before.setup({ history_size = 42 })
 
-        vim.keymap.set('n', '<C-h>', before.jump_to_last_edit, {})
-        vim.keymap.set('n', '<C-l>', before.jump_to_next_edit, {})
+        vim.keymap.set('n', '<C-j>', before.jump_to_last_edit, {})
+        vim.keymap.set('n', '<C-k>', before.jump_to_next_edit, {})
     end,
 }
