ac9785413cf3606101b5f1d525906815fb6dd215
Stephane Pluchart <stephane.pluchart@quarks.pro>
2024-04-08 14:16:24 +0200

    feat(plugins): add diffview cmd's

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,5 +1,13 @@
 return {
     'sindrets/diffview.nvim',
+    cmd = {
+        'DiffviewOpen',
+        'DiffviewClose',
+        'DiffviewRefresh',
+        'DiffviewFileHistory',
+        'DiffviewFocusFiles',
+        'DiffviewToggleFiles',
+    },
     config = function()
         require('diffview')
     end,
