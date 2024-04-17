a95dd017eab7e94e6f4168e5c02a23d70369e7d2
n3wborn <n3wborn@protonmail.com>
2024-04-17 23:04:28 +0200

    refactor: nvim-treesiter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,43 @@
+-- heavily inspired by LazyVim/LazyVim config
 return {
     {
         'nvim-treesitter/nvim-treesitter',
+        version = false,
         build = ':TSUpdate',
         event = { 'BufReadPost', 'BufNewFile' },
         dependencies = {
-            'nvim-treesitter/nvim-treesitter-textobjects',
-            'nvim-treesitter/nvim-treesitter-refactor',
+            {
+                'nvim-treesitter/nvim-treesitter-textobjects',
+                config = function()
+                    -- When in diff mode, we want to use the default
+                    -- vim text objects c & C instead of the treesitter ones.
+                    local move = require('nvim-treesitter.textobjects.move') ---@type table<string,fun(...)>
+                    local configs = require('nvim-treesitter.configs')
+                    for name, fn in pairs(move) do
+                        if name:find('goto') == 1 then
+                            move[name] = function(q, ...)
+                                if vim.wo.diff then
+                                    local config = configs.get_module('textobjects.move')[name] ---@type table<string,string>
+                                    for key, query in pairs(config or {}) do
+                                        if q == query and key:find('[%]%[][cC]') then
+                                            vim.cmd('normal! ' .. key)
+                                            return
+                                        end
+                                    end
+                                end
+                                return fn(q, ...)
+                            end
+                        end
+                    end
+                end,
+            },
+            {
+                'nvim-treesitter/nvim-treesitter-refactor',
+            },
         },
+        cmd = { 'TSUpdateSync', 'TSUpdate', 'TSInstall' },
+        ---@type TSConfig
+        ---@diagnostic disable-next-line: missing-fields
         opts = {
             auto_install = true,
             ensure_installed = {
@@ -123,9 +154,30 @@ return {
                 },
             },
         },
+        ---@param opts TSConfig
         config = function(_, opts)
-            local ts = require('nvim-treesitter.configs')
-            ts.setup(opts)
+            if type(opts.ensure_installed) == 'table' then
+                ---@type table<string, boolean>
+                local added = {}
+                opts.ensure_installed = vim.tbl_filter(function(lang)
+                    if added[lang] then
+                        return false
+                    end
+                    added[lang] = true
+                    return true
+                end, opts.ensure_installed)
+            end
+            require('nvim-treesitter.configs').setup(opts)
         end,
+        -- config = function(_, opts)
+        --     local ts = require('nvim-treesitter.configs')
+        --     ts.setup(opts)
+        -- end,
+    },
+    {
+        'windwp/nvim-ts-autotag',
+        -- event = { 'BufReadPost', 'BufNewFile' },
+        event = 'VeryLazy',
+        opts = {},
     },
 }
