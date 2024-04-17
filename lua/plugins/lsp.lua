40f1ff5cbc13a34ace465f562494f3f18ecc0279
n3wborn <n3wborn@protonmail.com>
2024-04-17 23:06:36 +0200

    refactor: remove illuminate

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,27 +1,5 @@
 return {
     {
-        'RRethy/vim-illuminate',
-        event = { 'BufReadPost', 'BufNewFile' },
-        opts = {
-            providers = {
-                'lsp',
-                'treesitter',
-            },
-            filetypes_denylist = {
-                'nvimTree',
-                'nvim-tree',
-                'neo-tree',
-                'lazy',
-                'telescope',
-            },
-        },
-        config = function(_, opts)
-            require('illuminate').configure(opts)
-
-            vim.cmd('hi link illuminatedWord Visual')
-        end,
-    },
-    {
         'L3MON4D3/LuaSnip',
         dependencies = {
             'rafamadriz/friendly-snippets',
@@ -90,7 +68,6 @@ return {
 
             --- on_attach
             local on_attach = function(client)
-                require('illuminate').on_attach(client)
             end
 
             for _, server in ipairs({
