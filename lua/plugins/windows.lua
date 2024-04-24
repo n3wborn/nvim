90846204b885914581c0b0473cf904e226e03818
Stephane Pluchart <stephane.pluchart@quarks.pro>
2024-04-24 14:39:16 +0200

    fix: global and windows options

--- lua/plugins/windows.lua
+++ lua/plugins/windows.lua
@@ -30,8 +30,8 @@ return {
                     enable = false, -- Display colorcolumn in the foccused window only
                     list = '+1', -- Set the comma-saperated list for the colorcolumn
                 },
-                signcolumn = true, -- Display signcolumn in the focussed window only
-                winhighlight = false, -- Auto highlighting for focussed/unfocussed windows
+                signcolumn = false, -- Display signcolumn in the focussed window only
+                winhighlight = true, -- Auto highlighting for focussed/unfocussed windows
             },
         },
         config = function(_, opts)
