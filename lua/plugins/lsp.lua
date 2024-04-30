5883d9d06ee4d2e9b5f8ee5ec7870dbb3c6077ee
Stéphane P <n3wborn@pm.me>
2024-04-30 08:20:47 +0200

    refactor: remove snippets plugins (#332)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,42 +1,4 @@
 return {
-    {
-        'L3MON4D3/LuaSnip',
-        dependencies = {
-            'rafamadriz/friendly-snippets',
-            config = function()
-                require('luasnip.loaders.from_vscode').lazy_load()
-            end,
-        },
-        opts = {
-            history = true,
-            delete_check_events = 'TextChanged',
-        },
-        keys = {
-            {
-                '<tab>',
-                function()
-                    return require('luasnip').jumpable(1) and '<Plug>luasnip-jump-next' or '<tab>'
-                end,
-                expr = true,
-                silent = true,
-                mode = 'i',
-            },
-            {
-                '<tab>',
-                function()
-                    require('luasnip').jump(1)
-                end,
-                mode = 's',
-            },
-            {
-                '<s-tab>',
-                function()
-                    require('luasnip').jump(-1)
-                end,
-                mode = { 'i', 's' },
-            },
-        },
-    },
     { 'folke/neodev.nvim', opts = {} },
     {
         'SmiteshP/nvim-navbuddy',
