3e43cbecc6694bafa8c30d21831c4e8bef76ea8b
n3wborn <n3wborn@protonmail.com>
2024-05-07 00:11:26 +0200

    refactor: fzf-lua config

--- lua/plugins/fzf_lua.lua
+++ lua/plugins/fzf_lua.lua
@@ -12,9 +12,9 @@ return {
             desc = 'Find File',
         },
         {
-            '<leader>fF²',
+            '<leader>fF',
             function()
-                require('fzf-lua').file({})
+                require('fzf-lua').files()
             end,
             desc = 'Find File',
         },
@@ -120,36 +120,100 @@ return {
     opts = function()
         local actions = require('fzf-lua.actions')
         return {
+            -- https://github.com/ibhagwan/nvim-lua/blob/dc846e06d3a9e1df99840d2fc833dca1d0b6e4e1/lua/plugins/fzf-lua/setup.lua#L67
+            winopts = {
+                height = 0.25,
+                width = 0.4,
+                row = 0.5,
+                -- hl = { normal = 'Pmenu' },
+                -- border = 'none',
+            },
+            fzf_opts = {
+                ['--no-info'] = '',
+                ['--info'] = 'hidden',
+                ['--padding'] = '13%,5%,13%,5%',
+                ['--header'] = ' ',
+                ['--no-scrollbar'] = '',
+            },
             files = {
-                fzf_opts = {
-                    ['--history'] = vim.fn.stdpath('data') .. '/fzf-lua-files-history',
+                previewer = 'bat',
+                git_icons = true,
+                preview_opts = 'hidden',
+                find_opts = [[-type f -not -path '*/\.git/*' -printf '%P\n']],
+                rg_opts = [[--color=never --files --hidden --follow -g "!.git"]],
+                fd_opts = [[--color=never --type f --hidden --follow --exclude .git]],
+            },
+            buffers = {
+                formatter = 'path.filename_first',
+                preview_opts = 'hidden',
+                fzf_opts = { ['--delimiter'] = ' ', ['--with-nth'] = '-1..' },
+            },
+            git = {
+                bcommits = {
+                    cmd = "git log --color --pretty=format:'%C(yellow)%h%Creset %Cgreen%><(12)%cr%><|(12)%Creset %s' <file>",
+                    preview = "git show --stat --color --format='%C(cyan)%an%C(reset)%C(bold yellow)%d%C(reset): %s' {1} -- <file>",
+                    actions = {
+                        ['ctrl-d'] = function(...)
+                            require('fzf-lua').actions.git_buf_vsplit(...)
+                            vim.cmd('windo diffthis')
+                            local switch = vim.api.nvim_replace_termcodes('<C-w>h', true, false, true)
+                            vim.api.nvim_feedkeys(switch, 't', false)
+                        end,
+                    },
+                    preview_opts = 'nohidden',
+                    winopts = {
+                        preview = {
+                            layout = 'vertical',
+                            vertical = 'right:50%',
+                            wrap = 'wrap',
+                        },
+                        row = 1,
+                        width = vim.api.nvim_win_get_width(0),
+                        height = 0.3,
+                    },
                 },
+                branches = {
+                    cmd = 'git branch --all --color',
+                    winopts = {
+                        preview = {
+                            layout = 'vertical',
+                            vertical = 'right:50%',
+                            wrap = 'wrap',
+                        },
+                        row = 1,
+                        width = vim.api.nvim_win_get_width(0),
+                        height = 0.3,
+                    },
+                },
+            },
+            autocmds = {
+                winopts = {
+                    width = 0.8,
+                    height = 0.7,
+                },
+            },
+            grep = {
+                rg_glob = false,
+                glob_flag = '--iglob', -- for case sensitive globs use '--glob'
+                glob_separator = '%s%-%-', -- query separator pattern (lua): ' --'
+                grep_opts = '--binary-files=without-match --line-number --recursive --color=auto --perl-regexp -e',
+                rg_opts = '--column --line-number --no-heading --color=always --smart-case --max-columns=4096 -e',
             },
             actions = {
                 files = {
-                    -- instead of the default action 'actions.file_edit_or_qf'
-                    -- it's important to define all other actions here as this
-                    -- table does not get merged with the global defaults
                     ['default'] = actions.file_edit_or_qf,
+                    ['ctrl-l'] = actions.arg_add,
                     ['ctrl-x'] = actions.file_split,
                     ['ctrl-v'] = actions.file_vsplit,
                     ['ctrl-t'] = actions.file_tabedit,
-                    ['alt-q'] = actions.file_sel_to_qf,
-                    ['alt-l'] = actions.file_sel_to_ll,
-                },
-                buffers = {
-                    -- providers that inherit these actions:
-                    --   buffers, tabs, lines, blines
-                    ['default'] = actions.buf_edit,
-                    ['ctrl-x'] = actions.buf_split,
-                    ['ctrl-v'] = actions.buf_vsplit,
-                    ['ctrl-t'] = actions.buf_tabedit,
+                    ['ctrl-q'] = actions.file_sel_to_qf,
+                    ['alt-q'] = actions.file_sel_to_ll,
                 },
             },
         }
     end,
     config = function(_, opts)
         -- calling `setup` is optional for customization
-        require('fzf-lua').setup({ 'default' }, opts)
+        require('fzf-lua').setup({ 'max-perf', opts })
     end,
 }
