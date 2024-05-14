8c8c145539667241a26bf0b43ecf7094dc550078
Stephane Pluchart <stephane.pluchart@quarks.pro>
2024-05-14 19:30:15 +0200

    refactor: fzf config

--- lua/plugins/fzf_lua.lua
+++ lua/plugins/fzf_lua.lua
@@ -58,14 +58,14 @@ return {
             function()
                 require('fzf-lua').git_bcommits()
             end,
-            desc = 'Git Previous commits',
+            desc = 'Git commit log (buffer)',
         },
         {
             '<leader>gb',
             function()
                 require('fzf-lua').git_branches()
             end,
-            desc = 'Git Branch',
+            desc = 'Git Branches',
         },
         {
             '<leader>gS',
@@ -121,33 +121,26 @@ return {
         local actions = require('fzf-lua.actions')
         return {
             -- https://github.com/ibhagwan/nvim-lua/blob/dc846e06d3a9e1df99840d2fc833dca1d0b6e4e1/lua/plugins/fzf-lua/setup.lua#L67
-            winopts = {
-                height = 0.25,
-                width = 0.4,
-                row = 0.5,
-                -- hl = { normal = 'Pmenu' },
-                -- border = 'none',
-            },
-            fzf_opts = {
-                ['--no-info'] = '',
-                ['--info'] = 'hidden',
-                ['--padding'] = '13%,5%,13%,5%',
-                ['--header'] = ' ',
-                ['--no-scrollbar'] = '',
-            },
-            files = {
-                previewer = 'bat',
-                git_icons = true,
-                preview_opts = 'hidden',
-                find_opts = [[-type f -not -path '*/\.git/*' -printf '%P\n']],
-                rg_opts = [[--color=never --files --hidden --follow -g "!.git"]],
-                fd_opts = [[--color=never --type f --hidden --follow --exclude .git]],
-            },
-            buffers = {
-                formatter = 'path.filename_first',
-                preview_opts = 'hidden',
-                fzf_opts = { ['--delimiter'] = ' ', ['--with-nth'] = '-1..' },
-            },
+            -- fzf_opts = {
+            --     ['--no-info'] = '',
+            --     ['--info'] = 'hidden',
+            --     ['--padding'] = '13%,5%,13%,5%',
+            --     ['--header'] = ' ',
+            --     ['--no-scrollbar'] = '',
+            -- },
+            -- files = {
+            --     previewer = 'bat',
+            --     git_icons = true,
+            --     preview_opts = 'hidden',
+            --     find_opts = [[-type f -not -path '*/\.git/*' -printf '%P\n']],
+            --     rg_opts = [[--color=never --files --hidden --follow -g "!.git"]],
+            --     fd_opts = [[--color=never --type f --hidden --follow --exclude .git]],
+            -- },
+            -- buffers = {
+            --     formatter = 'path.filename_first',
+            --     preview_opts = 'hidden',
+            --     fzf_opts = { ['--delimiter'] = ' ', ['--with-nth'] = '-1..' },
+            -- },
             git = {
                 bcommits = {
                     cmd = "git log --color --pretty=format:'%C(yellow)%h%Creset %Cgreen%><(12)%cr%><|(12)%Creset %s' <file>",
@@ -161,44 +154,21 @@ return {
                         end,
                     },
                     preview_opts = 'nohidden',
-                    winopts = {
-                        preview = {
-                            layout = 'vertical',
-                            vertical = 'right:50%',
-                            wrap = 'wrap',
-                        },
-                        row = 1,
-                        width = vim.api.nvim_win_get_width(0),
-                        height = 0.3,
-                    },
-                },
-                branches = {
-                    cmd = 'git branch --all --color',
-                    winopts = {
-                        preview = {
-                            layout = 'vertical',
-                            vertical = 'right:50%',
-                            wrap = 'wrap',
-                        },
-                        row = 1,
-                        width = vim.api.nvim_win_get_width(0),
-                        height = 0.3,
-                    },
                 },
             },
-            autocmds = {
-                winopts = {
-                    width = 0.8,
-                    height = 0.7,
-                },
-            },
-            grep = {
-                rg_glob = false,
-                glob_flag = '--iglob', -- for case sensitive globs use '--glob'
-                glob_separator = '%s%-%-', -- query separator pattern (lua): ' --'
-                grep_opts = '--binary-files=without-match --line-number --recursive --color=auto --perl-regexp -e',
-                rg_opts = '--column --line-number --no-heading --color=always --smart-case --max-columns=4096 -e',
-            },
+            -- autocmds = {
+            --     winopts = {
+            --         width = 0.8,
+            --         height = 0.7,
+            --     },
+            -- },
+            -- grep = {
+            --     rg_glob = false,
+            --     glob_flag = '--iglob', -- for case sensitive globs use '--glob'
+            --     glob_separator = '%s%-%-', -- query separator pattern (lua): ' --'
+            --     grep_opts = '--binary-files=without-match --line-number --recursive --color=auto --perl-regexp -e',
+            --     rg_opts = '--column --line-number --no-heading --color=always --smart-case --max-columns=4096 -e',
+            -- },
             actions = {
                 files = {
                     ['default'] = actions.file_edit_or_qf,
@@ -213,7 +183,12 @@ return {
         }
     end,
     config = function(_, opts)
-        -- calling `setup` is optional for customization
-        require('fzf-lua').setup({ 'max-perf', opts })
+        -- require('fzf-lua').setup({ 'defaults', opts })
+        -- require('fzf-lua').setup({ 'fzf-native', opts })
+        -- require('fzf-lua').setup({ 'fzf-tmux', opts })
+        -- require('fzf-lua').setup({ 'fzf-vim', opts })
+        -- require('fzf-lua').setup({ 'max-perf', opts })
+        -- require('fzf-lua').setup({ 'telescope', opts })
+        require('fzf-lua').setup({ 'skim', opts })
     end,
 }
