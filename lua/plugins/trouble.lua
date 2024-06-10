074277c906c9a93d800fe3683c3d4cd17012f26d
Stéphane P <n3wborn@pm.me>
2024-06-10 20:19:48 +0200

    refactor(trouble): fix version and add keymaps (#366)

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,7 +1,39 @@
 return {
     'folke/trouble.nvim',
     cmd = { 'Trouble' },
-    branch = false,
+    version = false,
+    keys = {
+        {
+            '<leader>xx',
+            '<cmd>Trouble diagnostics toggle<cr>',
+            desc = 'Diagnostics (Trouble)',
+        },
+        {
+            '<leader>xX',
+            '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+            desc = 'Buffer Diagnostics (Trouble)',
+        },
+        {
+            '<leader>cs',
+            '<cmd>Trouble symbols toggle focus=false<cr>',
+            desc = 'Symbols (Trouble)',
+        },
+        {
+            '<leader>cl',
+            '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+            desc = 'LSP Definitions / references / ... (Trouble)',
+        },
+        {
+            '<leader>xL',
+            '<cmd>Trouble loclist toggle<cr>',
+            desc = 'Location List (Trouble)',
+        },
+        {
+            '<leader>xQ',
+            '<cmd>Trouble qflist toggle<cr>',
+            desc = 'Quickfix List (Trouble)',
+        },
+    },
     opts = {
         auto_close = false, -- auto close when there are no items
         auto_open = false, -- auto open when there are items
