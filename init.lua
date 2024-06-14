00c0c01f4231b6825c8ea285d7bb606d6aed2b32
Stéphane P <n3wborn@pm.me>
2024-06-14 08:29:05 +0200

    feat: move lazy setup in init.lua (#371)

--- init.lua
+++ init.lua
@@ -4,7 +4,35 @@ _G.global = {}
 _G.global.float_border_opts = { border = 'rounded', focusable = false, scope = 'line' }
 
 require('custom')
-require('lazy').setup('plugins')
+require('lazy').setup({
+    spec = {
+        { import = 'plugins' },
+    },
+    defaults = {
+        lazy = true,
+        version = false,
+    },
+    install = {
+        missing = true,
+        colorscheme = { 'catpuccin' },
+    },
+    checker = { enabled = true },
+    performance = {
+        rtp = {
+            disabled_plugins = {
+                'gzip',
+                'matchit',
+                'matchparen',
+                'netrwPlugin',
+                'tarPlugin',
+                'tohtml',
+                'tutor',
+                'zipPlugin',
+            },
+        },
+    },
+    ui = { border = 'rounded' },
+})
 
 require('custom.keymaps')
 require('custom.autocommands')
