014575e7a99869ca1eb44dad2ed7fb90b389fc93
n3wborn <n3wborn@protonmail.com>
2024-06-18 22:43:10 +0200

    refactor: gitsigns keymaps

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -62,3 +62,51 @@ u.map('n', '<C-d>', '<C-d>zz', { desc = 'Center cursor after moving down half-pa
 u.map('n', '<C-u>', '<C-u>zz', { desc = 'Center cursor after moving up half-page' })
 u.map('n', '<C-f>', '<C-f>zz', { desc = 'Center cursor after moving forward page' })
 u.map('n', '<C-b>', '<C-b>zz', { desc = 'Center cursor after moving backward page' })
+
+--- previsou on_attach gitsigns mapping here
+local gitsigns = require('gitsigns')
+local jump_hunk_opts = { preview = true, navigation_message = 'f' }
+
+u.map('n', '<leader>hn', function()
+    if vim.wo.diff then
+        vim.cmd.normal({ ']c', bang = true })
+    else
+        gitsigns.nav_hunk('next', jump_hunk_opts)
+    end
+    return 'Ignore'
+end)
+
+u.map('n', '<leader>hN', function()
+    if vim.wo.diff then
+        vim.cmd.normal({ '[c', bang = true })
+    else
+        gitsigns.nav_hunk('prev', jump_hunk_opts)
+    end
+    return 'Ignore'
+end)
+
+-- Actions
+u.map('n', '<leader>hs', gitsigns.stage_hunk)
+u.map('v', '<leader>hs', function()
+    gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+end)
+u.map('v', '<leader>hr', function()
+    gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+end)
+u.map('n', '<leader>hS', gitsigns.stage_buffer)
+u.map('n', '<leader>hu', gitsigns.undo_stage_hunk)
+u.map('n', '<leader>hR', gitsigns.reset_buffer)
+u.map('n', '<leader>hr', gitsigns.reset_hunk)
+u.map('n', '<leader>hp', gitsigns.preview_hunk)
+u.map('n', '<leader>hb', function()
+    gitsigns.blame_line({ full = true })
+end)
+u.map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+u.map('n', '<leader>hd', gitsigns.diffthis)
+u.map('n', '<leader>hD', function()
+    gitsigns.diffthis('~')
+end)
+u.map('n', '<leader>td', gitsigns.toggle_deleted)
+
+-- Text object
+u.map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
