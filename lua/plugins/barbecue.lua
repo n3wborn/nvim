b25d09b1a5f4c148d4228eb56357cfc54f447b96
Stephane Pluchart <stephane.pluchart@quarks.pro>
2024-07-01 18:39:18 +0200

    fix: barbecue config

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -1,17 +1,33 @@
 return {
     'utilyre/barbecue.nvim',
+    name = 'barbecue',
+    event = { 'BufEnter' },
+    version = '*',
     dependencies = {
         'SmiteshP/nvim-navic',
         'nvim-tree/nvim-web-devicons',
     },
-    opts = {
-        attach_navic = true,
-        exclude_filetypes = {
-            'NvimTree',
-        },
-        theme = 'catpuccin-mocha',
-    },
     config = function()
-        require('barbecue').setup()
+        require('barbecue').setup({
+            attach_navic = false,
+            show_dirname = false,
+            theme = 'catpuccin-mocha',
+            create_autocmd = false,
+        })
+
+        vim.api.nvim_create_autocmd({
+            'WinResized',
+            'BufWinEnter',
+            'CursorHold',
+            'InsertLeave',
+
+            -- include this if you have set `show_modified` to `true`
+            'BufModifiedSet',
+        }, {
+            group = vim.api.nvim_create_augroup('barbecue.updater', {}),
+            callback = function()
+                require('barbecue.ui').update()
+            end,
+        })
     end,
 }
