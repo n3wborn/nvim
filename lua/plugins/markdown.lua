c2117c2630bfd5ba176f72c1e4a41896104cab6c
n3wborn <n3wborn@protonmail.com>
2024-08-06 06:15:15 +0200

    feat(plugins): replace markdown.nvim by markview

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,8 +1,9 @@
 return {
-    'MeanderingProgrammer/markdown.nvim',
-    name = 'render-markdown',
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    config = function()
-        require('render-markdown').setup()
-    end,
+    'OXY2DEV/markview.nvim',
+    lazy = false, -- (already lazy-loaded)
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter',
+
+        'nvim-tree/nvim-web-devicons',
+    },
 }
