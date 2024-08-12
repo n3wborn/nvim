817941ff2dac68a2e58e4dad06adea6b3fb54b36
Stephane Pluchart <stephane.pluchart@quarks.pro>
2024-08-12 10:45:13 +0200

    fix(ft-plugin): gitcommit buffer local editorconfig value

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -3,4 +3,4 @@ vim.opt_local.spell = true
 vim.opt_local.number = false
 vim.opt_local.relativenumber = false
 vim.opt_local.linebreak = true
-vim.opt_local.editorconfig = false
+vim.b.editorconfig = false
