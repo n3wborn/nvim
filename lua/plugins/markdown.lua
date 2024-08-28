e7af0cc7bd78636755943a2d99ba118f32721b59
Stephane Pluchart <stephane.pluchart@quarks.pro>
2024-08-28 11:35:17 +0200

    feat(markview): enable pending checbox

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -3,7 +3,20 @@ return {
     lazy = false, -- (already lazy-loaded)
     dependencies = {
         'nvim-treesitter/nvim-treesitter',
-
         'nvim-tree/nvim-web-devicons',
     },
+    opts = {
+        Checkboxes = {
+            custom = {
+                {
+                    match = '~',
+                    text = '◕',
+                    hl = 'CheckboxProgress',
+                },
+            },
+        },
+    },
+    config = function(opts)
+        require('markview').setup(opts)
+    end,
 }
