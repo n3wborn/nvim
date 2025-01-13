9ea45166eb8f6b529e82ca1544440e5c09c15429
Stephane Pluchart <stephane.pluchart@quarks.pro>
2025-01-13 16:02:31 +0100

    refactor(keymaps): comment next/prev references snacks keymaps

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -42,8 +42,8 @@ return {
         { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
         { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
         { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
-        { "*",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
-        { "#",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
+        -- { "*",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
+        -- { "#",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
     },
     init = function()
             vim.api.nvim_create_autocmd("User", {
