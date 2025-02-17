6c82c3fd6185ec1c2f8ef00e9ce7c1251a353d71
Stephane Pluchart <stephane.pluchart@quarks.pro>
2025-02-17 15:24:57 +0100

    fix(hurl): remove markdown optional deps

--- lua/plugins/hurl.lua
+++ lua/plugins/hurl.lua
@@ -4,14 +4,6 @@ return {
         'MunifTanjim/nui.nvim',
         'nvim-lua/plenary.nvim',
         'nvim-treesitter/nvim-treesitter',
-        -- Optional, for markdown rendering with render-markdown.nvim
-        {
-            'MeanderingProgrammer/render-markdown.nvim',
-            opts = {
-                file_types = { 'markdown' },
-            },
-            ft = { 'markdown' },
-        },
     },
     ft = 'hurl',
     opts = {
