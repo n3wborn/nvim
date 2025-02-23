2f3090c25fbe2ed4be6477dbc7fb9150cf11c70c
Stephane Pluchart <stephane.pluchart@quarks.pro>
2025-02-23 18:02:17 +0100

    fix(plugins): lazydev config

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -3,8 +3,23 @@ return {
         'folke/lazydev.nvim',
         ft = 'lua',
         opts = {
+            runtime = vim.env.VIMRUNTIME --[[@as string]],
             library = {
+                -- Only load luvit types when the `vim.uv` word is found
                 { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+                -- always load the LazyVim library
+                'lazy.nvim',
+                'snacks.nvim',
+            },
+            integrations = {
+                -- Fixes lspconfig's workspace management for LuaLS
+                -- Only create a new workspace if the buffer is not part
+                -- of an existing workspace or one of its libraries
+                lspconfig = true,
+                -- add the cmp source for completion of:
+                -- `require "modname"`
+                -- `---@module "modname"`
+                cmp = true,
             },
         },
     },
