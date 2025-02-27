5370b1e7aca02e4ac2c6f40a3cd4297402a05b31
Stéphane P <n3wborn@pm.me>
2025-02-27 14:14:57 +0100

    feat(autocmd): show lsp progress notification (#456)
    * feat(autocmd): add LspProgress
    
    * feat: do not show if client is lua_ls

--- lua/custom/autocommands.lua
+++ lua/custom/autocommands.lua
@@ -139,4 +139,21 @@ vim.api.nvim_create_autocmd('FileType', {
     end,
 })
 
+vim.api.nvim_create_autocmd('LspProgress', {
+    ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+    callback = function(ev)
+        local spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' }
+        if vim.lsp.get_client_by_id(ev.data.client_id).name == 'lua_ls' then
+            return
+        end
+        vim.notify(vim.lsp.status(), 'info', {
+            id = 'lsp_progress',
+            title = 'LSP Progress',
+            opts = function(notif)
+                notif.icon = ev.data.params.value.kind == 'end' and ' '
+                    or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+            end,
+        })
+    end,
+})
 vim.opt.updatetime = 100
