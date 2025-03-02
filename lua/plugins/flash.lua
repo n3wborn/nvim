e20cd95dc1888e31b41d403fdd7999740bbce1e3
Stéphane P <n3wborn@pm.me>
2025-03-02 16:06:53 +0100

    feat(plugins): add folke/flash.nvim (#464)

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,10 @@
+return {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {},
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+  },
+}
