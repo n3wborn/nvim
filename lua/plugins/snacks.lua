dee4d21132d3a3ac9e8473385036bc8cc2da23a6
Stephane Pluchart <stephane.pluchart@quarks.pro>
2025-04-29 08:08:43 +0200

    feat(keymaps): update/add snacks keymaps

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -24,13 +24,50 @@ return {
             notification = {
                 relative = 'editor',
             }
-        }
+        },
+        picker = {
+            layout = {
+                preset = "vscode",  -- default, sidebar, telescope, ivy, ivy_split, dropdown, select, vscode
+                cycle = true,
+                preview = true,
+                layout = {
+                    width = 0.7,
+                    height = 0.7,
+                }
+            },
+            previewers = {
+                file = {
+                    max_size = 1024 * 1024, -- 1MB
+                    max_line_length = 100, -- max line length
+                    ft = nil, ---@type string? filetype for highlighting. Use `nil` for auto detect
+                },
+            },
+            formatters = {
+                text = {
+                    ft = nil, ---@type string? filetype for highlighting
+                },
+                file = {
+                    filename_first = false, -- display filename before the file path
+                    truncate = 200, -- truncate the file path to (roughly) this length
+                    filename_only = false, -- only show the filename
+                    icon_width = 2, -- width of the icon (in characters)
+                    git_status_hl = true, -- use the git status highlight group for the filename
+                },
+            },
+        },
     },
+
     keys = {
+        { "<leader>sd", function() Snacks.picker.grep_word() end, desc = "Search current word" },
+        { "<leader>sp", function() Snacks.picker.grep() end, desc = "Grep" },
+        { "<leader>sP", function() Snacks.picker.git_grep() end, desc = "git grep" },
         { "<leader>ff", function() Snacks.picker.files() end, desc = "List Files" },
         { "<leader>fr", function() Snacks.picker.recent() end, desc = "List Recent Files" },
-        { "<space>s", function() Snacks.picker.smart() end, desc = "Smart Picker" },
+        { "<space>S", function() Snacks.picker.smart({ multi = { "recent", "files" },  matcher = {cwd_bonus = false, frecency = true, sort_empty = true} }) end, desc = "Smart Picker" },
         { "gb",         function() Snacks.picker.buffers() end, desc = "List Buffers" },
+        { "<leader>gj", function() Snacks.picker.jumps() end, desc = "List Jumps" },
+        { "<leader>gm", function() Snacks.picker.marks() end, desc = "List Marks" },
+        { '<leader>gr', function() Snacks.picker.registers() end, desc = "List Registers" },
         { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
         { "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
         { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
@@ -40,14 +77,11 @@ return {
         { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
         { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse" },
         { "<leader>gb", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
-        { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
+        { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
         { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
         { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
+        { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
         { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
-        { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
-        { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
-        -- { "*",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
-        -- { "#",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
     },
     init = function()
             vim.api.nvim_create_autocmd("User", {
