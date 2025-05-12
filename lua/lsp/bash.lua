4525f2bb75547553ada907ae96bf81b5904732cd
n3wborn <n3wborn@protonmail.com>
2025-05-12 21:52:13 +0200

    refactor(lsp): move config from after/ftplugin to lsp/

---
+++ lua/lsp/bash.lua
@@ -0,0 +1,18 @@
+return {
+    name = 'bash_language_server',
+    cmd = { 'bash-language-server', 'start' },
+    settings = {
+        bashIde = {
+            -- Glob pattern for finding and parsing shell script files in the workspace.
+            -- Used by the background analysis features across files.
+
+            -- Prevent recursive scanning which will cause issues when opening a file
+            -- directly in the home directory (e.g. ~/foo.sh).
+            --
+            -- Default upstream pattern is "**/*@(.sh|.inc|.bash|.command)".
+            globPattern = vim.env.GLOB_PATTERN or '*@(.sh|.inc|.bash|.command)',
+        },
+    },
+    filetypes = { 'bash', 'sh' },
+    root_markers = { '.git' },
+}
