4fc9bef5519e5c53d533373ee810186bf60d103b
Stéphane P <n3wborn@pm.me>
2025-06-26 12:18:31 +0200

    fix: markview check_rtp message (#486)

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -5,4 +5,9 @@ return {
         'nvim-treesitter/nvim-treesitter',
         'nvim-tree/nvim-web-devicons',
     },
+    opts = {
+        experimental = {
+            check_rtp_message = false,
+        },
+    },
 }
