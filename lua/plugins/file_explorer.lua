9ebe074967f65c5c45c0167359b2bf537417ad38
Stephane Pluchart <stephane.pluchart@quarks.pro>
2025-08-07 13:45:25 +0200

    fix(plugins): add 'stevearc/oil.nvim', update A7Lavinraj/fyler.nvim

--- lua/plugins/file_explorer.lua
+++ lua/plugins/file_explorer.lua
@@ -1,13 +1,44 @@
 return {
+
+    {
+        'stevearc/oil.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        cmd = 'Oil',
+        event = 'VeryLazy',
+        config = function()
+            require('oil').setup({
+                columns = { 'icon' },
+                keymaps = {
+                    ['<C-h>'] = false,
+                    ['<M-h>'] = 'actions.select_split',
+                },
+                view_options = {
+                    show_hidden = true,
+                },
+            })
+
+            -- Open parent directory in current window
+            vim.keymap.set('n', '-', require('oil').open, { desc = 'Open parent directory' })
+
+            -- Open parent directory in floating window
+            vim.keymap.set('n', '<space>-', require('oil').toggle_float)
+        end,
+    },
     {
         'A7Lavinraj/fyler.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' },
-        cmd = { 'Fyler' },
         opts = {
             icon_provider = 'nvim-web-devicons',
         },
         keys = {
-            { '<leader>e', '<cmd>Fyler open<cr>', desc = 'Fyler' },
+            {
+                '<leader>e',
+                function()
+                    local fyler = require('fyler')
+                    fyler.open()
+                end,
+                desc = 'Fyler',
+            },
         },
     },
 }
