8bb57dcaefa17e33741368e299fac16bfdffcad8
n3wborn <n3wborn@protonmail.com>
2025-08-07 19:28:46 +0200

    feat(aucmd): Close all non-existing buffers on FocusGained

--- lua/custom/autocommands.lua
+++ lua/custom/autocommands.lua
@@ -137,3 +137,47 @@ vim.api.nvim_create_autocmd({ 'LspAttach', 'LspDetach', 'DiagnosticChanged' }, {
     end),
     desc = 'Update statusline/winbar',
 })
+
+vim.api.nvim_create_autocmd('FocusGained', {
+    desc = 'User: Close all non-existing buffers on `FocusGained`.',
+    callback = function()
+        local closedBuffers = {}
+        local allBufs = vim.fn.getbufinfo({ buflisted = 1 })
+        vim.iter(allBufs):each(function(buf)
+            if not vim.api.nvim_buf_is_valid(buf.bufnr) then
+                return
+            end
+            local stillExists = vim.uv.fs_stat(buf.name) ~= nil
+            local specialBuffer = vim.bo[buf.bufnr].buftype ~= ''
+            local newBuffer = buf.name == ''
+            if stillExists or specialBuffer or newBuffer then
+                return
+            end
+            table.insert(closedBuffers, vim.fs.basename(buf.name))
+            vim.api.nvim_buf_delete(buf.bufnr, { force = false })
+        end)
+        if #closedBuffers == 0 then
+            return
+        end
+
+        if #closedBuffers == 1 then
+            vim.notify(closedBuffers[1], nil, { title = 'Buffer closed', icon = '󰅗' })
+        else
+            local text = '- ' .. table.concat(closedBuffers, '\n- ')
+            vim.notify(text, nil, { title = 'Buffers closed', icon = '󰅗' })
+        end
+
+        -- If ending up in empty buffer, re-open the first oldfile that exists
+        vim.schedule(function()
+            if vim.api.nvim_buf_get_name(0) ~= '' then
+                return
+            end
+            for _, file in ipairs(vim.v.oldfiles) do
+                if vim.uv.fs_stat(file) and vim.fs.basename(file) ~= 'COMMIT_EDITMSG' then
+                    vim.cmd.edit(file)
+                    return
+                end
+            end
+        end)
+    end,
+})
