164dccf140a8cb7ad94e5894916f39e555eede29
Stephane Pluchart <stephane.pluchart@quarks.pro>
2025-08-07 13:39:48 +0200

    feat(plugins): add dmmulroy/ts-error-translator.nvim

---
+++ lua/plugins/typescript.lua
@@ -0,0 +1,29 @@
+return {
+    {
+        'dmmulroy/ts-error-translator.nvim',
+    },
+    {
+        'pmizio/typescript-tools.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+        config = function()
+            require('typescript-tools').setup({
+                settings = {
+                    jsx_close_tag = { enable = true },
+                    tsserver_plugins = {
+                        '@styled/typescript-styled-plugin',
+                    },
+                    --- https://github.com/microsoft/TypeScript/blob/v5.0.4/src/server/protocol.ts#L3439
+                    tsserver_file_preferences = {
+                        includeInlayParameterNameHints = 'all',
+                        includeCompletionsForModuleExports = true,
+                    },
+                    -- https://github.com/microsoft/TypeScript/blob/v5.0.4/src/server/protocol.ts#L3418
+                    tsserver_format_options = {
+                        allowIncompleteCompletions = false,
+                        allowRenameOfImportPath = true,
+                    },
+                },
+            })
+        end,
+    },
+}
