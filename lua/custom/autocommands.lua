fa8f9c8ee76c8f59c2dd72dd35c5180a04b87ef5
n3wborn <n3wborn@protonmail.com>
2025-08-09 09:14:57 +0200

    feat(aucmd): add autosave

--- lua/custom/autocommands.lua
+++ lua/custom/autocommands.lua
@@ -188,3 +188,35 @@ vim.api.nvim_create_autocmd('FocusGained', {
         end)
     end,
 })
+
+-- https://github.com/chrisgrieser/.config/blob/main/nvim/lua/config/autocmds.lua#L84
+vim.api.nvim_create_autocmd({ 'InsertLeave', 'TextChanged', 'BufLeave', 'FocusLost' }, {
+    desc = 'User: Auto-save',
+    callback = function(ctx)
+        local saveInstantly = ctx.event == 'FocusLost' or ctx.event == 'BufLeave'
+        local bufnr = ctx.buf
+        local bo, b = vim.bo[bufnr], vim.b[bufnr]
+        local bufname = ctx.file
+        if bo.buftype ~= '' or bo.ft == 'gitcommit' or bo.readonly then
+            return
+        end
+        if b.saveQueued and not saveInstantly then
+            return
+        end
+
+        b.saveQueued = true
+        vim.defer_fn(function()
+            if not vim.api.nvim_buf_is_valid(bufnr) then
+                return
+            end
+
+            vim.api.nvim_buf_call(bufnr, function()
+                -- saving with explicit name prevents issues when changing `cwd`
+                -- `:update!` suppresses "The file has been changed since reading it!!!"
+                local vimCmd = ('silent! noautocmd lockmarks update! %q'):format(bufname)
+                vim.cmd(vimCmd)
+            end)
+            b.saveQueued = false
+        end, saveInstantly and 0 or 2000)
+    end,
+})
