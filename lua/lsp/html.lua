e3f743a2e6edb13e5fb7a6bd8b0e7619142708d8
n3wborn <n3wborn@protonmail.com>
2025-08-29 10:38:39 +0200

    refactor(lsp): html

--- lua/lsp/html.lua
+++ lua/lsp/html.lua
@@ -2,6 +2,14 @@
 ---@type vim.lsp.Config
 return {
     cmd = { 'vscode-html-language-server', '--stdio' },
-    filetypes = { 'html' },
-    embeddedLanguages = { css = true, javascript = true },
+    filetypes = { 'html', 'templ' },
+    init_options = {
+        configurationSection = { 'html', 'css', 'javascript' },
+        embeddedLanguages = {
+            css = true,
+            javascript = true,
+        },
+        provideFormatter = false,
+    },
+    settings = {},
 }
