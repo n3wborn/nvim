343d2cdc7a781634f031a759ce7c36d9c3c8d6ef
n3wborn <n3wborn@protonmail.com>
2025-08-29 11:01:52 +0200

    refactor(cssls): update config

--- lua/lsp/cssls.lua
+++ lua/lsp/cssls.lua
@@ -3,12 +3,19 @@ return {
     cmd = { 'vscode-css-language-server', '--stdio' },
     filetypes = { 'css', 'scss', 'less' },
     init_options = { provideFormatter = false },
+    root_markers = { 'package.json', '.git' },
     settings = {
         css = {
             validate = true,
             vendorPrefix = 'ignore',
             duplicateProperties = 'warning',
             zeroUnits = 'warning',
+        },
+        less = {
+            validate = true,
+        },
+        scss = {
+            validate = true,
         },
     },
 }
