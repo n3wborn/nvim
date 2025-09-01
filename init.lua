8b1f9649f16df91799fd90bea46dc11b7d3565c4
n3wborn <n3wborn@protonmail.com>
2025-09-01 21:07:44 +0200

    fix(lsp): mv intelephense into after/lsp/

--- init.lua
+++ init.lua
@@ -131,22 +131,6 @@ vim.lsp.config('*', {
 })
 
 --- @type vim.lsp.Config
-vim.lsp.config('intelephense', {
-    cmd = { 'intelephense', '--stdio' },
-    filetypes = { 'php' },
-    root_markers = { '.git', 'composer.json' },
-    init_options = {
-        licenceKey = vim.env.INTELEPHENSE_LICENSE_KEY,
-    },
-    settings = {
-        intelephense = {
-            files = {
-                maxSize = 10485760, -- 10Mo
-                associations = { '*.php', '*.phtml' },
-            },
-        },
-    },
-})
 
 vim.lsp.config('lua_la', {
     cmd = { 'lua-language-server' },
