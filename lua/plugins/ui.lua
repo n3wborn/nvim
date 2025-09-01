da2394133d6ce21e48502661207e1a24adf09757
n3wborn <n3wborn@protonmail.com>
2025-09-01 23:22:37 +0200

    feat(ui): add incline and bufferline

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -42,6 +42,48 @@ return {
             vim.cmd.colorscheme('catppuccin')
         end,
     },
+
+    -- filename
+    {
+        'b0o/incline.nvim',
+        dependencies = { 'catppuccin/nvim', 'nvim-tree/nvim-web-devicons' },
+        event = 'BufReadPre',
+        priority = 1200,
+        config = function()
+            require('incline').setup({
+                window = { margin = { vertical = 0, horizontal = 1 } },
+                hide = {
+                    cursorline = true,
+                },
+                render = function(props)
+                    local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+                    if vim.bo[props.buf].modified then
+                        filename = '[+] ' .. filename
+                    end
+
+                    local icon, color = require('nvim-web-devicons').get_icon_color(filename)
+                    return { { icon, guifg = color }, { ' ' }, { filename } }
+                end,
+            })
+        end,
+    },
+    {
+        'akinsho/bufferline.nvim',
+        event = 'VeryLazy',
+        keys = {
+            { '<Tab>', '<Cmd>BufferLineCycleNext<CR>', desc = 'Next tab' },
+            { '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', desc = 'Prev tab' },
+        },
+        opts = {
+            options = {
+                mode = 'tabs',
+                -- separator_style = "slant",
+                show_buffer_close_icons = false,
+                show_close_icon = false,
+            },
+        },
+    },
+
     ---@type LazyPluginSpec
     {
         'nvim-lualine/lualine.nvim',
@@ -71,17 +113,13 @@ return {
                 inactive_sections = {
                     lualine_a = {},
                     lualine_b = {},
-                    -- lualine_c = { 'filename' },
-                    lualine_x = { 'location' },
+                    lualine_c = {},
+                    lualine_x = {},
                     lualine_y = {},
                     lualine_z = {},
                 },
                 tabline = {},
             }
-            return config
-        end,
-        config = function(_, opts)
-            require('lualine').setup(opts)
         end,
     },
     ---@type LazyPluginSpec
