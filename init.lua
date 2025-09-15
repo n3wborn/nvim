3afc2330f1cc5e982be41063fd1b6a42a6a715a7
Stephane Pluchart <stephane.pluchart@quarks.pro>
2025-09-15 13:35:47 +0200

    Revert "feat: add project  management"
    This reverts commit 249394df5987a307db11fbd5c196555a2404decb.

--- init.lua
+++ init.lua
@@ -65,6 +65,17 @@ require('lazy').setup({
             opts = { lsp = { auto_attach = true } },
         },
         {
+            'folke/persistence.nvim',
+            event = 'BufReadPre', -- this will only start session saving when an actual file was opened
+            opts = {
+                dir = vim.fn.stdpath('state') .. '/sessions/', -- directory where session files are saved
+                -- minimum number of file buffers that need to be open to save
+                -- Set to 0 to always save
+                need = 1,
+                branch = true, -- use git branch to save session
+            },
+        },
+        {
             'kylechui/nvim-surround',
             version = '*',
             event = 'VeryLazy',
