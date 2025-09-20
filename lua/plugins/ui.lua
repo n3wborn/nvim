042f435beed4c4553a3547caaa19a9084e64632d
n3wborn <n3wborn@protonmail.com>
2025-09-20 17:09:55 +0200

    feat(ui): update lualine

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -70,6 +70,7 @@ return {
     ---@type LazyPluginSpec
     {
         'nvim-lualine/lualine.nvim',
+
         dependencies = {
             'nvim-tree/nvim-web-devicons',
         },
@@ -78,20 +79,69 @@ return {
             options = {
                 icons_enabled = true,
                 theme = 'catppuccin',
+                globalstatus = vim.o.laststatus == 3,
+                disabled_filetypes = { statusline = { 'dashboard', 'alpha', 'ministarter', 'snacks_dashboard' } },
                 component_separators = { left = '', right = '' },
                 section_separators = { left = '', right = '' },
-                disabled_filetypes = {},
                 always_divide_middle = true,
             },
             sections = {
                 lualine_a = { 'mode' },
-                lualine_b = { 'branch', 'diff', 'diagnostics' },
+                lualine_b = { 'branch' },
                 lualine_c = { 'filename' },
-                lualine_x = { 'lsp_status' },
-                lualine_y = { 'encoding', 'fileformat', 'filetype' },
-                lualine_z = {},
+                lualine_x = {
+                    require('snacks').profiler.status(),
+                    -- stylua: ignore
+                    {
+                        function() return require("noice").api.status.command.get() end,
+                        cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+                        color = function() return { fg = require('snacks').util.color("Statement") } end,
+                    },
+                    -- stylua: ignore
+                    {
+                        function() return require("noice").api.status.mode.get() end,
+                        cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+                        color = function() return { fg = require('snacks').util.color("Constant") } end,
+                    },
+                    -- stylua: ignore
+                    {
+                        function() return "  " .. require("dap").status() end,
+                        cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+                        color = function() return { fg = require('snacks').util.color("Debug") } end,
+                    },
+                    -- stylua: ignore
+                    {
+                        require("lazy.status").updates,
+                        cond = require("lazy.status").has_updates,
+                        color = function() return { fg = require('snacks').util.color("Special") } end,
+                    },
+                    {
+                        'diff',
+                        source = function()
+                            local gitsigns = vim.b.gitsigns_status_dict
+
+                            symbols = {
+                                added = ' ',
+                                modified = ' ',
+                                removed = ' ',
+                            }
+
+                            if gitsigns then
+                                return {
+                                    added = gitsigns.added,
+                                    modified = gitsigns.changed,
+                                    removed = gitsigns.removed,
+                                }
+                            end
+                        end,
+                    },
+                },
+                lualine_y = {
+                    { 'progress', separator = ' ', padding = { left = 1, right = 1 } },
+                },
+                lualine_z = { 'encoding', 'fileformat', 'filetype' },
             },
-            inactive_sections = {},
+            extensions = { 'neo-tree', 'lazy', 'fzf' },
         },
     },
     ---@type LazyPluginSpec
