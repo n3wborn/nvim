e5d577e3fdafc05cf6358986cf185b1e38304938
Stephane Pluchart <stephane.pluchart@quarks.pro>
2025-09-21 17:12:13 +0200

    chore(lsp): remove emmylua_ls comments

--- after/lsp/emmylua_ls.lua
+++ after/lsp/emmylua_ls.lua
@@ -24,27 +24,3 @@ return {
         },
     },
 }
-
--- require('lspconfig.configs').emmylua_ls = {
--- name = 'emmylua_ls',
--- default_config = {
---     cmd = { 'emmylua_ls' },
---     filetypes = { 'lua' },
---     root_dir = require('lspconfig.util').find_git_ancestor,
---     single_file_support = true,
---     settings = {
---         Lua = {
---             runtime = {
---                 version = "LuaJIT",
---                 requirePattern = {
---                     "lua/?.lua",
---                     "lua/?/init.lua",
---                 }
---             },
---             workspace = {
---                 library = get_workspace_libraries(),
---             },
---         }
---     }
--- }
---   }
