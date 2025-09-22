a009247a5cb0725b5f787adea57960f45e7838c8
Stephane Pluchart <stephane.pluchart@quarks.pro>
2025-09-22 09:06:27 +0200

    refactor(git): git-conflict

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -28,23 +28,7 @@ return {
         end,
     },
     ---@type LazyPluginSpec
-    {
-        'akinsho/git-conflict.nvim',
-        event = 'BufReadPre',
-        opts = {
-            disable_diagnostics = true,
-            --[[
-            MAPPINGS
-
-            co — choose ours
-            ct — choose theirs
-            cb — choose both
-            c0 — choose none
-            ]x — move to previous conflict
-            [x — move to next conflict
-        ]]
-        },
-    },
+    { 'akinsho/git-conflict.nvim', version = '*', config = true },
     ---@type LazyPluginSpec
     {
         'lewis6991/gitsigns.nvim',
