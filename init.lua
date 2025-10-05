5a9318fc318c6defaddd9786e152c0643da48abf
n3wborn <n3wborn@protonmail.com>
2025-10-05 18:19:45 +0200

    refactor(textobjects): add mini.ai and move mini.* in onefile

--- init.lua
+++ init.lua
@@ -53,32 +53,10 @@ require('lazy').setup({
             event = { 'BufReadPre', 'BufNewFile' },
         },
         {
-            'kylechui/nvim-surround',
-            version = '*',
-            event = 'VeryLazy',
-            config = function()
-                require('nvim-surround').setup()
-            end,
-        },
-        {
-            {
-                'nvim-mini/mini.comment',
-                event = 'VeryLazy',
-                opts = {
-                    options = {
-                        custom_commentstring = function()
-                            return require('ts_context_commentstring.internal').calculate_commentstring()
-                                or vim.bo.commentstring
-                        end,
-                    },
-                },
-            },
-            {
-                'JoosepAlviste/nvim-ts-context-commentstring',
-                lazy = true,
-                opts = {
-                    enable_autocmd = false,
-                },
+            'JoosepAlviste/nvim-ts-context-commentstring',
+            lazy = true,
+            opts = {
+                enable_autocmd = false,
             },
         },
         {
