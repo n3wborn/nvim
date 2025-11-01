6d911592d01c362db1603df733022dccebb86868
n3wborn <n3wborn@protonmail.com>
2025-11-01 14:40:14 +0100

    refactor(ui): move lualine in it's own config file

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -172,84 +172,6 @@ return {
     },
     ---@type LazyPluginSpec
     {
-        'nvim-lualine/lualine.nvim',
-        dependencies = {
-            'nvim-tree/nvim-web-devicons',
-        },
-        event = 'VeryLazy',
-        opts = {
-            options = {
-                icons_enabled = true,
-                theme = 'catppuccin',
-                globalstatus = vim.o.laststatus == 3,
-                disabled_filetypes = {
-                    statusline = { 'dashboard', 'alpha', 'ministarter', 'snacks_dashboard', 'gitcommit' },
-                },
-                component_separators = { left = '', right = '' },
-                section_separators = { left = '', right = '' },
-                always_divide_middle = true,
-            },
-            sections = {
-                lualine_a = { 'mode' },
-                lualine_b = { 'branch' },
-                lualine_c = { 'filename' },
-                lualine_x = {
-                    require('snacks').profiler.status(),
-                    -- stylua: ignore
-                    {
-                        function() return require("noice").api.status.command.get() end,
-                        cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-                        color = function() return { fg = require('snacks').util.color("Statement") } end,
-                    },
-                    -- stylua: ignore
-                    {
-                        function() return require("noice").api.status.mode.get() end,
-                        cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-                        color = function() return { fg = require('snacks').util.color("Constant") } end,
-                    },
-                    -- stylua: ignore
-                    {
-                        function() return "  " .. require("dap").status() end,
-                        cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
-                        color = function() return { fg = require('snacks').util.color("Debug") } end,
-                    },
-                    -- stylua: ignore
-                    {
-                        require("lazy.status").updates,
-                        cond = require("lazy.status").has_updates,
-                        color = function() return { fg = require('snacks').util.color("Special") } end,
-                    },
-                    {
-                        'diff',
-                        source = function()
-                            local gitsigns = vim.b.gitsigns_status_dict
-
-                            symbols = {
-                                added = ' ',
-                                modified = ' ',
-                                removed = ' ',
-                            }
-
-                            if gitsigns then
-                                return {
-                                    added = gitsigns.added,
-                                    modified = gitsigns.changed,
-                                    removed = gitsigns.removed,
-                                }
-                            end
-                        end,
-                    },
-                },
-                lualine_y = {
-                    { 'progress', separator = ' ', padding = { left = 1, right = 1 } },
-                },
-                lualine_z = { 'encoding', 'fileformat', 'filetype' },
-            },
-            extensions = { 'neo-tree', 'lazy', 'fzf' },
-        },
-    },
-    ---@type LazyPluginSpec
-    {
         'NvChad/nvim-colorizer.lua',
         event = 'BufReadPre',
         opts = {
@@ -288,12 +210,12 @@ return {
             'SmiteshP/nvim-navic',
             lazy = true,
             opts = function()
-                local icons = require('custom.icons')
+                local icons = require('custom.icons').kinds
                 return {
                     separator = ' ',
                     highlight = true,
                     depth_limit = 5,
-                    icons = icons.kinds,
+                    icons = icons,
                     lazy_update_context = true,
                 }
             end,
