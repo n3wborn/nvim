53e901f79086655ae1ff29b06f9b9b9472b31568
Stephane Pluchart <stephane.pluchart@quarks.pro>
2025-11-04 17:16:11 +0100

    feat(plugins): render_markdown

---
+++ lua/plugins/render_markdown.lua
@@ -0,0 +1,20 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    enabled = true,
+    event = 'VeryLazy',
+    opts = {
+        bullet = {
+            enabled = true,
+        },
+        checkbox = {
+            enabled = true,
+            position = 'inline',
+        },
+        html = {
+            enabled = true,
+            comment = {
+                conceal = true,
+            },
+        },
+    },
+}
