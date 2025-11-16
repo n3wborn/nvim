f269c8303b6150eed5e151a5ceb0d9fc55ebd7f0
n3wborn <n3wborn@protonmail.com>
2025-11-16 23:47:09 +0100

    feat(treesitter): add queries and update lazy config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,32 +2,11 @@
 return {
     ---@type LazyPluginSpec
     {
-
         'nvim-treesitter/nvim-treesitter',
-        version = false, -- last release is way too old and doesn't work on Windows
         build = ':TSUpdate',
-        event = { 'VeryLazy' },
-        lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-        init = function(plugin)
-            -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-            -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-            -- no longer trigger the **nvim-treesitter** module to be loaded in time.
-            -- Luckily, the only things that those plugins need are the custom queries, which we make available
-            -- during startup.
-            require('lazy.core.loader').add_to_rtp(plugin)
-            require('nvim-treesitter.query_predicates')
-        end,
-        cmd = { 'TSUpdateSync', 'TSUpdate', 'TSInstall', 'TSContext' },
-        keys = {
-            { '<c-space>', desc = 'Increment Selection' },
-            { '<bs>', desc = 'Decrement Selection', mode = 'x' },
-        },
-        ---@type TSConfig
-        ---@diagnostic disable-next-line: missing-fields
-        opts = {
-            highlight = { enable = true, additional_vim_regex_highlighting = false },
-            indent = { enable = true },
-            ensure_installed = {
+        config = function()
+            local configs = require('nvim-treesitter.configs')
+            local ensure_installed = {
                 'awk',
                 'bash',
                 'c',
@@ -52,6 +31,9 @@ return {
                 'markdown',
                 'markdown_inline',
                 'perl',
+                'php',
+                'phpdoc',
+                'php_only',
                 'python',
                 'query',
                 'regex',
@@ -70,20 +52,23 @@ return {
                 'vimdoc',
                 'vue',
                 'yaml',
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = '<C-space>',
-                    node_incremental = '<C-space>',
-                    scope_incremental = false,
-                    node_decremental = '<bs>',
+            }
+
+            configs.setup({
+                ensure_installed = ensure_installed,
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = '<C-space>',
+                        node_incremental = '<C-space>',
+                        scope_incremental = false,
+                        node_decremental = '<bs>',
+                    },
                 },
-            },
-        },
-        ---@param opts TSConfig
-        config = function(_, opts)
-            require('nvim-treesitter.configs').setup(opts)
+            })
         end,
     },
     ---@type LazyPluginSpec
