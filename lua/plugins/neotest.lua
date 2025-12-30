9251f7b8dcb5f3a3f913a9df7117fcd682aeaf23
n3wborn <n3wborn@protonmail.com>
2025-12-30 21:05:08 +0100

    refactor: move php.lua content

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,9 +1,17 @@
 return {
     'nvim-neotest/neotest',
     dependencies = {
+        'olimorris/neotest-phpunit',
         'nvim-neotest/nvim-nio',
         'nvim-lua/plenary.nvim',
         'antoinemadec/FixCursorHold.nvim',
         'nvim-treesitter/nvim-treesitter',
+    },
+    opts = {
+        adapters = {
+            ['neotest-phpunit'] = {
+                root_ignore_files = { 'tests/Pest.php' },
+            },
+        },
     },
 }
