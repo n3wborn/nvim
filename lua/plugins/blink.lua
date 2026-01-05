520c15f2ae9ef724ba33a84e0bb4464168d68ac6
Stéphane P <n3wborn@pm.me>
2026-01-06 00:29:18 +0100

    feat: improve blink (#554)
    * feat(blink): remove useless configs
    
    * feat(blink): add blink-emoji
    
    * fix(plugins): specs in bas places (#553)
    
    * feat(blink): improve providers config, keymaps, add emoji, rm useless

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,18 +3,10 @@ return {
         'saghen/blink.cmp',
         version = '*',
         -- build = "cargo build --release",
-        opts_extend = {
-            'sources.completion.enabled_providers',
-            'sources.compat',
-            'sources.default',
-        },
-        dependencies = {
-            'saghen/blink.compat',
-            optional = true, -- make optional so it's only enabled if any extras need it
-            opts = {},
-            version = not vim.g.lazyvim_blink_main and '*',
-        },
         event = { 'InsertEnter', 'CmdlineEnter' },
+        dependencies = {
+            'moyiz/blink-emoji.nvim',
+        },
         ---@module 'blink.cmp'
         ---@type blink.cmp.Config
         opts = {
@@ -23,7 +15,9 @@ return {
                     draw = {
                         columns = {
                             { 'label', 'label_description', gap = 1 },
-                            { 'kind_icon', gap = 1, 'kind' },
+                            { 'kind_icon' },
+                            { 'kind' },
+                            { 'source_name', gap = 1 },
                         },
                     },
                 },
@@ -35,6 +29,8 @@ return {
                 },
             },
             keymap = {
+                ['<Down>'] = { 'select_next', 'fallback' },
+                ['<Up>'] = { 'select_prev', 'fallback' },
                 ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
                 ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
                 ['<C-y>'] = { 'select_and_accept', 'fallback' },
@@ -57,9 +53,38 @@ return {
                     'lsp',
                     'path',
                     'buffer',
+                    'emoji',
+                    per_filetype = {
+                        lua = { inherit_defaults = true, 'lazydev' },
+                    },
                 },
                 providers = {
+                    emoji = {
+                        module = 'blink-emoji',
+                        name = 'Emoji',
+                        score_offset = 15,
+                        opts = {
+                            insert = true, -- Insert emoji (default) or complete its name
+                            ---@type string|table|fun():table
+                            trigger = function()
+                                return { ':' }
+                            end,
+                        },
+                        should_show_items = function()
+                            return vim.tbl_contains({ 'gitcommit', 'markdown' }, vim.o.filetype)
+                        end,
+                    },
+                    lsp = {
+                        min_keyword_length = 0,
+                        score_offset = 90,
+                    },
+                    lazydev = {
+                        name = 'LazyDev',
+                        module = 'lazydev.integrations.blink',
+                        score_offset = 100,
+                    },
                     buffer = {
+                        score_offset = 15,
                         opts = {
                             -- get all buffers, even ones like neo-tree
                             -- get_bufnrs = vim.api.nvim_list_bufs,
@@ -77,81 +102,6 @@ return {
             fuzzy = {
                 implementation = 'prefer_rust_with_warning',
             },
-        },
-        config = function(_, opts)
-            -- setup compat sources
-            local enabled = opts.sources.default
-            for _, source in ipairs(opts.sources.compat or {}) do
-                opts.sources.providers[source] = vim.tbl_deep_extend(
-                    'force',
-                    { name = source, module = 'blink.compat.source' },
-                    opts.sources.providers[source] or {}
-                )
-                if type(enabled) == 'table' and not vim.tbl_contains(enabled, source) then
-                    table.insert(enabled, source)
-                end
-            end
-
-            -- Unset custom prop to pass blink.cmp validation
-            opts.sources.compat = nil
-            local icons_kinds = require('config.icons').kind
-            -- check if we need to override symbol kinds
-            for _, provider in pairs(opts.sources.providers or {}) do
-                ---@cast provider blink.cmp.SourceProviderConfig|{kind?:string}
-                if provider.kind then
-                    local CompletionItemKind = require('blink.cmp.types').CompletionItemKind
-                    local kind_idx = #CompletionItemKind + 1
-
-                    CompletionItemKind[kind_idx] = provider.kind
-                    ---@diagnostic disable-next-line: no-unknown
-                    CompletionItemKind[provider.kind] = kind_idx
-
-                    ---@type fun(ctx: blink.cmp.Context, items: blink.cmp.CompletionItem[]): blink.cmp.CompletionItem[]
-                    local transform_items = provider.transform_items
-                    ---@param ctx blink.cmp.Context
-                    ---@param items blink.cmp.CompletionItem[]
-                    provider.transform_items = function(ctx, items)
-                        items = transform_items and transform_items(ctx, items) or items
-                        for _, item in ipairs(items) do
-                            item.kind = kind_idx or item.kind
-                            item.kind_icon = icons_kinds[item.kind_name] or item.kind_icon or nil
-                        end
-                        return items
-                    end
-
-                    -- Unset custom prop to pass blink.cmp validation
-                    provider.kind = nil
-                end
-            end
-
-            require('blink.cmp').setup(opts)
-        end,
-    },
-
-    -- lazydev
-    {
-        'saghen/blink.cmp',
-        opts = {
-            sources = {
-                per_filetype = {
-                    lua = { inherit_defaults = true, 'lazydev' },
-                },
-                providers = {
-                    lazydev = {
-                        name = 'LazyDev',
-                        module = 'lazydev.integrations.blink',
-                        score_offset = 100, -- show at a higher priority than lsp
-                    },
-                },
-            },
-        },
-    },
-    -- catppuccin support
-    {
-        'catppuccin',
-        optional = true,
-        opts = {
-            integrations = { blink_cmp = true },
         },
     },
 }
