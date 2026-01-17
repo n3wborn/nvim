2044163ab1614a394941bb0332bfb9aafe20752e
n3wborn <n3wborn@protonmail.com>
2026-01-17 20:09:49 +0100

    Revert "feat(plugins): add stay-center (#561)"
    This reverts commit 2492ed4f996db8f74d33254403eb51e5d919d0c5.

--- lua/plugins/stay_centered.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'arnamak/stay-centered.nvim',
-    event = 'VeryLazy',
-    config = function()
-        require('stay-centered').setup({
-            -- skip_filetypes = { 'lua', 'typescript' }
-        })
-    end,
-}
