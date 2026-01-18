e96b501091724a42f90541121e82843d5125f629
n3wborn <n3wborn@protonmail.com>
2026-01-18 16:36:54 +0100

    fix: noice config

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -136,15 +136,9 @@ return {
         dependencies = {
             'MunifTanjim/nui.nvim',
         },
+        ---@type NoiceConfig
         opts = {
-            lsp = {
-                progress = { enabled = true },
-                override = {
-                    ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-                    ['vim.lsp.util.stylize_markdown'] = true,
-                    ['cmp.entry.get_documentation'] = true,
-                },
-            },
+            messages = { enabled = true },
             presets = {
                 bottom_search = true, -- use a classic bottom cmdline for search
                 command_palette = true, -- position the cmdline and popupmenu together
