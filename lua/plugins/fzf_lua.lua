c3d1b900390feb38e531b47c87d9f8a30f72bf98
Stéphane P <n3wborn@pm.me>
2026-01-20 23:01:32 +0100

    feat(fzf_lua): improve config (#573)

--- lua/plugins/fzf_lua.lua
+++ lua/plugins/fzf_lua.lua
@@ -5,10 +5,44 @@ return {
     cmd = 'FzfLua',
     keys = {
         { '<space>F', ':FzfLua<cr>' },
+        {
+            '<leader>ss',
+            function()
+                require('fzf-lua').lsp_document_symbols()
+            end,
+            desc = 'Goto Symbol',
+        },
+        {
+            '<leader>sS',
+            function()
+                require('fzf-lua').lsp_live_workspace_symbols()
+            end,
+            desc = 'Goto Symbol (Workspace)',
+        },
     },
     opts = function()
-        local actions = require('fzf-lua.actions')
+        local fzf = require('fzf-lua')
+        local config = fzf.config
+        local actions = fzf.actions
+
+        config.defaults.keymap.fzf['ctrl-q'] = 'select-all+accept'
+        config.defaults.keymap.fzf['ctrl-u'] = 'half-page-up'
+        config.defaults.keymap.fzf['ctrl-d'] = 'half-page-down'
+        config.defaults.keymap.fzf['ctrl-x'] = 'jump'
+        config.defaults.keymap.fzf['ctrl-f'] = 'preview-page-down'
+        config.defaults.keymap.fzf['ctrl-b'] = 'preview-page-up'
+        config.defaults.keymap.builtin['<c-f>'] = 'preview-page-down'
+        config.defaults.keymap.builtin['<c-b>'] = 'preview-page-up'
+
         return {
+            lsp = {
+                symbols = {
+                    child_prefix = false,
+                },
+                code_actions = {
+                    previewer = vim.fn.executable('delta') == 1 and 'codeaction_native' or nil,
+                },
+            },
             buffers = {
                 formatter = 'path.filename_first',
             },
