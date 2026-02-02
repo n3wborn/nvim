e0f4b6cb8bdb0d9015302983c21138c51ebe60ab
n3wborn <n3wborn@protonmail.com>
2026-02-02 23:00:29 +0100

    refactor(utils): remove useless code

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,10 +1,3 @@
-local api = vim.api
-
--- global inspect fn
-_G.inspect = function(...)
-    print(vim.inspect(...))
-end
-
 local get_map_options = function(custom_options)
     local options = { noremap = true, silent = true }
     if custom_options then
@@ -32,125 +25,14 @@ M.buf_map = function(bufnr, mode, target, source, opts)
     M.map(mode, target, source, get_map_options(opts))
 end
 
-M.for_each = function(tbl, cb)
-    for _, v in ipairs(tbl) do
-        cb(v)
-    end
-end
-
 M.buf_command = function(bufnr, name, fn, opts)
-    api.nvim_buf_create_user_command(bufnr, name, fn, opts or {})
-end
-
-M.table = {
-    some = function(tbl, cb)
-        for k, v in pairs(tbl) do
-            if cb(k, v) then
-                return true
-            end
-        end
-        return false
-    end,
-}
-
-M.timer = {
-    start_time = nil,
-    start = function()
-        M.timer.start_time = vim.loop.now()
-    end,
-    stop = function()
-        print(vim.loop.now() - M.timer.start_time .. ' ms')
-        M.timer.start_time = nil
-    end,
-
-    start_nano = function()
-        M.timer.start_time = vim.loop.hrtime()
-    end,
-    stop_nano = function()
-        print(vim.loop.hrtime() - M.timer.start_time .. ' ns')
-        M.timer.start_time = nil
-    end,
-}
-
-M.command = function(name, fn, opts)
-    api.nvim_create_user_command(name, fn, opts or {})
-end
-
-M.t = function(str)
-    return vim.keycode(str)
-end
-
-M.input = function(keys, mode)
-    vim.api.nvim_feedkeys(M.t(keys), mode or 'i', true)
-end
-
-M.warn = function(msg)
-    api.nvim_echo({ { msg, 'WarningMsg' } }, true, {})
-end
-
-M.is_file = function(path)
-    if path == '' then
-        return false
-    end
-
-    local stat = vim.loop.fs_stat(path)
-    return stat and stat.type == 'file'
-end
-
-M.get_cwd = function()
-    return assert(vim.uv.cwd)
-end
-
----@param files table
----@param file_name string
-M.get_root_dir = function(files, file_name)
-    vim.fs.dirname(vim.fs.find(files or { '.git' }, {
-        upward = true,
-        path = vim.fs.dirname(file_name),
-    })[1])
+    vim.api.nvim_buf_create_user_command(bufnr, name, fn, opts or {})
 end
 
 M.yank_file_path = function()
     local file_path = vim.api.nvim_buf_get_name(0)
     vim.fn.setreg('+', file_path)
     vim.api.nvim_echo({ { 'File path copied to clipboard: ' .. file_path } }, true, {})
-end
-
--- taken from https://github.com/chrisgrieser/.config/blob/main/nvim/lua/plugins/folding-plugins.lua
-M.foldTextFormatter = function(virtText, lnum, endLnum, width, truncate)
-    local foldIcon = ''
-    local hlgroup = 'NonText'
-    local newVirtText = {}
-    local suffix = '  ' .. foldIcon .. '  ' .. tostring(endLnum - lnum)
-    local sufWidth = vim.fn.strdisplaywidth(suffix)
-    local targetWidth = width - sufWidth
-    local curWidth = 0
-    for _, chunk in ipairs(virtText) do
-        local chunkText = chunk[1]
-        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-        if targetWidth > curWidth + chunkWidth then
-            table.insert(newVirtText, chunk)
-        else
-            chunkText = truncate(chunkText, targetWidth - curWidth)
-            local hlGroup = chunk[2]
-            table.insert(newVirtText, { chunkText, hlGroup })
-            chunkWidth = vim.fn.strdisplaywidth(chunkText)
-            if curWidth + chunkWidth < targetWidth then
-                suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
-            end
-            break
-        end
-        curWidth = curWidth + chunkWidth
-    end
-    table.insert(newVirtText, { suffix, hlgroup })
-    return newVirtText
-end
-
----@param msg string
-M.notif = function(msg)
-    vim.schedule(function()
-        vim.notify(msg)
-    end)
 end
 
 return M
