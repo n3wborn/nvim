43a81c2f3ae95c3d2a6e4b7ba5e6f8494f18717f
n3wborn <n3wborn@protonmail.com>
2026-02-15 16:18:43 +0100

    feat(fzf-lua): add keymaps

--- lua/plugins/fzf_lua.lua
+++ lua/plugins/fzf_lua.lua
@@ -21,8 +21,57 @@ return {
             end,
             desc = 'Goto Symbol (Workspace)',
         },
+        {
+            '<leader>gd',
+            function()
+                require('fzf-lua').lsp_definitions()
+            end,
+            desc = 'Goto Symbol (Workspace)',
+        },
+        {
+            '<leader>gf',
+            function()
+                require('fzf-lua').lsp_finder()
+            end,
+            desc = 'LSP finder',
+        },
+        {
+            '<leader>gi',
+            function()
+                require('fzf-lua').lsp_implementations()
+            end,
+            desc = 'List Implementation',
+        },
+        {
+            'gb',
+            function()
+                require('fzf-lua').buffers({ cwd_only = true })
+            end,
+            desc = 'Search in current working Buffers',
+        },
+        {
+            'gB',
+            function()
+                require('fzf-lua').buffers({ cwd_only = false })
+            end,
+            desc = 'Search in every Buffers',
+        },
+        {
+            '<c-x><c-f>',
+            function()
+                require('fzf-lua').complete_path({
+                    file_icons = true,
+                    git_icons = true,
+                    color_icons = true,
+                    multiprocess = true,
+                    winopts = { fullscreen = false },
+                })
+            end,
+            desc = 'Complete Path',
+            mode = { 'i', 'x' },
+        },
     },
-    opts = function()
+    config = function()
         local fzf = require('fzf-lua')
         local config = fzf.config
         local actions = fzf.actions
@@ -35,39 +84,31 @@ return {
         config.defaults.keymap.fzf['ctrl-b'] = 'preview-page-up'
         config.defaults.keymap.builtin['<c-f>'] = 'preview-page-down'
         config.defaults.keymap.builtin['<c-b>'] = 'preview-page-up'
+        config.defaults.file_icons = 'mini'
 
-        return {
-            lsp = {
-                symbols = {
-                    child_prefix = false,
-                },
-                code_actions = {
-                    previewer = vim.fn.executable('delta') == 1 and 'codeaction_native' or nil,
-                },
+        fzf.setup('skim')
+
+        config.setup_opts.winopts = { fullscreen = true }
+        config.setup_opts.buffers = { formatter = 'path.filename_first' }
+        config.setup_opts.lsp = {
+            symbols = {
+                child_prefix = false,
             },
-            buffers = {
-                formatter = 'path.filename_first',
-            },
-            actions = {
-                files = {
-                    ['default'] = actions.file_edit_or_qf,
-                    ['ctrl-l'] = actions.arg_add,
-                    ['ctrl-x'] = actions.file_split,
-                    ['ctrl-v'] = actions.file_vsplit,
-                    ['ctrl-t'] = actions.file_tabedit,
-                    ['ctrl-q'] = actions.file_sel_to_qf,
-                    ['alt-q'] = actions.file_sel_to_ll,
-                },
+            code_actions = {
+                previewer = vim.fn.executable('delta') == 1 and 'codeaction_native' or nil,
             },
         }
-    end,
-    config = function(_, opts)
-        -- require('fzf-lua').setup({ 'defaults', opts })
-        -- require('fzf-lua').setup({ 'fzf-native', opts })
-        -- require('fzf-lua').setup({ 'fzf-tmux', opts })
-        -- require('fzf-lua').setup({ 'fzf-vim', opts })
-        -- require('fzf-lua').setup({ 'max-perf', opts })
-        -- require('fzf-lua').setup({ 'telescope', opts })
-        require('fzf-lua').setup({ 'skim', opts })
+
+        actions = {
+            files = {
+                ['default'] = actions.file_edit_or_qf,
+                ['ctrl-l'] = actions.arg_add,
+                ['ctrl-x'] = actions.file_split,
+                ['ctrl-v'] = actions.file_vsplit,
+                ['ctrl-t'] = actions.file_tabedit,
+                ['ctrl-q'] = actions.file_sel_to_qf,
+                ['alt-q'] = actions.file_sel_to_ll,
+            },
+        }
     end,
 }
