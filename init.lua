bd060040295d17d1ba9cb173c7bfaf803878da26
n3wborn <n3wborn@protonmail.com>
2026-02-15 16:19:22 +0100

    fix(surround): remove branch version

--- init.lua
+++ init.lua
@@ -40,7 +40,6 @@ require('lazy').setup({
         },
         {
             'kylechui/nvim-surround',
-            version = '^3.0.0', -- Use for stability; omit to use `main` branch for the latest features
             event = 'VeryLazy',
             config = function()
                 require('nvim-surround').setup()
