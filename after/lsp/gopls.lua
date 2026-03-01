5f1942b322d22f949f0e0e1229b51c68ac5b83ee
n3wborn <n3wborn@protonmail.com>
2026-03-01 17:16:20 +0100

    feat(lsp): improve gopls

--- after/lsp/gopls.lua
+++ after/lsp/gopls.lua
@@ -1,6 +1,16 @@
 ---@type vim.lsp.Config
 return {
     cmd = { 'gopls' },
-    filetypes = { 'go', 'gomod' },
+    filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
+    settings = {
+        gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
+            analyses = {
+                unusedparams = true,
+            },
+            gofumpt = true,
+        },
+    },
     root_markers = { 'go.mod', 'go.sum' },
 }
