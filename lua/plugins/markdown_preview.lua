3aa42632c3c1c0c83044d3103d67062880243c0b
n3wborn <n3wborn@protonmail.com>
2026-03-02 22:45:01 +0100

    feat(plugins): update when loading which md previewer

--- lua/plugins/markdown_preview.lua
+++ lua/plugins/markdown_preview.lua
@@ -2,11 +2,5 @@ return {
     'iamcco/markdown-preview.nvim',
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
     build = 'cd app && npm install',
-    init = function()
-        vim.g.mkdp_filetypes = { 'markdown' }
-    end,
-    cond = function()
-        return vim.g.markdown_preview_enabled and not vim.g.live_previewer_enabled
-    end,
     ft = { 'markdown' },
 }
