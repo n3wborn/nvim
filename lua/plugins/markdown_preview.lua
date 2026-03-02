5d2f08a92e76bdc228909cde25f71965a32b9e26
Stephane Pluchart <stephane.pluchart@quarksgroup.com>
2026-03-02 15:18:01 +0100

    fix(markdown_preview): update build command

--- lua/plugins/markdown_preview.lua
+++ lua/plugins/markdown_preview.lua
@@ -1,7 +1,7 @@
 return {
     'iamcco/markdown-preview.nvim',
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    build = 'cd app && yarn install',
+    build = 'cd app && npm install',
     init = function()
         vim.g.mkdp_filetypes = { 'markdown' }
     end,
