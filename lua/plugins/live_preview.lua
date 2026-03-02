eaa71dc36b61cc5ce33c6673d86de8041e7eee75
Stéphane P <n3wborn@pm.me>
2026-03-02 22:48:55 +0100

    feat: update md previewer conditional choice  (#596)
    * feat(lazy): markdown previewer optional loading condition
    
    * feat(plugins): update when loading which md previewer

--- lua/plugins/live_preview.lua
+++ lua/plugins/live_preview.lua
@@ -1,6 +1,10 @@
+---@type LazyPluginSpec
 return {
     'brianhuster/live-preview.nvim',
     ft = { 'markdown' },
+    cond = function()
+        return vim.g.live_previewer_enabled
+    end,
     dependencies = {
         'folke/snacks.nvim',
     },
--- lua/plugins/markdown_preview.lua
+++ lua/plugins/markdown_preview.lua
@@ -2,8 +2,5 @@ return {
     'iamcco/markdown-preview.nvim',
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
     build = 'cd app && npm install',
-    init = function()
-        vim.g.mkdp_filetypes = { 'markdown' }
-    end,
     ft = { 'markdown' },
 }
