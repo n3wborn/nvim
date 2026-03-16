013e9a80e2e157526920aae074663f702e3827b6
n3wborn <n3wborn@protonmail.com>
2026-03-16 22:19:59 +0100

    feat(treesitter): add packChanged event cb

--- init.lua
+++ init.lua
@@ -6,6 +6,18 @@ _G.global.float_border_opts = { border = 'rounded', focusable = false, scope = '
 
 require('config.options')
 
+vim.api.nvim_create_autocmd('PackChanged', {
+    callback = function(ev)
+        local name, kind = ev.data.spec.name, ev.data.kind
+        if name == 'nvim-treesitter' and kind == 'update' then
+            if not ev.data.active then
+                vim.cmd.packadd('nvim-treesitter')
+            end
+            vim.cmd('TSUpdate')
+        end
+    end,
+})
+
 -- :help vim-pack
 vim.pack.add({
     { src = 'https://github.com/neovim/nvim-lspconfig' },
