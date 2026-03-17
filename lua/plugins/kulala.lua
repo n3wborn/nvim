79c6e75ea35583c4ad6031a2cf5e02e0cd48aa8d
n3wborn <n3wborn@protonmail.com>
2026-03-17 21:16:13 +0100

    feat(kualal): update config

--- lua/plugins/kulala.lua
+++ lua/plugins/kulala.lua
@@ -12,27 +12,42 @@
 return {
     'mistweaverco/kulala.nvim',
     ft = { 'http', 'rest' },
-    keys = {
-        {
-            '<leader>K',
-            function()
-                require('kulala').run()
-            end,
-            { 'n', 'v' },
+    opts = {
+        icons = {
+            inlay = {
+                loading = '󰔛',
+                done = '',
+                error = '',
+            },
+            textHighlight = 'Comment',
         },
-        {
-            '<leader>Ka',
-            function()
-                require('kulala').run_all()
-            end,
-            { 'n' },
-        },
-        {
-            '<leader>Kr',
-            function()
-                require('kulala').replay()
-            end,
-            { 'n' },
+        ui = {
+            win_opts = {
+                wo = {
+                    signcolumn = 'no',
+                },
+                bo = {
+                    buflisted = true,
+                },
+            },
+            syntax_hl = {
+                ['@operator.kulala_http'] = 'Operator',
+                ['@punctuation.bracket.kulala_http'] = '@punctuation.bracket',
+                ['@variable.kulala_http'] = '@variable',
+            },
         },
     },
+    keys = function()
+        local kulala = require('kulala')
+        return {
+            { '[r', kulala.jump_prev, desc = 'Goto next request' },
+            { ']r', kulala.jump_next, desc = 'Goto prev request' },
+            { '<leader>K', kulala.run, desc = 'Kulala Run' },
+            { '<leader>Ka', kulala.run_all, desc = 'Kulala Run All' },
+            { '<leader>Kr', kulala.replay, desc = 'Kulala Run All' },
+            { '<leader>Ke', kulala.set_selected_env, desc = 'Kulala Env' },
+            { '<leader>Kc', kulala.copy, desc = 'Kulala Copy (Curl)' },
+            { '<leader>Kp', kulala.from_curl, desc = 'Kulala Paste (Curl)' },
+        }
+    end,
 }
