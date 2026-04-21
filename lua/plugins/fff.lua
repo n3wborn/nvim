2d7d150a4cb349618876d42eb750ff5c72520666
Stéphane P <n3wborn@pm.me>
2026-04-21 09:12:56 +0200

    feat(plugins): update fff config (#623)
    * feat(fff): update config
    
    * feat(fff): remove debug mode

--- lua/plugins/fff.lua
+++ lua/plugins/fff.lua
@@ -1,44 +1,57 @@
+vim.g.fff = {
+    prompt = '🪿 ',
+    layout = {
+        height = 0.9,
+        width = 0.9,
+        prompt_position = 'top',
+        preview_size = 0.6,
+    },
+    preview = {
+        enabled = true,
+        line_numbers = true,
+    },
+    debug = {
+        enabled = false,
+        show_scores = true,
+    },
+    grep = {
+        max_matches_per_file = 1, -- 0 to unlimited
+        time_budget_ms = 100, -- prevents UI freeze, 0 = no limit
+    },
+    keymaps = {
+        close = '<Esc>',
+        select = '<CR>',
+        select_split = '<C-s>',
+        select_vsplit = '<C-v>',
+        select_tab = '<C-t>',
+        -- you can assign multiple keys to any action
+        move_up = { '<Up>', '<C-p>' },
+        move_down = { '<Down>', '<C-n>' },
+        preview_scroll_up = '<C-u>',
+        preview_scroll_down = '<C-d>',
+        toggle_debug = '<F2>',
+        -- grep mode: cycle between plain text, regex, and fuzzy search
+        cycle_grep_modes = '<S-Tab>',
+        -- goes to the previous query in history
+        cycle_previous_query = '<C-Up>',
+        -- multi-select keymaps for quickfix
+        toggle_select = '<Tab>',
+        send_to_quickfix = '<C-q>',
+        -- this are specific for the normal mode (you can exit it using any other keybind like jj)
+        focus_list = '<leader>l',
+        focus_preview = '<leader>p',
+    },
+    file_picker = {
+        current_file_label = '(current)',
+    },
+}
+
 ---@type LazyPluginSpec
 return {
     'dmtrKovalenko/fff.nvim',
     build = function()
         require('fff.download').download_or_build_binary() -- use gb on lazy to rebuild when needed
     end,
-    opts = {
-        prompt = '🪿 ',
-        layout = {
-            height = 0.9,
-            width = 0.9,
-            prompt_position = 'top',
-            preview_size = 0.6,
-            show_scrollbar = false,
-            -- How to shorten long directory paths in the file list:
-            -- 'middle_number' (default): uses dots for 1-3 hidden (a/./b, a/../b, a/.../b)
-            --                            and numbers for 4+ (a/.4./b, a/.5./b)
-            -- 'middle': always uses dots (a/./b, a/../b, a/.../b)
-            -- 'end': truncates from the end (home/user/projects)
-            path_shorten_strategy = 'middle_number',
-        },
-        preview = {
-            enabled = true,
-            max_size = 8 * 10 * 1024 * 1024, -- Do not try to read files larger than 80MB
-            chunk_size = 8192, -- Bytes per chunk for dynamic loading (8kb - fits ~100-200 lines)
-            binary_file_threshold = 0, -- amount of bytes to scan for binary content (set 0 to disable)
-            imagemagick_info_format_str = '%m: %wx%h, %[colorspace], %q-bit',
-            line_numbers = true,
-            cursorlineopt = 'both', -- the cursorlineopt used for lines in grep file previews, see :h cursorlineopt
-        },
-        debug = {
-            enabled = true,
-            show_scores = true,
-        },
-        grep = {
-            max_file_size = 8 * 10 * 1024 * 1024, -- Skip files larger than 80MB
-            max_matches_per_file = 1, -- 0 to unlimited
-            smart_case = true,
-            time_budget_ms = 150, -- prevents UI freeze, 0 = no limit
-        },
-    },
     lazy = false, -- it already lazy-load itself
     keys = {
         {
@@ -73,7 +86,12 @@ return {
         {
             '<space>sd',
             function()
-                require('fff').live_grep({ query = vim.fn.expand('<cword>') })
+                local word = vim.fn.expand('<cword>')
+                if word == '' then
+                    word = vim.fn.expand('<cWORD>')
+                end
+
+                require('fff').live_grep({ query = word })
             end,
             desc = '[FFF] Find Current Word',
         },
