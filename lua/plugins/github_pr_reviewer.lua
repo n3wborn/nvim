2825af18cd3daa4141b1e06a19572c1972a29b33
n3wborn <n3wborn@pm.me>
2026-05-10 14:19:50 +0200

    refactor(keymaps): move keymaps into plugin's keys

--- lua/plugins/github_pr_reviewer.lua
+++ lua/plugins/github_pr_reviewer.lua
@@ -26,6 +26,33 @@ return {
         -- Key to go to previous modified file (only works in review mode)
         prev_file_key = '<C-h>',
     },
+    keys = function()
+        local pr = require('github-pr-reviewer')
+
+        -- stylua: ignore start
+        return {
+            { "<leader>p",  function() pr.menu() end,                  { desc = "PR Review Menu" } },
+
+            { "<leader>pr", function() pr.review() end,                { desc = "Start PR review" } },
+            { "<leader>pl", function() pr.list_review_requests() end,  { desc = "List review requests" } },
+            { "<leader>pc", function() pr.review_cleanup() end,        { desc = "Cleanup PR review" } },
+            { "<leader>pi", function() pr.info() end,                  { desc = "Show PR info" } },
+            { "<leader>po", function() pr.open() end,                  { desc = "Open PR in browser" } },
+            { "<leader>pb", function() pr.review_buffer() end,         { desc = "Toggle review buffer" } },
+
+            { "<leader>pC", function() pr.line_comment() end,          { desc = "Add line comment" } },
+            { "<leader>pP", function() pr.pending_comment() end,       { desc = "Add pending comment" } },
+            { "<leader>pv", function() pr.list_all_comments() end,     { desc = "List all comments" } },
+            { "<leader>pp", function() pr.list_pending_comments() end, { desc = "List pending comments" } },
+            { "<leader>pR", function() pr.reply() end,                 { desc = "Reply to comment" } },
+            { "<leader>pe", function() pr.edit_comment() end,          { desc = "Edit my comment" } },
+            { "<leader>pd", function() pr.delete_comment() end,        { desc = "Delete my comment" } },
+
+            { "<leader>pa", function() pr.approve() end,               { desc = "Approve PR" } },
+            { "<leader>px", function() pr.request_changes() end,       { desc = "Request changes" } },
+        }
+        -- stylua: ignore end
+    end,
     cmd = {
         'PR',
         'PRReview',
