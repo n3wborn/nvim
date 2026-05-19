63668a55f144030628d50079d334871d4bddab74
N3wborn <n3wborn@pm.me>
2026-05-19 21:21:47 +0200

    feat(fff): move into lazy spec, add keymaps, use vim.g (#654)
    I still struggle to find the best way to handle this one.
    I'm quite sure it was possible to not have a line with location
    of found occurences in one of the live_grep keymaps on month or
    too before.

--- init.lua
+++ init.lua
@@ -3,7 +3,54 @@ vim.g.maplocalleader = ','
 
 _G.global = {}
 _G.global.float_border_opts = { border = 'rounded', focusable = false, scope = 'line' }
+vim.g.fff = {
+    prompt = '🪿 ',
 
+    layout = {
+        height = 0.9,
+        width = 0.9,
+        prompt_position = 'top',
+        preview_size = 0.6,
+    },
+    preview = {
+        enabled = true,
+        line_numbers = true,
+    },
+    debug = {
+        enabled = false,
+        show_scores = false,
+    },
+    grep = {
+        max_matches_per_file = 1, -- 0 to unlimited
+        time_budget_ms = 100, -- prevents UI freeze, 0 = no limit
+    },
+    keymaps = {
+        close = '<esc><esc>',
+        select = '<CR>',
+        select_split = '<C-s>',
+        select_vsplit = '<C-v>',
+        select_tab = '<C-t>',
+        -- you can assign multiple keys to any action
+        move_up = { '<Up>', '<C-p>' },
+        move_down = { '<Down>', '<C-n>' },
+        preview_scroll_up = '<C-u>',
+        preview_scroll_down = '<C-d>',
+        toggle_debug = '<F2>',
+        -- grep mode: cycle between plain text, regex, and fuzzy search
+        cycle_grep_modes = '<S-Tab>',
+        -- goes to the previous query in history
+        cycle_previous_query = '<C-Up>',
+        -- multi-select keymaps for quickfix
+        toggle_select = '<Tab>',
+        send_to_quickfix = '<C-q>',
+        -- this are specific for the normal mode (you can exit it using any other keybind like jj)
+        focus_list = '<leader>l',
+        focus_preview = '<leader>p',
+    },
+    file_picker = {
+        current_file_label = '[Current File]',
+    },
+}
 local disabled_builtins = {
     'gzip',
     'matchit',
@@ -55,6 +102,13 @@ require('lazy').setup({
             dependencies = 'nvzone/volt',
             opts = {},
             cmd = { 'Typr', 'TyprStats' },
+        },
+        {
+            'dmtrKovalenko/fff.nvim',
+            lazy = false,
+            build = function()
+                require('fff.download').download_or_build_binary()
+            end,
         },
     },
     defaults = {
