026042ef0a67d813a880f8194f84dbea0096e985
N3wborn <n3wborn@pm.me>
2026-05-25 01:35:16 +0200

    feat(arborist): use dev branch, add lua parser (#660)

--- lua/plugins/arborist.lua
+++ lua/plugins/arborist.lua
@@ -1,14 +1,15 @@
 ---@type LazyPluginSpec
 return {
     'arborist-ts/arborist.nvim',
+    branch = 'dev',
     event = { 'BufEnter', 'BufNewFile' },
     ---@module "arborist"
     ---@type arborist.Config
     opts = {
         prefer_wasm = false,
         update_cadence = 'daily',
-        install_popular = true,
         ensure_installed = {
+            'lua',
             'zig',
         },
     },
