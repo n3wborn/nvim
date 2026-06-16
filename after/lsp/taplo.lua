7ba1ff0290a77662fc114466e802689c4367b8d2
n3wborn <n3wborn@pm.me>
2026-06-16 21:57:01 +0200

    feat(lsp): update taplo config
    Set specific config and document

---
+++ after/lsp/taplo.lua
@@ -0,0 +1,19 @@
+return {
+    -- kind of schema notation needed for taplo: `"$schema" = 'https://starship.rs/config-schema.json'`
+    cmd = { 'taplo', 'lsp', 'stdio' },
+    filetypes = { 'toml' },
+    settings = {
+        -- Use the defaults that the VSCode extension uses: https://github.com/tamasfe/taplo/blob/2e01e8cca235aae3d3f6d4415c06fd52e1523934/editors/vscode/package.json
+        taplo = {
+            configFile = { enabled = true },
+            schema = {
+                enabled = true,
+                catalogs = { 'https://www.schemastore.org/api/json/catalog.json' },
+                cache = {
+                    memoryExpiration = 60,
+                    diskExpiration = 600,
+                },
+            },
+        },
+    },
+}
