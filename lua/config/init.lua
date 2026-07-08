2be7c41a1cda35984c5a00bf8331bcb64236c474
n3wborn <n3wborn@pm.me>
2026-07-08 02:23:13 +0200

    feat(folding): set method preference LSP > treesitter > indent

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,5 +1,6 @@
+require('config.options')
 require('config.keymaps')
 require('config.autocommands')
-require('config.lsp')
-require('config.options')
 require('config.winbar')
+require('config.lsp')
+require('config.folding')
