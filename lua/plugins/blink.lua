acbbe5fdfb5f1a97cc2bd3d6028466c571e94de8
n3wborn <n3wborn@pm.me>
2026-08-02 23:05:14 +0200

    feat(blink): enable normal mode without typing <ESC> twice

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -49,7 +49,6 @@ return {
             ['<Tab>'] = { 'snippet_forward', 'select_next', 'fallback' },
             ['<S-Tab>'] = { 'snippet_backward', 'select_prev', 'fallback' },
             ['<CR>'] = { 'select_and_accept', 'fallback' },
-            ['<Esc>'] = { 'cancel', 'hide_documentation', 'fallback' },
 
             ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
 
