da87683e0446a6eec129960df089ead73c315918
N3wborn <n3wborn@pm.me>
2026-08-10 01:13:22 +0200

    feat(php): add phpcs config to nvim-lint (#732)

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -2,15 +2,15 @@
 ---@type LazyPluginSpec
 return {
     'mfussenegger/nvim-lint',
-    event = { 'BufReadPre', 'BufNewFile' }, -- to disable, comment this out
+    event = { 'BufReadPre', 'BufNewFile' },
+
     opts = {
-        -- Event to trigger linters
         events = { 'BufWritePost', 'BufReadPost', 'InsertLeave' },
         linters_by_ft = {
             gitcommit = { 'gitlint' },
             javascript = { 'eslint_d' },
             javascriptreact = { 'eslint_d' },
-            php = { 'php' },
+            php = { 'php', 'phpcs' },
             python = { 'ruff' },
             typescript = { 'eslint_d' },
             typescriptreact = { 'eslint_d' },
@@ -18,27 +18,17 @@ return {
         },
         linters = {},
     },
-    config = function(_, opts)
-        local M = {}
-        local lint = require('lint')
 
-        for name, linter in pairs(opts.linters) do
-            if type(linter) == 'table' and type(lint.linters[name]) == 'table' then
-                lint.linters[name] = vim.tbl_deep_extend('force', lint.linters[name], linter)
-                if type(linter.prepend_args) == 'table' then
-                    lint.linters[name].args = lint.linters[name].args or {}
-                    vim.list_extend(lint.linters[name].args, linter.prepend_args)
-                end
-            else
-                lint.linters[name] = linter
-            end
-        end
-        lint.linters_by_ft = opts.linters_by_ft
+    config = function(_, opts)
+        local lint = require('lint')
+        local M = {}
 
         function M.debounce(ms, fn)
             local timer = vim.uv.new_timer()
+
             return function(...)
                 local argv = { ... }
+
                 timer:start(ms, 0, function()
                     timer:stop()
                     vim.schedule_wrap(fn)(unpack(argv))
@@ -46,11 +36,92 @@ return {
             end
         end
 
+        do
+            local native_phpcs = lint.linters.phpcs
+
+            if native_phpcs then
+                local phpcs = vim.deepcopy(native_phpcs)
+
+                phpcs.cmd = function()
+                    local filename = vim.api.nvim_buf_get_name(0)
+                    local dirname = vim.fs.dirname(filename)
+
+                    local local_phpcs = vim.fs.find('vendor/bin/phpcs', {
+                        upward = true,
+                        path = dirname,
+                        type = 'file',
+                    })[1]
+
+                    if local_phpcs then
+                        return local_phpcs
+                    end
+
+                    return 'phpcs'
+                end
+
+                phpcs.args = {
+                    '-q',
+                    '--report=json',
+
+                    function()
+                        local filename = vim.api.nvim_buf_get_name(0)
+
+                        return '--stdin-path=' .. vim.fn.fnamemodify(filename, ':p:.')
+                    end,
+
+                    function()
+                        local filename = vim.api.nvim_buf_get_name(0)
+                        local dirname = vim.fs.dirname(filename)
+
+                        local config = vim.fs.find({
+                            'phpcs.xml',
+                            'phpcs.xml.dist',
+                            '.phpcs.xml',
+                            '.phpcs.xml.dist',
+                        }, {
+                            upward = true,
+                            path = dirname,
+                            type = 'file',
+                        })[1]
+
+                        if config then
+                            return '--standard=' .. config
+                        end
+
+                        return '--standard=PSR12'
+                    end,
+
+                    '-',
+                }
+
+                lint.linters.phpcs = phpcs
+            end
+        end
+
+        for name, linter in pairs(opts.linters) do
+            if name ~= 'phpcs' then
+                if type(linter) == 'table' and type(lint.linters[name]) == 'table' then
+                    lint.linters[name] = vim.tbl_deep_extend('force', lint.linters[name], linter)
+
+                    if type(linter.prepend_args) == 'table' then
+                        lint.linters[name].args = lint.linters[name].args or {}
+
+                        vim.list_extend(lint.linters[name].args, linter.prepend_args)
+                    end
+                else
+                    lint.linters[name] = linter
+                end
+            end
+        end
+
+        lint.linters_by_ft = opts.linters_by_ft
+
         function M.lint()
             -- Use nvim-lint's logic first:
             -- * checks if linters exist for the full filetype first
             -- * otherwise will split filetype by "." and add all those linters
-            -- * this differs from conform.nvim which only uses the first filetype that has a formatter
+            -- * this differs from conform.nvim which only uses the first
+            --   filetype that has a formatter
             local names = lint._resolve_linter_by_ft(vim.bo.filetype)
 
             -- Create a copy of the names table to avoid modifying the original.
@@ -65,13 +136,19 @@ return {
             vim.list_extend(names, lint.linters_by_ft['*'] or {})
 
             -- Filter out linters that don't exist or don't match the condition.
-            local ctx = { filename = vim.api.nvim_buf_get_name(0) }
+            local ctx = {
+                filename = vim.api.nvim_buf_get_name(0),
+            }
+
             ctx.dirname = vim.fn.fnamemodify(ctx.filename, ':h')
+
             names = vim.tbl_filter(function(name)
                 local linter = lint.linters[name]
+
                 if not linter then
                     vim.notify('Linter not found: ' .. name, { title = 'nvim-lint' })
                 end
+
                 return linter and not (type(linter) == 'table' and linter.condition and not linter.condition(ctx))
             end, names)
 
@@ -82,7 +159,9 @@ return {
         end
 
         vim.api.nvim_create_autocmd(opts.events, {
-            group = vim.api.nvim_create_augroup('nvim-lint', { clear = true }),
+            group = vim.api.nvim_create_augroup('nvim-lint', {
+                clear = true,
+            }),
             callback = M.debounce(100, M.lint),
         })
     end,
