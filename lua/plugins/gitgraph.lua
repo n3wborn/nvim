e8e007c988f9e1820e2d1fe54134d0774eb7e164
n3wborn <n3wborn@pm.me>
2026-09-01 22:41:32 +0200

    feat(gitgraph): notification

--- lua/plugins/gitgraph.lua
+++ lua/plugins/gitgraph.lua
@@ -44,13 +44,13 @@ return {
         hooks = {
             -- Check diff of a commit
             on_select_commit = function(commit)
-                vim.notify('DiffviewOpen ' .. commit.hash .. '^!')
                 vim.cmd(':DiffviewOpen ' .. commit.hash .. '^!')
+                vim.notify('DiffviewOpen ' .. commit.hash .. '^!')
             end,
             -- Check diff from commit a -> commit b
             on_select_range_commit = function(from, to)
-                vim.notify('DiffviewOpen ' .. from.hash .. '~1..' .. to.hash)
                 vim.cmd(':DiffviewOpen ' .. from.hash .. '~1..' .. to.hash)
+                vim.notify('DiffviewOpen ' .. from.hash .. '~1..' .. to.hash)
             end,
         },
     },
